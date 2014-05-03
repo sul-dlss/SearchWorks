dc53594698e52389bff62adbe0a5335e520ae94a
Jessie Keck <jessie.keck@gmail.com>
2014-05-02 20:42:28 -0700

    fixup w/ rspec install

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,9 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+
+  config.include Capybara::DSL
+
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
