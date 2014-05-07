61eed690973085a840104c9cfd298a0145431b36
Jack Reed <pjreed@stanford.edu>
2014-05-07 14:24:58 -0700

    creating desktop layout for search bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,14 @@
 module ApplicationHelper
 
+  def render_search_bar_advanced_widget
+    render partial:'catalog/search_bar_advanced_widget'
+  end
+
+  def render_search_bar_browse_widget
+    render partial: 'catalog/search_bar_browse_widget'
+  end
+
+  def render_search_bar_selections_widget
+    render partial: 'catalog/search_bar_selections_widget'
+  end
 end
---
+++ spec/features/blacklight_customizations/search_toolbar_spec.rb
@@ -0,0 +1,15 @@
+require "spec_helper"
+
+describe "Search toolbar", js: true, feature: true do
+    before { visit root_path }
+  describe "has SearchWorks customizations" do
+    it "should display correct elements" do
+      within "#search-navbar" do
+        expect(page.find('button.btn.btn-primary.search-btn')).to have_no_content 'Search'
+        expect(page).to have_css("li.disabled a", text: "Advanced")
+        expect(page).to have_css("li.disabled a", text: "Browse")
+        expect(page).to have_css("li.disabled a", text: "Selections")
+      end
+    end
+  end
+end
