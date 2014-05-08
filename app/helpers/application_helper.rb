652c79a861b405757980036b75c1e01832c63b85
Jack Reed <pjreed@stanford.edu>
2014-05-08 19:32:31 -0400

    adds search target to the layout with tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,9 @@ module ApplicationHelper
   def render_search_bar_selections_widget
     render partial: 'catalog/search_bar_selections_widget'
   end
+
+  def render_search_targets_widget
+    render partial: 'catalog/search_targets_widget'
+  end
+
 end
--- spec/features/blacklight_customizations/search_toolbar_spec.rb
+++ spec/features/blacklight_customizations/search_toolbar_spec.rb
@@ -6,9 +6,9 @@ describe "Search toolbar", js: true, feature: true do
     it "should display correct elements" do
       within "#search-navbar" do
         expect(page.find('button.btn.btn-primary.search-btn')).to have_no_content 'Search'
-        expect(page).to have_css("li.disabled a", text: "Advanced")
-        expect(page).to have_css("li.disabled a", text: "Browse")
-        expect(page).to have_css("li.disabled a", text: "Selections")
+        expect(page).to have_css("li.disabled a", text: "Advanced", visible: true)
+        expect(page).to have_css("li.disabled a", text: "Browse", visible: true)
+        expect(page).to have_css("li.disabled a", text: "Selections", visible: true)
       end
     end
   end
