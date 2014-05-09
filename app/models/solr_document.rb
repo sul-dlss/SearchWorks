1390b043430ed726314eeb3dde836aeccb9b180b
Jessie Keck <jessie.keck@gmail.com>
2014-05-08 18:21:36 -0700

    Add basic Blacklight controller/solr doc config to match new solr config/schema
    This is mainly for demo purposes and our metadata displays will require
    much more detailed crafting that is possible w/ the basic Blacklight
    index/show field configuration. There aren't tests for these configs
    because we'll be ripping them out as soon as we address those tickets.

--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -3,10 +3,10 @@ class SolrDocument
 
   include Blacklight::Solr::Document    
       # The following shows how to setup this blacklight document to display marc documents
-  extension_parameters[:marc_source_field] = :marc_display
+  extension_parameters[:marc_source_field] = :marcxml
   extension_parameters[:marc_format_type] = :marcxml
   use_extension( Blacklight::Solr::Document::Marc) do |document|
-    document.key?( :marc_display  )
+    document.key?( :marcxml  )
   end
   
   field_semantics.merge!(    
