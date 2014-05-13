74f6c74ae0aa658db9f26f0a7c4e1f10ba0e4399
Jack Reed <pjreed@stanford.edu>
2014-05-13 16:05:45 -0700

    adds custom access point facet heading

--- spec/features/blacklight_customizations/facets_spec.rb
+++ spec/features/blacklight_customizations/facets_spec.rb
@@ -11,4 +11,21 @@ feature "Facets Customizations" do
       end
     end
   end
+  scenario "while not in an access point facet title should not change" do
+    visit root_path
+    fill_in "q", with: ''
+    click_button 'search'
+
+    within "div#facets" do
+      expect(page).to have_css("div.top-panel-heading.panel-heading h4", text: "Limit your search")
+    end
+  end
+
+  scenario "while at an access point facet title should reflect custom heading" do
+    visit '/databases'
+
+    within "div#facets" do
+      expect(page).to have_css("div.top-panel-heading.panel-heading h4", text: "Within databases")
+    end
+  end
 end
