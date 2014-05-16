8634d12f43ecdc8327fa822b3efff9b1dab7cf9b
Jack Reed <pjreed@stanford.edu>
2014-05-16 10:48:10 -0700

    adds aria landmarks
    adds testing for all ARIA landmarks

---
+++ spec/features/blacklight_customizations/accessibility_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+feature "Aria Landmarks", js: true do
+  before do
+    visit root_path
+    fill_in "q", with: ''
+    click_button 'search'
+  end
+
+  scenario "should have header landmark" do
+    expect(page).to have_xpath("//header[@id='topnav' and @role='banner']")
+  end
+
+  scenario "should have feedback landmark" do
+    pending("should have feedback landmark")
+  end
+
+  scenario "should have SearchWorks navbar landmark" do
+    expect(page).to have_xpath("//nav[@id='search-navbar' and @role='navigation']")
+  end
+
+  scenario "should have search form landmark" do
+    expect(page).to have_xpath("//form[@role='search']")
+  end
+
+  scenario "should have context toolbar landmark" do
+    pending("should have context toolbar landmark")
+  end
+
+  scenario "should have main container landmark" do
+    expect(page).to have_xpath("//section[@role='main']")
+  end
+
+  scenario "should have footer landmark" do
+    pending("should have footer landmark")
+  end
+
+  scenario "should have browse nearby landmark" do
+    pending("should have browse nearby landmark")
+  end
+end
