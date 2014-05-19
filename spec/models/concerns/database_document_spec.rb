1d8ee62601ecb2a31d552bd1475ed23aac837d3d
Jessie Keck <jessie.keck@gmail.com>
2014-05-19 11:33:32 -0700

    Add database document concern to identify databases

---
+++ spec/models/concerns/database_document_spec.rb
@@ -0,0 +1,22 @@
+require "spec_helper"
+
+class DatabaseDocumentTest < Hash
+  include DatabaseDocument
+  def format_key
+    :the_format
+  end
+end
+
+describe DatabaseDocument do
+  describe "#is_a_database?" do
+    let(:document) { DatabaseDocumentTest.new }
+    it "should return true for databases" do
+      document[:the_format] = ["Book", "Database"]
+      expect(document.is_a_database?).to be_true
+    end
+    it "should return false non-databases" do
+      document[:the_format] = ["Not a db"]
+      expect(document.is_a_database?).to be_false
+    end
+  end
+end
