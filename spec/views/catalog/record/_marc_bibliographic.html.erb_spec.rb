5072facbd670f13fa5581abd397004d77b96121a
Jessie Keck <jessie.keck@gmail.com>
2014-05-21 11:47:03 -0700

    Only show MARC 592 for databases.

---
+++ spec/views/catalog/record/_marc_bibliographic.html.erb_spec.rb
@@ -0,0 +1,24 @@
+require "spec_helper"
+
+describe "catalog/record/_marc_bibliographic.html.erb" do
+  include MarcMetadataFixtures
+
+  describe "MARC 592" do
+    let(:document) { SolrDocument.new(marcxml: marc_592_fixture) }
+    before do
+      assign(:document, document)
+    end
+    it "should display for databases" do
+      document.stub(:is_a_database?).and_return(true)
+      render
+      expect(rendered).to have_css("dt", text: "Note")
+      expect(rendered).to have_css("dd", text: "A local note added to subjects only")
+    end
+    it "should not display for non-databases" do
+      document.stub(:is_a_database?).and_return(false)
+      render
+      expect(rendered).to_not have_css("dt", text: "Note")
+      expect(rendered).to_not have_css("dd", text: "A local note added to subjects only")
+    end
+  end
+end
