5f6b7a11c3aa59e81bcb5f2249aa18b7fabcf100
Jessie Keck <jessie.keck@gmail.com>
2014-05-22 09:28:56 -0700

    Add finding aid support to SearchWorks::Links.

--- spec/lib/links_spec.rb
+++ spec/lib/links_spec.rb
@@ -5,7 +5,9 @@ describe SearchWorks::Links do
   before do
     links.stub(:all).and_return([
       OpenStruct.new(text: "non-fulltext link", fulltext?: false),
-      OpenStruct.new(text: "fulltext link",     fulltext?: true)
+      OpenStruct.new(text: "fulltext link",     fulltext?: true),
+      OpenStruct.new(text: "1st finding aid link", fulltext?: true,  finding_aid?: true),
+      OpenStruct.new(text: "2nd finding aid link", fulltext?: false, finding_aid?: true)
     ])
   end
   it "should identify fulltext links" do
@@ -16,8 +18,13 @@ describe SearchWorks::Links do
     expect(links.supplemental.length).to eq 1
     expect(links.supplemental.first.text).to eq "non-fulltext link"
   end
+  it "should identify finding aid links" do
+    expect(links.finding_aid.length).to eq 2
+    expect(links.finding_aid.first.text).to eq "1st finding aid link"
+    expect(links.finding_aid.last.text).to eq "2nd finding aid link"
+  end
   describe "Link" do
-    let(:link) { SearchWorks::Links::Link.new(text: "text", fulltext: true, stanford_only: true) }
+    let(:link) { SearchWorks::Links::Link.new(text: "text", fulltext: true, stanford_only: true, finding_aid: true) }
     it "should parse the text option" do
       expect(link.text).to eq "text"
     end
@@ -26,6 +33,9 @@ describe SearchWorks::Links do
     end
     it "should parse the stanford only option" do
       expect(link).to be_stanford_only
+    end
+    it "should parse the finding_aid option" do
+      expect(link).to be_finding_aid
     end
   end
 end
