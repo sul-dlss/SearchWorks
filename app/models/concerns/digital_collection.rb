aee70a54be8d2bb5b85d37d2c0bea9543cfc4efd
Jessie Keck <jessie.keck@gmail.com>
2014-05-27 16:32:13 -0700

    Add #collection_members method in DigitalCollection mixin to get {N} items in a collection.

--- app/models/concerns/digital_collection.rb
+++ app/models/concerns/digital_collection.rb
@@ -2,4 +2,37 @@ module DigitalCollection
   def is_a_collection?
     self[:collection_type] and self[:collection_type].include?('Digital Collection')
   end
+
+  def collection_members(options={})
+    return nil unless is_a_collection?
+    @collection_members ||= CollectionMembers.new(self)
+  end
+
+  private
+
+  class CollectionMembers
+    def initialize(document, options={})
+      @options = options
+      @document = document
+    end
+
+    def total
+      response['numFound']
+    end
+    def documents
+      response['docs'].map do |document|
+        ::SolrDocument.new(document)
+      end
+    end
+    private
+    def response
+      @response ||= Blacklight.solr.select(
+        params: {
+          fq: "collection:\"#{@document[:id]}\"",
+          rows: (@options[:rows] || 20)
+        }
+      )['response']
+    end
+  end
+
 end
