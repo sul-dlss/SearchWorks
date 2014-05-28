f1c3367832bede7f207e113bc32a952aedc2ce5f
Jessie Keck <jessie.keck@gmail.com>
2014-05-27 17:03:20 -0700

    Add #parent_collections method to CollectionMember mixin to identify the parent collections of an object.

--- spec/models/concerns/collection_member_spec.rb
+++ spec/models/concerns/collection_member_spec.rb
@@ -19,4 +19,33 @@ describe CollectionMember do
       expect(merged_sirsi.is_a_collection_member?).to be_true
     end
   end
+  describe "#parent_collections" do
+    let(:multi_collection) { SolrDocument.new( collection: ['12345', '54321'] ) }
+    let(:stub_solr) { double('solr') }
+    let(:stub_params) { { params: {fq: "id:12345"}} }
+    let(:stub_response) {{
+      'response' => {
+        'docs' => [{id: 12345}]
+      }
+    }}
+    before do
+      Blacklight.stub(:solr).and_return(stub_solr)
+    end
+    it "should search solr for ids in the collection" do
+      expect(Blacklight.solr).to receive(:select).with(stub_params).and_return(stub_response)
+      expect(member.parent_collections).to be_present
+    end
+    it "should return a solr document" do
+      expect(Blacklight.solr).to receive(:select).with(stub_params).and_return(stub_response)
+      member.parent_collections.each do |parent|
+        expect(parent).to be_a SolrDocument
+      end
+    end
+    it "#parent_collection_params should return all IDs joined w/ OR" do
+      expect(multi_collection.send(:parent_collection_params)).to eq "id:12345 OR id:54321"
+    end
+    it "should return nil for non collection members" do
+      expect(non_member.parent_collections).to be_nil
+    end
+  end
 end
