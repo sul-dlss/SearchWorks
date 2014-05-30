ab1f220be14b5ef9a46a6c8987edf28bfe00eece
Jessie Keck <jessie.keck@gmail.com>
2014-05-30 14:05:15 -0700

    Add Merged Image Collection index/show views.

---
+++ spec/features/merged_image_collections_spec.rb
@@ -0,0 +1,35 @@
+require "spec_helper"
+
+feature "Merged Image Collections" do
+  scenario "in search results" do
+    visit root_path
+    fill_in 'q', with: '34'
+    click_button 'search'
+
+    expect(page).to have_css('dt', text: "DIGITAL CONTENT:")
+    expect(page).to have_css('dd', text: /\d+ items?/)
+
+    within('.image-filmstrip') do
+      within('ul.listing') do
+        expect(page).to have_css("a img.thumb-35")
+        expect(page).to have_css("a img.thumb-36")
+      end
+    end
+  end
+  scenario "record view should display metadata and filmstrip" do
+    visit catalog_path('34')
+
+    expect(page).to have_css('h1', text: 'Merged Image Collection1')
+
+    within('.image-filmstrip') do
+      within('ul.listing') do
+        expect(page).to have_css("a img.thumb-35")
+        expect(page).to have_css("a img.thumb-36")
+      end
+    end
+
+    expect(page).to have_css('h2', text: 'Contents/Summary')
+    expect(page).to have_css('h2', text: 'Subjects')
+    expect(page).to have_css('h2', text: 'Bibliographic information')
+  end
+end
---
+++ spec/integration/external-data/merged_image_collections_spec.rb
@@ -0,0 +1,43 @@
+require "spec_helper"
+#6780453
+
+describe "Merged Image Collection", feature: true, :"data-integration" => true do
+  describe "search results" do
+    before do
+      visit root_path
+      fill_in 'q', with: '6780453'
+      click_button 'search'
+    end
+    it "should render metadata and a filmstrip" do
+      expect(page).to have_css('h5 a', text: "Reid W. Dennis collection of California lithographs, 1850-1906")
+      expect(page).to have_css('dt', text: "DIGITAL CONTENT:")
+      expect(page).to have_css('dd', text: "48 items")
+
+      within('.image-filmstrip') do
+        within('ul.listing') do
+          expect(page).to have_css("a img.thumb-zz400gd3785")
+          expect(page).to have_css("a img.thumb-pd337xr9038")
+        end
+      end
+    end
+  end
+  describe "record view" do
+    before do
+      visit catalog_path('6780453')
+    end
+    it "should render metadata and a filmstrip" do
+      expect(page).to have_css('h1', text: "Reid W. Dennis collection of California lithographs, 1850-1906")
+
+      expect(page).to have_css('h2', text: "Contents/Summary")
+      expect(page).to have_css('h2', text: "Subjects")
+      expect(page).to have_css('h2', text: "Bibliographic information")
+
+      within('.image-filmstrip') do
+        within('ul.listing') do
+          expect(page).to have_css("a img.thumb-zz400gd3785")
+          expect(page).to have_css("a img.thumb-pd337xr9038")
+        end
+      end
+    end
+  end
+end
