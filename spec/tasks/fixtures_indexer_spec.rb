e62f52219390759d992553a83b62adfcf2326ee7
Jessie Keck <jessie.keck@gmail.com>
2014-05-30 17:26:56 -0700

    Add test for FixturesIndexer#run.

--- spec/tasks/fixtures_indexer_spec.rb
+++ spec/tasks/fixtures_indexer_spec.rb
@@ -2,9 +2,21 @@ require 'spec_helper'
 require 'fixtures_indexer'
 
 describe FixturesIndexer do
+  let(:stub_solr) { double('solr') }
+  before do
+    expect(Blacklight).to receive(:solr).at_least(1).times.and_return(stub_solr)
+  end
+  describe "run" do
+    it "should index the fixtures and commit them" do
+      expect(stub_solr).to receive(:add).twice.with(subject.fixtures).and_return(true)
+      expect(stub_solr).to receive(:commit).twice.and_return(true)
+      FixturesIndexer.run
+      subject.run
+    end
+  end
   describe "#new" do
     it "should set the solr client" do
-      expect(FixturesIndexer.new.instance_variable_get("@solr")).to be_a RSolr::Client
+      expect(FixturesIndexer.new.instance_variable_get("@solr")).to eq stub_solr
     end
   end
   describe "#fixtures" do
