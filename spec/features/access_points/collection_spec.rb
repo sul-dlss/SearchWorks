5ddd48a7635238d147a8dd7147759c27facd99d8
Jessie Keck <jessie.keck@gmail.com>
2014-06-03 08:23:48 -0700

    Remove dl/dd surrounding summary and only join it if present.

--- spec/features/access_points/collection_spec.rb
+++ spec/features/access_points/collection_spec.rb
@@ -7,7 +7,7 @@ feature "Collection Access Point" do
   scenario "Access point masthead should be visible with 1 course reserve document" do
     within("#masthead") do
       expect(page).to have_css("h1", text: "Image Collection1")
-      expect(page).to have_css("dd", text: "A collection of fixture images from the SearchWorks development index.")
+      expect(page).to have_css("div", text: "A collection of fixture images from the SearchWorks development index.")
       expect(page).to have_css("dt", text: "DIGITAL CONTENT:")
       expect(page).to have_css("dd", text: "1 item")
     end
