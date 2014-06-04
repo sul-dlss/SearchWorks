f2526eca934e621c0af8df06a3fd0f8d695218fb
Jack Reed <pjreed@stanford.edu>
2014-06-04 16:17:40 -0700

    adds merge file collection functionality
    updates fixtures and only show 3 files

---
+++ spec/features/merged_file_collection_spec.rb
@@ -0,0 +1,35 @@
+require "spec_helper"
+
+feature "Merged File Collections" do
+  scenario "in search results" do
+    visit root_path
+    fill_in 'q', with: '38'
+    click_button 'search'
+
+    expect(page).to have_css('dt', text: "DIGITAL CONTENT:")
+    expect(page).to have_css('dd', text: /\d+ items?/)
+
+    within('.file-collection-members') do
+      expect(page).to have_css("a", text:"File Item3")
+      expect(page).to have_css("a", text:"File Item4 - Deathstar Bluprints")
+      expect(page).to have_css("a", text:"File Item5 - Blaster User Manual - The Good Parts")
+      expect(page).to_not have_content("selfie.vine")
+    end
+  end
+  scenario "record view should display metadata and file list" do
+    visit catalog_path('38')
+
+    expect(page).to have_css('h1', text: 'Merged File Collection1')
+
+    within('.file-collection-members') do
+      expect(page).to have_css("a", text:"File Item3")
+      expect(page).to have_css("a", text:"File Item4 - Deathstar Bluprints")
+      expect(page).to have_css("a", text:"File Item5 - Blaster User Manual - The Good Parts")
+      expect(page).to_not have_content("selfie.vine")
+    end
+
+    expect(page).to have_css('h2', text: 'Contents/Summary')
+    expect(page).to have_css('h2', text: 'Subjects')
+    expect(page).to have_css('h2', text: 'Bibliographic information')
+  end
+end
