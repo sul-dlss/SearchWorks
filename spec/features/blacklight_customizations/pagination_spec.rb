e74103c6b3f8ffba781745ab3e0cc38ed64f0241
Jessie Keck <jessie.keck@gmail.com>
2014-06-03 17:00:09 -0700

    Override blacklight paginator for local customizations.
    Remove deep paging links
    Change i18n value used in next/previous links to add custom character

---
+++ spec/features/blacklight_customizations/pagination_spec.rb
@@ -0,0 +1,25 @@
+# encoding: UTF-8
+
+require "spec_helper"
+
+feature "Pagination customization", :"data-integration" => true do
+  before do
+    visit root_path
+    within(first('.home-facet')) do
+      click_link 'Book'
+    end
+  end
+  scenario "should not have deep paging links" do
+    within('ul.pagination') do
+      expect(page).to have_css('li a', count: 8) # would be 10 w/ deep paging links
+      expect(page).to_not have_css('li a', text: /\d{3},\d{3}/) # should not contain a link in the 100k range
+      expect(page).to_not have_css('li a', text: /\d{1,3}.\d{3},\d{3}/) # should not contain a link in the million range
+    end
+  end
+  scenario "should have an overridden character" do
+    within('ul.pagination') do
+      expect(page).to have_css('li a', text: "◄ Previous")
+      expect(page).to have_css('li a', text: "Next ►")
+    end
+  end
+end
