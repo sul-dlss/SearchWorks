2a3560b1dd95113ccb4a50eb091e9fe9132aa3e1
Jack Reed <pjreed@stanford.edu>
2014-06-11 11:22:07 -0700

    initial work on preview controller

---
+++ app/controllers/preview_controller.rb
@@ -0,0 +1,21 @@
+class PreviewController < ApplicationController
+  include Blacklight::Configurable
+  include Blacklight::SolrHelper
+  include Blacklight::Catalog::SearchContext
+  copy_blacklight_config_from(CatalogController)
+
+  def show
+    @response, @document = get_solr_response_for_doc_id
+    respond_to do |format|
+      format.html do
+        render preview: @document, layout: false
+      end
+    end
+  end
+
+  private
+  def _prefixes
+    @_prefixes ||= super + ['catalog']
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   get "feedback" => "feedback_forms#new"
   get "backend_lookup" => "catalog#backend_lookup", defaults: {format: :json}, as: :catalog_backend_lookup
 
+  resources :preview, only: :show
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
