e417a8a98d1214259d6895efb5cbfd12754c1aea
Jessie Keck <jessie.keck@gmail.com>
2014-06-12 12:41:40 -0700

    Add simple Browse integration test.

---
+++ spec/integration/external-data/callnumber_browse_spec.rb
@@ -0,0 +1,26 @@
+# encoding: UTF-8
+require "spec_helper"
+
+describe "Callnumber browse", feature: true, :"data-integration" => true do
+  it "should have an embedded panel on the record page" do
+    visit catalog_path('9696118')
+    within('.record-browse-nearby') do
+      expect(page).to have_css('a', text: 'PS3552 .E74 B4 2012')
+      expect(page).to have_css('a', text: 'Z239 .G75 B477 2012')
+      click_link('Z239 .G75 B477 2012')
+    end
+    expect(page).to have_css("h1", text: "Browse nearby Z239 .G75 B477 2012" )
+    expect(page).to have_css('.document', count: 20)
+
+    # current document should be in results
+    expect(page).to have_css('.document .index_title', text: "Bean spasms : collaborations")
+
+    # page forward
+    within('.browse-toolbar') do
+      click_link('►')
+    end
+
+    # current document should not be in results
+    expect(page).not_to have_css('.index_title', text: "Bean spasms : collaborations")
+  end
+end
