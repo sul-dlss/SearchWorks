9245b41223e4d755197f48d44c2394ae7ad694af
Jack Reed <pjreed@stanford.edu>
2014-06-16 17:14:02 -0700

    adds gallery preview plugin

---
+++ app/assets/javascripts/gallery-preview.js
@@ -0,0 +1,169 @@
+(function($) {
+
+  /*
+    jQuery plugin to attach gallery preview triggers and render previews in
+    gallery view
+
+      Usage: $(selector).galleryPreview();
+
+    This plugin :
+      - adds preview triggers to the gallery preview button
+      - on preview click event, fetches preview content from
+        'data-preview-url' data attribute value and renders it
+  */
+
+
+  $.fn.galleryPreview = function() {
+
+    return this.each(function() {
+      var $item = $(this),
+          $targetId = $($item.data('doc-id'))[0],
+          $previewTarget = $($item.data('preview-target')),
+          $triggerBtn, $closeBtn, $arrow, $content, $viewport, $gallery, $itemsPerRow, $itemWidth;
+
+      init();
+      attachTriggerEvents();
+
+      function showPreview() {
+        var previewUrl = $item.data('preview-url'),
+            $previewArrow,
+            maxLeft,
+            arrowLeft;
+
+        $previewTarget.addClass('preview').empty();
+
+        appendPreviewContent(previewUrl, $previewTarget);
+
+        appendPointer($previewTarget);
+
+        $previewTarget.css('display', 'inline-block');
+
+        $previewTarget.append($closeBtn).show();
+
+        $triggerBtn.html('<span class="glyphicon glyphicon-chevron-down"></span> Close');
+
+        attachPreviewEvents();
+
+        $triggerBtn.addClass('preview-open');
+      }
+
+
+      function appendPreviewContent(url, $target) {
+        var request = $.ajax({
+          url: url,
+          type: 'GET',
+          dataType: 'html'
+        });
+
+        request.done(function(html) {
+          $target.append($('<div class="preview-content"></div>').append(html));
+        });
+
+        request.fail(function(jqXhr, textStatus) {
+          console.log('GET request for ' + url + ' failed: ' + textStatus);
+        });
+      }
+
+
+      function appendPointer($target) {
+        $target.append($arrow);
+
+        maxLeft = $target.width() - $arrow.width() - 1,
+        arrowLeft = parseInt($item.position().left + ($item.width()/2) - 20);
+
+        if (arrowLeft < 0) arrowLeft = 0;
+        if (arrowLeft > maxLeft) arrowLeft = maxLeft;
+
+        $arrow.css('left', arrowLeft);
+      }
+
+
+      function attachTriggerEvents() {
+        $item.find($triggerBtn).on('click', $.proxy(function(e) {
+          if (previewOpen()){
+            closePreview();
+          }else{
+            showPreview();
+          }
+        }, this));
+
+        $("#documents").on('click', $.proxy(function(e) {
+          if (!currentPreview(e)){
+              closePreview();
+          }
+
+        }, this));
+      }
+
+      function currentPreview(e){
+        if (e.target === $triggerBtn[0]){
+          return true;
+        }else{
+          return false;
+        }
+      }
+
+      function previewOpen(){
+        if ($triggerBtn.hasClass('preview-open')){
+          return true;
+        }else{
+          return false;
+        }
+      }
+
+
+
+      function attachPreviewEvents() {
+        $previewTarget.find($closeBtn).on('click', $.proxy(function() {
+          closePreview();
+        }, this));
+      }
+
+      function closePreview() {
+        $previewTarget.removeClass('preview');
+        $triggerBtn.removeClass('preview-open');
+        $previewTarget.hide();
+        $triggerBtn.html('<span class="glyphicon glyphicon-chevron-right"></span> Preview');
+      }
+
+      function itemsPerRow() {
+        var width = $('#documents').width();
+        return  Math.floor(width/$itemWidth);
+      }
+
+      function reorderPreviewDivs() {
+        var previewDivs = $('.preview-container');
+        var previewIndex = previewDivs.index($previewTarget) + 1;
+        $itemsPerRow = itemsPerRow();
+        if (previewIndex % $itemsPerRow !== 0){
+          while (previewIndex % $itemsPerRow !== 0){
+            previewIndex++;
+          }
+          if (previewIndex > previewDivs.length){
+            previewIndex = previewDivs.length;
+          }
+          var detachedPreview = $previewTarget.detach();
+          $($gallery[(previewIndex-1)]).after($previewTarget);
+        }
+      }
+
+
+      function init() {
+        $itemWidth = $item.outerWidth() + 10;
+        $triggerBtn = $item.find('*[data-behavior="preview-button-trigger"]');//$('<div/>').addClass('preview-trigger-btn preview-opacity').html('<span class="glyphicon glyphicon-chevron-down small">');
+        $closeBtn = $('<a class="preview-close"><span class="glyphicon glyphicon-remove"></span></a>');
+        $arrow = $('<div class="preview-arrow"></div>');
+        $gallery = $('.gallery-document');
+        reorderPreviewDivs();
+      }
+
+    });
+
+  };
+
+})(jQuery);
+
+
+Blacklight.onLoad(function() {
+  $('*[data-behavior="gallery-preview"]').galleryPreview();
+});
--- spec/features/gallery_view_spec.rb
+++ spec/features/gallery_view_spec.rb
@@ -6,10 +6,13 @@ feature "Gallery View" do
     click_link "Gallery"
     expect(page).to have_css("span.glyphicon.glyphicon-gallery.view-icon-gallery")
     expect(page).to have_css("span.caption", text: "Gallery")
-    expect(page).to have_css(".document a span.fake-cover", text: "An object", visible: true)
-    expect(page).to_not have_css(".document a div.fake-cover-text", text: "Car : a drama of the American workplace", visible: true)
-    expect(page).to have_css(".document h5.index_title", text: "An object")
-    expect(page).to have_css(".document button.btn-primary", text: "Preview")
+    expect(page).to have_css(".gallery-document a span.fake-cover", text: "An object", visible: true)
+    expect(page).to_not have_css(".gallery-document a div.fake-cover-text", text: "Car : a drama of the American workplace", visible: true)
+    expect(page).to have_css(".gallery-document h5.index_title", text: "An object")
+    expect(page).to have_css(".gallery-document button.btn-primary", text: "Preview")
     expect(page).to have_css("form.bookmark_toggle label.toggle_bookmark", text: "Select")
+    page.first(".docid-1").click
+    expect(page).to have_css("dt", text: "Language:")
+    expect(page).to have_css("dd", text: "English.")
   end
 end
