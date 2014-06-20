e644e095e5d7aebd692c08f133f49b35b74d6c7d
Jessie Keck <jessie.keck@gmail.com>
2014-06-19 21:12:33 -0700

    Add partial to display location (holdings) information on search results.

--- spec/lib/holdings/location_spec.rb
+++ spec/lib/holdings/location_spec.rb
@@ -7,4 +7,21 @@ describe Holdings::Location do
   it "should identify location level requests" do
     expect(Holdings::Location.new("SSRC-DATA")).to be_location_level_request
   end
+  describe "enumeration helpers" do
+    let(:none) { Holdings::Location.new("STACKS") }
+    let(:one) { Holdings::Location.new("STACKS", ["something"]) }
+    let(:multiple) { Holdings::Location.new("STACKS", ["something", "else"]) }
+    it "should not return true if there are no items" do
+      expect(none).to_not be_one_item
+      expect(none).to_not be_more_than_one_item
+    end
+    it "should return true for a single item" do
+      expect(one).to be_one_item
+      expect(one).to_not be_more_than_one_item
+    end
+    it "should return true for multiple items" do
+      expect(multiple).to_not be_one_item
+      expect(multiple).to be_more_than_one_item
+    end
+  end
 end
