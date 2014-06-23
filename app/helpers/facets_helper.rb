b0f4d01a789a6dee959ef2955507bfd54fdfd17b
Jack Reed <pjreed@stanford.edu>
2014-06-23 16:32:19 -0700

    adds shim for ie8 support and overwrites unneeded helper method

--- app/helpers/facets_helper.rb
+++ app/helpers/facets_helper.rb
@@ -9,4 +9,22 @@ module FacetsHelper
       render_facet_limit(facet_by_field_name(facet_name), options)
     end
   end
+
+  include BlacklightRangeLimit::RangeLimitHelper
+  def add_range_missing(solr_field, my_params = params)
+    my_params = Marshal.load(Marshal.dump(my_params))
+    my_params["range"] ||= {}
+
+    # Commented out dummy range and other lines not needed for SearchWorks and are
+    # causing errors when
+    # my_params["range"][solr_field] ||= {}
+    # my_params["range"][solr_field]["missing"] = "true"
+
+    # Need to ensure there's a search_field to trick Blacklight
+    # into displaying results, not placeholder page. Kind of hacky,
+    # but works for now.
+    # my_params["search_field"] ||= "dummy_range"
+
+    my_params
+  end
 end
