c4a29548d1264e9399986a6f9bae8c9eabd57824
Jessie Keck <jessie.keck@gmail.com>
2014-06-27 13:09:11 -0700

    Change Online accordion section in search results to use online access panel link logic.

--- spec/views/catalog/_accordion_section_online.html.erb_spec.rb
+++ spec/views/catalog/_accordion_section_online.html.erb_spec.rb
@@ -9,7 +9,8 @@ describe "catalog/_accordion_section_online.html.erb" do
       assign(:document,
         SolrDocument.new(
           id: "12345",
-          isbn_display: [ 123 ]
+          isbn_display: [ 123 ],
+          marcbib_xml: stanford_only_856
         )
       )
       render
@@ -20,6 +21,9 @@ describe "catalog/_accordion_section_online.html.erb" do
       expect(rendered).to have_css('.accordion-section.online a.header', text: "Online")
       expect(rendered).to have_css('.details .google-books')
       expect(rendered).to have_css('.details a', text: "Google Books Full view")
+    end
+    it "should include the links" do
+      expect(rendered).to have_css('span.stanford-only a', text: 'Link text', count: 4)
     end
   end
 
