7f9259a88fc917aebe8b8e2424c4450faf35a910
Jack Reed <pjreed@stanford.edu>
2014-07-03 16:35:52 -0400

    overwrite and add in default bootstrap button styles

--- spec/features/gallery_view_spec.rb
+++ spec/features/gallery_view_spec.rb
@@ -13,7 +13,7 @@ feature "Gallery View" do
     expect(page).to have_css(".gallery-document a[tabindex='-1'] span.fake-cover", text: "An object", visible: true)
     expect(page).to_not have_css(".gallery-document a div.fake-cover-text", text: "Car : a drama of the American workplace", visible: true)
     expect(page).to have_css(".gallery-document h5.index_title", text: "An object")
-    expect(page).to have_css(".gallery-document button.btn-primary", text: "Preview")
+    expect(page).to have_css(".gallery-document button.btn-preview", text: "Preview")
     expect(page).to have_css("form.bookmark_toggle label.toggle_bookmark", text: "Select")
 
     page.first("button.btn.docid-1").click
