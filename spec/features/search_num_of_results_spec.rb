3acab9d6cc9dc87fe18c39dddb4a2531e9c9a48c
Christopher Jesudurai <jchris@stanford.edu>
2014-07-03 12:29:12 -0700

    Adjust heading levels and style changes

---
+++ spec/features/search_num_of_results_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+feature "Search results count" do
+  scenario "should display number of results header" do
+    visit catalog_index_path f: { format: ["Book"] }
+    within "#content" do
+      within ".search_num_of_results" do
+        expect(page).to have_css("h2", text: /[\d,]+ results?/)
+      end
+    end
+  end
+end
