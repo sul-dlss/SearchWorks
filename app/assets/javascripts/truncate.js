07c2dff100ca2817a6a80419aed61389dfa17df5
Christopher Jesudurai <jchris@stanford.edu>
2014-07-07 13:39:03 -0700

    Fix for gallery title overflow

--- app/assets/javascripts/truncate.js
+++ app/assets/javascripts/truncate.js
@@ -1,6 +1,7 @@
 Blacklight.onLoad(function(){
   $("[data-behavior='truncate']").responsiveTruncate({height: 60});
   $("[data-behavior='trunk8']").trunk8();
+  $(".gallery-document h3.index_title").trunk8({ lines: 4 });
 });
 
 $(window).resize(function() {
