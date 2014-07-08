0a70b13329e756db5fa7703d68ccd982c8fe64f9
Christopher Jesudurai <jchris@stanford.edu>
2014-07-07 22:47:29 -0700

    Add 'skip nav' links to pages

---
+++ spec/features/skip_to_nav_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_helper'
+
+feature "Skip-to Navigation" do
+
+  scenario "should have skip-to navigation links to search field and main container in home page" do
+    visit root_url
+    within "#skip-link" do
+      expect(page).to have_css("a[href='#search_field']", text: "Skip to search")
+      expect(page).to have_css("a[href='#main-container']", text: "Skip to main content")
+    end
+  end
+
+  scenario "should have skip-to navigation links to search field, main container and records in results page" do
+    visit root_url
+    fill_in "q", with: "20"
+    click_button 'search'
+
+    within "#skip-link" do
+      expect(page).to have_css("a[href='#search_field']", text: "Skip to search")
+      expect(page).to have_css("a[href='#main-container']", text: "Skip to main content")
+      expect(page).to have_css("a[href='#documents']", text: "Skip to first result")
+    end
+  end
+
+  scenario "should have skip-to navigation links to search field, main container and records in selections page", js: true do
+    visit root_path
+    fill_in 'q', with: '20'
+    click_button 'search'
+    find(:css, '#toggle_bookmark_20').set(true)
+    visit selections_path
+
+    within "#skip-link" do
+      expect(page).to have_css("a[href='#search_field']", text: "Skip to search")
+      expect(page).to have_css("a[href='#main-container']", text: "Skip to main content")
+      expect(page).to have_css("a[href='#documents']", text: "Skip to first result")
+    end
+  end
+
+  scenario "should have skip-to navigation links to search field, main container and records in record view page" do
+    visit catalog_path 20
+
+    within "#skip-link" do
+      expect(page).to have_css("a[href='#search_field']", text: "Skip to search")
+      expect(page).to have_css("a[href='#document']", text: "Skip to main content")
+    end
+  end
+
+  scenario "should have skip-to navigation links to form in advanced search page" do
+    visit advanced_search_path
+
+    within "#skip-link" do
+      expect(page).to have_css("a[href='#advanced-search-form']", text: "Skip to advanced search form")
+    end
+  end
+
+end
