8f7aa13a62b44e2ba976d50d144fd86146cccb66
Jessie Keck <jessie.keck@gmail.com>
2014-07-15 11:50:12 -0700

    Change course reserve masthead markup to be dl instead of p.

--- spec/features/access_points/course_reserve_spec.rb
+++ spec/features/access_points/course_reserve_spec.rb
@@ -8,8 +8,10 @@ feature "Course Reserve Access Point" do
     expect(page).to have_title("Course reserves in SearchWorks")
     within("#masthead") do
       expect(page).to have_css("h1", text: "Course reserve list: CAT-401-01-01")
-      expect(page).to have_css("p", text: "CAT-401-01-01 -- Emergency Kittenz")
-      expect(page).to have_css("p", text: "INSTRUCTOR: McDonald, Ronald")
+      expect(page).to have_css("dt", text: "Instructor")
+      expect(page).to have_css("dd", text: "McDonald, Ronald")
+      expect(page).to have_css("dt", text: "Course")
+      expect(page).to have_css("dd", text: "CAT-401-01-01 -- Emergency Kittenz")
     end
     within("#content") do
       expect(page).to have_css("div.document", count:1)
