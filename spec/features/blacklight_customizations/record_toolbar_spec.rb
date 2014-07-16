3d1d450d77ff79f966e599672fa10b67a6078ea0
Jack Reed <pjreed@stanford.edu>
2014-07-16 15:10:22 -0400

    removes duplicate links and styles record toolbar

--- spec/features/blacklight_customizations/record_toolbar_spec.rb
+++ spec/features/blacklight_customizations/record_toolbar_spec.rb
@@ -2,26 +2,53 @@ require 'spec_helper'
 
 feature "Record Toolbar" do
   before do
-    visit('/view/1')
+    visit root_path
   end
 
-  scenario "should have record toolbar visible", js: true do
+  scenario "should have record toolbar visible but no back to search or pagination", js: true do
+    visit '/view/1'
     within "#content" do
-      expect(page).to have_css("div.record-toolbar.hidden-xs", visible: true)
-      expect(page).to have_css("div.record-toolbar.hidden-sm.hidden-md.hidden-lg", visible: false)
+      expect(page).to have_css("div.record-toolbar", visible: true)
 
-      within "div.record-toolbar.hidden-xs" do
-        expect(page).to have_css("a.btn.btn-default", text:"Back to results", visible: true)
-        expect(page).to have_css("a.previous.disabled", visible: true)
-        expect(page).to have_css("a.previous", visible: true)
-
-        expect(page).to have_css("div.record-toolbar-tools", visible: true)
-        within "div.record-toolbar-tools" do
-          expect(page).to have_css("button.btn.btn-default", text:"Send to", visible: true)
-        end
+      within "div.navbar-header" do
+        expect(page).to_not have_css("button.navbar-toggle", visible: true)
+        expect(page).to_not have_css("a.btn.btn-sul-toolbar", text:"Back to results", visible: true)
+        expect(page).to_not have_css("a.previous.disabled", visible: true)
+        expect(page).to_not have_css("a.previous", visible: true)
+        expect(page).to_not have_css("a.next.disabled", visible: true)
+        expect(page).to_not have_css("a.next", visible: true)
       end
 
+      within "div.navbar-collapse" do
+        expect(page).to have_css("li a", text: "Cite")
+        expect(page).to have_css("li button", text: "Send to")
+        expect(page).to have_css("form label", text: "Select")
+      end
     end
   end
 
+  scenario "should have back to search and pagination", js: true do
+    visit catalog_index_path f: {format: ["Book"]}
+    page.find("a", text: "An object").click
+
+    within "#content" do
+      within "div.navbar-header" do
+        expect(page).to_not have_css("button.navbar-toggle", visible: true)
+        expect(page).to have_css("a.btn.btn-sul-toolbar", text:"Back to results", visible: true)
+        expect(page).to have_css("a.previous", visible: true)
+        expect(page).to have_css("a.next", visible: true)
+      end
+      within "div.navbar-collapse" do
+        expect(page).to have_css("li a", text: "Cite")
+        expect(page).to have_css("li button", text: "Send to")
+        expect(page).to have_css("form label", text: "Select")
+        click_button "Send to"
+        expect(page).to have_css("li a", text: "text")
+        expect(page).to have_css("li a", text: "email")
+        expect(page).to have_css("li a", text: "RefWorks")
+        expect(page).to have_css("li a", text: "EndNote")
+        expect(page).to have_css("li a", text: "printer")
+      end
+    end
+  end
 end
