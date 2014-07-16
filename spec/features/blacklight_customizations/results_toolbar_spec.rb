d1828b4588606194111dffe281090351e4176e8b
Jessie Keck <jessie.keck@gmail.com>
2014-07-15 17:21:49 -0700

    Style results toolbar.

--- spec/features/blacklight_customizations/results_toolbar_spec.rb
+++ spec/features/blacklight_customizations/results_toolbar_spec.rb
@@ -11,13 +11,13 @@ feature "Results Toolbar", js: true do
   scenario "should have desktop tools visible" do
     within "#sortAndPerPage" do
       within "div.page_links" do
-        expect(page).to have_css("a.btn.btn-sul-toolbar.disabled", text: /PREVIOUS/, visible: true)
+        expect(page).to have_css("a.btn.btn-sul-toolbar.disabled", text: /Previous/, visible: true)
         expect(page).to have_css("span.page_entries", text: /1 - 10/, visible: true)
-        expect(page).to have_css("a.btn.btn-sul-toolbar", text: /NEXT/, visible: true)
+        expect(page).to have_css("a.btn.btn-sul-toolbar", text: /Next/, visible: true)
       end
       expect(page).to have_css("div#view-type-dropdown a")
-      expect(page).to have_css("div#sort-dropdown", text: "SORT BY RELEVANCE", visible: true)
-      expect(page).to have_css("#select_all-dropdown", text: "SELECT ALL")
+      expect(page).to have_css("div#sort-dropdown", text: "Sort by relevance", visible: true)
+      expect(page).to have_css("#select_all-dropdown", text: "Select all")
       expect(page).to_not have_css("a", text: /Cite/)
       expect(page).to_not have_css("button", text: /Send/)
     end
