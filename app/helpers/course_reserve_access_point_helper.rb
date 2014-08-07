1b3afc1b41619e27a176af2cfc86a8ab02f7dc30
Jessie Keck <jessie.keck@gmail.com>
2014-08-07 16:39:49 -0700

    Loop through all the documents in the response to find the selected course reserve in case it doesn't exist in the first document.

--- app/helpers/course_reserve_access_point_helper.rb
+++ app/helpers/course_reserve_access_point_helper.rb
@@ -1,10 +1,16 @@
 module CourseReserveAccessPointHelper
 
   def create_course
-    if @response.docs.first.present?
-      # Get the first response doc and return it's course reserve info that matches facet params
-      courses = @response.docs.first[:crez_course_info].map { |c| CourseReserves::CourseInfo.new(c)}
-      @course_info = courses.find { |c| c.id == params[:f][:course][0] && c.instructor == params[:f][:instructor][0] }
+    if @response.docs.present?
+      # Find the document in the response that contains the requested course
+      # and return it's course reserve info that matches facet params.
+      @course_info = @response.docs.map do |document|
+        document[:crez_course_info].map do |course|
+          CourseReserves::CourseInfo.new(course)
+        end.find do |course|
+          course.id == params[:f][:course][0] && course.instructor == params[:f][:instructor][0]
+        end
+      end.compact.first
     else
       # If no docs match the search params, return some course info though it will be missing course name
       @course_info = CourseReserves::CourseInfo.new("#{params[:f][:course][0]}-|--|-#{params[:f][:instructor][0]}")
