301b40f5480004b654ba59b68f1058ff740e61eb
Jack Reed <pjreed@stanford.edu>
2014-08-20 14:06:32 -0700

    adds ask a librarian functionality

---
+++ spec/views/shared/feedback_forms/_chat_with_librarian.html.erb_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe 'shared/feedback_forms/_chat_with_librarian.html.erb' do
+  before do
+    render
+  end
+  it 'should have correct data attributes' do
+    expect(rendered).to have_css('[data-jid="ic@chat.libraryh3lp.com"]')
+    expect(rendered).to have_css('[data-library-h3lp]')
+  end
+end
