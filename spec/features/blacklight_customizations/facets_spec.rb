50676ec39a87ff782c5fe0abd69cb3a247b74ada
Jessie Keck <jessie.keck@gmail.com>
2014-09-04 15:25:55 -0700

    Use Blacklight::AdvancedSearch from master.

--- spec/features/blacklight_customizations/facets_spec.rb
+++ spec/features/blacklight_customizations/facets_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 feature "Facets Customizations" do
-  scenario "material type icons should display" do
+  scenario "material type icons should display on the home page" do
     visit root_path
 
     within(".blacklight-format_main_ssim") do
@@ -9,6 +9,19 @@ feature "Facets Customizations" do
       within("ul.facet-values") do
         expect(page).to have_css("li span.sul-icon")
       end
+    end
+  end
+  scenario "material type icons should display after an advanced search", js: true do
+    visit advanced_search_path
+
+    click_on "Resource type"
+    check "f_inclusive_format_main_ssim_book"
+
+    click_on "advanced-search-submit"
+
+    within(".blacklight-format_main_ssim") do
+      expect(page).to have_css(".panel-title", text: "Resource type")
+      expect(page).to have_css("li span.sul-icon")
     end
   end
   scenario "while not in an access point facet title should not change", js: true do
