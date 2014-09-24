d414ee2d2e084d731965fcf26f1b5fbc774811ba
Jessie Keck <jessie.keck@gmail.com>
2014-09-24 16:17:36 -0700

    Change capitalization of 'Digital collections' in masthead.

--- spec/features/access_points/digital_collections_spec.rb
+++ spec/features/access_points/digital_collections_spec.rb
@@ -7,7 +7,7 @@ describe 'Digital Collections Access Point' do
   end
   it 'should include the digital collections masthead' do
     within(".digital-collections-masthead") do
-      expect(page).to have_css('h1', text: 'Digital Collections')
+      expect(page).to have_css('h1', text: 'Digital collections')
       expect(page).to have_css('.inline-links a', text: 'data')
       expect(page).to have_css('.inline-links a', text: 'images')
       expect(page).to have_css('.inline-links a', text: 'maps')
