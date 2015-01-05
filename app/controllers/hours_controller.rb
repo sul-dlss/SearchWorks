370c8668776b4d0a12a0c1d7f8ce4b0bae2e6aad
Jessie Keck <jessie.keck@gmail.com>
2015-01-05 13:49:42 -0800

    Return error message and 400 status code when HTML is requested for JSON only APIs.

--- app/controllers/hours_controller.rb
+++ app/controllers/hours_controller.rb
@@ -3,6 +3,7 @@ class HoursController < ApplicationController
     response = HoursRequest.new(params[:id]).get
     respond_to do |format|
       format.json { render json: response }
+      format.html { render 'public/500', layout: false, status: 400 }
     end
   end
 end
