e9e265771d404cf7f5c1beef46663f124ac40944
Jessie Keck <jessie.keck@gmail.com>
2015-01-05 13:15:26 -0800

    Add google site verification code for Webmaster Tools.

--- spec/features/blacklight_customizations/layout_spec.rb
+++ spec/features/blacklight_customizations/layout_spec.rb
@@ -1,8 +1,13 @@
 require "spec_helper"
 
 describe 'Customized Layout' do
-  it 'should include a base64 encoded string in a meta tag' do
+  before do
     visit root_path
+  end
+  it 'should include a base64 encoded string in a meta tag' do
     expect(page).to have_css('meta[name="application-name"][value="U2VhcmNoV29ya3M="]', visible: false)
+  end
+  it 'should include the google-site-verification code' do
+    expect(page).to have_css("meta[name='google-site-verification'][content='#{Settings.GOOGLE_SITE_VERIFICATION}']", visible: false)
   end
 end
