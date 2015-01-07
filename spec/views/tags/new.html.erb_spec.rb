41bd6af05caf99829e1197cafb1e571bb020174c
Naomi Dushay <ndushay@stanford.edu>
2015-01-07 10:48:46 -0800

    specs for square brackets not appearing in new Tag form

--- spec/views/tags/new.html.erb_spec.rb
+++ spec/views/tags/new.html.erb_spec.rb
@@ -2,14 +2,24 @@ require 'spec_helper'
 
 describe "tags/new" do
   before(:each) do
-    assign(:tag, stub_model(Tag).as_new_record)
+    assign(:tag, Tag.new({}))
   end
 
   it "renders new tag form" do
     render
-
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form[action=?][method=?]", tags_path, "post" do
     end
+  end
+  
+  it "doesn't have square brackets in the hasTarget text field" do
+    render
+    expect(rendered).to have_css('input#tag_hasTarget_id', text: '')
+    expect(rendered).not_to have_css('input#tag_hasTarget_id', text: '[]')
+  end
+  
+  it "doesn't have square brackets in the hasBody text area" do
+    render
+    expect(rendered).to have_css('textarea#tag_hasBody_id', text: '')
+    expect(rendered).not_to have_css('textarea#tag_hasBody_id', text: '[]')
   end
 end
