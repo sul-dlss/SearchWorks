9938fb2c0d611c41ca98f5f640a84c67a2b27e99
Naomi Dushay <ndushay@stanford.edu>
2015-01-08 11:29:34 -0800

    fix view specs

--- spec/views/tags/index.html.erb_spec.rb
+++ spec/views/tags/index.html.erb_spec.rb
@@ -3,13 +3,14 @@ require 'spec_helper'
 describe "tags/index" do
   before(:each) do
     assign(:tags, [
-      stub_model(Tag),
-      stub_model(Tag)
+      Tag.new({}),
+      Tag.new({})
     ])
   end
 
   it "renders a list of tags" do
     render
+    pending "test needs to be written"
     # Run the generator again with the --webrat flag if you want to use webrat matchers
   end
 end
--- spec/views/tags/show.html.erb_spec.rb
+++ spec/views/tags/show.html.erb_spec.rb
@@ -2,11 +2,12 @@ require 'spec_helper'
 
 describe "tags/show" do
   before(:each) do
-    @tag = assign(:tag, stub_model(Tag))
+    @tag = assign(:tag,  Tag.new({}))
   end
 
   it "renders attributes in <p>" do
     render
+    pending "test needs to be written"
     # Run the generator again with the --webrat flag if you want to use webrat matchers
   end
 end
