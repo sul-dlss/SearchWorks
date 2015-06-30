1706d31e958bf0f601ec13d4c1cb5c2ba994a1cb
Naomi Dushay <ndushay@stanford.edu>
2015-06-30 16:59:16 -0700

    anno helper methods tag_annos and comment_annos added

--- app/helpers/annotations_helper.rb
+++ app/helpers/annotations_helper.rb
@@ -7,4 +7,17 @@ module AnnotationsHelper
       motivations.map! { |u| u.to_s.sub(RDF::Vocab::OA.to_uri.to_s, '')}
     end
   end
+
+  # @return [Array<LD4L::OpenAnnotationRDF::TagAnnotation>] Array containing only the TagAnnotation objects from @annotations
+  def tag_annos
+    return [] unless @annotations.present?
+    @annotations.select { |anno| anno.instance_of?(LD4L::OpenAnnotationRDF::TagAnnotation) }
+  end
+
+  # @return [Array<LD4L::OpenAnnotationRDF::CommentAnnotation>] Array containing only the CommentAnnotation objects from @annotations
+  def comment_annos
+    return [] unless @annotations.present?
+    @annotations.select { |anno| anno.instance_of?(LD4L::OpenAnnotationRDF::CommentAnnotation) }
+  end
+
 end
--- spec/helpers/annotations_helper_spec.rb
+++ spec/helpers/annotations_helper_spec.rb
@@ -16,4 +16,66 @@ describe AnnotationsHelper, annos: true do
       expect(oa_motivations).to include "tagging"
     end
   end
+
+  context "#tag_annos" do
+    it 'returns Array of LD4L::OpenAnnotationRDF::TagAnnotation objects contained in passed Array' do
+      annos = [LD4L::OpenAnnotationRDF::Annotation.new,
+                LD4L::OpenAnnotationRDF::CommentAnnotation.new,
+                LD4L::OpenAnnotationRDF::TagAnnotation.new,
+                LD4L::OpenAnnotationRDF::CommentAnnotation.new,
+                LD4L::OpenAnnotationRDF::TagAnnotation.new]
+      assign(:annotations, annos)
+      tag_annos = helper.tag_annos
+      expect(tag_annos).to be_an Array
+      expect(tag_annos.size).to eq 2
+      tag_annos.each { |a| expect(a).to be_a LD4L::OpenAnnotationRDF::TagAnnotation }
+    end
+    it 'returns empty Array if there are no LD4L::OpenAnnotationRDF::TagAnnotation objects in the passed Array' do
+      annos = [LD4L::OpenAnnotationRDF::Annotation.new,
+                LD4L::OpenAnnotationRDF::CommentAnnotation.new,
+                LD4L::OpenAnnotationRDF::CommentAnnotation.new,
+                LD4L::OpenAnnotationRDF::Annotation.new]
+      assign(:annotations, annos)
+      expect(helper.tag_annos).to eq []
+    end
+    it 'returns an empty Array if it is passed an empty Array' do
+      assign(:annotations, [])
+      expect(helper.tag_annos).to eq []
+    end
+    it 'returns empty Array if it is passed nil' do
+      assign(:annotations, nil)
+      expect(helper.tag_annos).to eq []
+    end
+  end
+
+  context "#comment_annos" do
+    it 'returns Array of LD4L::OpenAnnotationRDF::CommentAnnotation objects contained in passed Array' do
+      annos = [LD4L::OpenAnnotationRDF::Annotation.new,
+                LD4L::OpenAnnotationRDF::CommentAnnotation.new,
+                LD4L::OpenAnnotationRDF::TagAnnotation.new,
+                LD4L::OpenAnnotationRDF::CommentAnnotation.new,
+                LD4L::OpenAnnotationRDF::TagAnnotation.new]
+      assign(:annotations, annos)
+      comment_annos = helper.comment_annos
+      expect(comment_annos).to be_an Array
+      expect(comment_annos.size).to eq 2
+      comment_annos.each { |a| expect(a).to be_a LD4L::OpenAnnotationRDF::CommentAnnotation }
+    end
+    it 'returns empty Array if there are no LD4L::OpenAnnotationRDF::CommentAnnotation objects in the passed Array' do
+      annos = [LD4L::OpenAnnotationRDF::Annotation.new,
+                LD4L::OpenAnnotationRDF::TagAnnotation.new,
+                LD4L::OpenAnnotationRDF::Annotation.new]
+      assign(:annotations, annos)
+      expect(helper.comment_annos).to eq []
+    end
+    it 'returns an empty Array if it is passed an empty Array' do
+      assign(:annotations, [])
+      expect(helper.comment_annos).to eq []
+    end
+    it 'returns empty Array if it is passed nil' do
+      assign(:annotations, nil)
+      expect(helper.comment_annos).to eq []
+    end
+  end
+
 end
