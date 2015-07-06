e2cd6f66bc0ffa6954c8bb641e01ae9769372e6e
Jessie Keck <jessie.keck@gmail.com>
2015-07-06 13:13:25 -0700

    Update UAT deploy environment to match new sandbox VM structure.

--- config/deploy/uat.rb
+++ config/deploy/uat.rb
@@ -1,5 +1,6 @@
 set :deploy_host, ask("Server", 'e.g. server.stanford.edu')
 set :bundle_without, %w{sqlite development test}.join(' ')
+set :deploy_to, "/opt/app/#{fetch(:user)}/sw-webapp"
 
 server fetch(:deploy_host), user: fetch(:user), roles: %w{web db app}
 
