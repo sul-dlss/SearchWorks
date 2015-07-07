a54b57d005b3b0d251a023627336ab7259bb5167
Jessie Keck <jessie.keck@gmail.com>
2015-07-07 12:05:05 -0700

    Update existing request link code to be....better.

--- app/helpers/request_link_helper.rb
+++ app/helpers/request_link_helper.rb
@@ -1,23 +1,48 @@
 module RequestLinkHelper
   def request_link(document, callnumber)
-    request_query = request_options(document, callnumber)
-    request_query.delete(:item_id) if Holdings::Library.new(callnumber.library, document).location_level_request?
-    if callnumber.on_order?
-      request_query.delete(:item_id)
-      request_query.delete(:home_lib)
-    end
     if callnumber.home_location == 'SSRC-DATA'
-      "#{Settings.SSRC_REQUESTS_URL}?authid=&unicorn_id_in=#{CGI::escape(document[:id])}&title_in=#{CGI::escape(presenter(document).document_heading)}&icpsr_no_in=&call_no_in=#{CGI::escape(callnumber.callnumber)}"
+      request_params = ssrc_params(document, callnumber)
+      "#{Settings.SSRC_REQUESTS_URL}?#{request_params.to_query}"
     else
-      "#{Settings.REQUESTS_URL}?#{request_query.to_query}"
+      request_params = process_request_params(document, callnumber)
+      "#{Settings.REQUESTS_URL}?#{request_params.to_query}"
     end
   end
+
   private
+
+  def process_request_params(document, callnumber)
+    request_params = request_options(document, callnumber)
+    request_params.delete(:item_id) if Holdings::Library.new(callnumber.library, document).location_level_request?
+    if callnumber.on_order?
+      request_params.delete(:item_id)
+      request_params.delete(:home_lib)
+    end
+    request_params
+  end
+
+  def ssrc_params(document, callnumber)
+    {
+      authid: '',
+      unicorn_id_in: document[:id],
+      title_in: presenter(document).document_heading,
+      icpsr_no_in: '',
+      call_no_in: callnumber.callnumber
+    }
+  end
+
   def request_options(document, callnumber)
-    {ckey:         CGI::escape(document[:id]),
-     item_id:      CGI::escape(callnumber.barcode),
-     home_lib:     CGI::escape(callnumber.library),
-     home_loc:     CGI::escape(callnumber.home_location),
-     current_loc:  CGI::escape(callnumber.current_location.code.present? ? callnumber.current_location.code : callnumber.home_location)}
+    {
+      ckey:         document[:id],
+      item_id:      callnumber.barcode,
+      home_lib:     callnumber.library,
+      home_loc:     callnumber.home_location,
+      current_loc:  current_or_home_location(callnumber)
+    }
+  end
+
+  def current_or_home_location(callnumber)
+    return callnumber.current_location.code if callnumber.current_location.code.present?
+    callnumber.home_location
   end
 end
