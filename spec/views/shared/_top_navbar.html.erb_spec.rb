c18c281e5c397a890f0dc00d2ccbcfdeea9f82f7
Jessie Keck <jessie.keck@gmail.com>
2015-07-10 12:03:41 -0700

    Implement devise-remote-users to integrate webauth and devise.

---
+++ spec/views/shared/_top_navbar.html.erb_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe 'shared/_top_navbar.html.erb' do
+  let(:user) { double('user') }
+  before do
+    allow(view).to receive(:current_user).and_return(user)
+  end
+  describe 'when there is a current user' do
+    before do
+      allow(user).to receive(:sunet).and_return('jstanford')
+      render
+    end
+
+    it 'has a logout link' do
+      expect(rendered).to have_link('Logout: jstanford')
+    end
+  end
+
+  describe 'when there is no current user' do
+    let(:user) { nil }
+    it 'has a login link' do
+      render
+      expect(rendered).to have_link('Login')
+    end
+  end
+end
