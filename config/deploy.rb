27bbb12a224ba44a13e2d5efc1622433907c415a
Naomi Dushay <ndushay@stanford.edu>
2015-07-21 11:24:19 -0700

    add public/.htaccess to linked files

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :log_level, :info
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml config/solr.yml config/secrets.yml config/schedule.rb public/robots.txt}
+set :linked_files, %w{config/database.yml config/solr.yml config/secrets.yml config/schedule.rb public/robots.txt public/.htaccess}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{config/settings log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
