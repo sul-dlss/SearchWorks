2e94aeadb6685e2d8cc76fc4d9169369a8f55009
Jessie Keck <jessie.keck@gmail.com>
2016-01-28 21:50:24 -0800

    Move all MARC and MODS contributors to the Authors/Contributors section.

--- spec/views/catalog/record/_mods_contributors.html.erb_spec.rb
+++ spec/views/catalog/record/_mods_contributors.html.erb_spec.rb
@@ -8,9 +8,14 @@ describe "catalog/record/_mods_contributors.html.erb" do
       assign(:document, SolrDocument.new(modsxml: mods_everything))
       render
     end
+
+    it 'should display primary authors' do
+      expect(rendered).to have_css('dt', text: 'Author/Creator')
+      expect(rendered).to have_css('dd', text: 'J. Smith')
+    end
+
     it "should display secondary authors" do
       expect(rendered).to have_css("dt", text: "Contributor")
-      expect(rendered).to have_css('dd', count: 1)
       expect(rendered).to have_css("dd a", text: "B. Smith")
       expect(rendered).to have_css("dd", text: /\(Producer\)/)
     end
--- spec/views/catalog/record/_mods_upper_metadata_items.html.erb_spec.rb
+++ spec/views/catalog/record/_mods_upper_metadata_items.html.erb_spec.rb
@@ -8,11 +8,6 @@ describe "catalog/record/_mods_upper_metadata_items.html.erb" do
     before do
       assign(:document, document)
     end
-    it "should display name" do
-      render
-      expect(rendered).to have_css("dt", text: "Author/Creator")
-      expect(rendered).to have_css("dd", text: "J. Smith")
-    end
     it "should display type" do
       render
       expect(rendered).to have_css("dt", text: "Type of resource")
