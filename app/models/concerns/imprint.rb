95a0798dadfb7a4d81c63ee71776e2ac1e7bc363
Jessie Keck <jessie.keck@gmail.com>
2016-02-02 15:19:35 -0800

    Replace old MARC Imprint parsing with new subclass of MarcField.

--- app/models/concerns/imprint.rb
+++
@@ -1,9 +0,0 @@
-module Imprint
-  def imprint
-    @imprint ||= begin
-      if self.respond_to?(:to_marc) && (marc_imprint = MarcImprint.new(self.to_marc)).present?
-        marc_imprint
-      end
-    end
-  end
-end
---
+++ app/models/concerns/marc_imprint.rb
@@ -0,0 +1,7 @@
+##
+# Simple mixin to add return an Imprint statement from MARC
+module MarcImprint
+  def imprint
+    @imprint = Imprint.new(self)
+  end
+end
