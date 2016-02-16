07dcaa878e9c00d3ec258e417c74bfc53999e2ee
Jessie Keck <jessie.keck@gmail.com>
2016-02-16 14:53:02 -0800

    Add set behavior to support virtual objects.

---
+++ app/models/concerns/solr_set.rb
@@ -0,0 +1,38 @@
+##
+# A mixin to add set member behavior to SolrDocuments.
+# Items that are in a set identify their set membership
+# by indexing their set's identifier in the set field.
+module SolrSet
+  def set_member?
+    self[:set].present?
+  end
+
+  def parent_sets
+    return unless set_member?
+    @parent_sets ||= set_document_list.map do |doc|
+      SolrDocument.new(doc)
+    end
+  end
+
+  # Used for generating simple title links to the parent sets w/o making a Solr request
+  def index_parent_sets
+    return unless set_member? && self[:set_with_title].present?
+    @index_parent_sets ||= self[:set_with_title].map do |set_with_title|
+      id, title = set_with_title.split('-|-').map(&:strip)
+      SolrDocument.new(id: id, title_display: title)
+    end
+  end
+
+  private
+
+  def set_document_list
+    @document_list ||= Blacklight.solr.select(set_solr_params)['response']['docs']
+  end
+
+  def set_solr_params
+    ids = self['set'].map do |set_id|
+      "id:#{set_id}"
+    end.join(' OR ')
+    { params: { fq: ids } }
+  end
+end
---
+++ spec/models/concerns/solr_set_spec.rb
@@ -0,0 +1,55 @@
+require 'spec_helper'
+
+describe SolrSet do
+  let(:solr_data) { { id: '12345', set: ['set1'], set_with_title: ['set1 -|- SetABC'] } }
+  let(:documents) { [{ id: 'set1', title_display: 'SetABC' }] }
+  subject { SolrDocument.new(solr_data) }
+
+  before do
+    allow(subject).to receive_messages(set_document_list: documents)
+  end
+
+  context 'when an item is a member of a set' do
+    describe '#set_member?' do
+      it 'is true' do
+        expect(subject).to be_set_member
+      end
+    end
+
+    describe '#parent_sets' do
+      it 'returns an array of solr documents' do
+        expect(subject.parent_sets).to be_all do |set|
+          set.is_a?(SolrDocument)
+        end
+      end
+    end
+
+    describe '#index_parent_sets' do
+      before do
+        expect(Blacklight).not_to receive(:solr)
+      end
+
+      it 'returns an array of solr documents without hitting solr' do
+        expect(subject.index_parent_sets).to be_all do |set|
+          set.is_a?(SolrDocument)
+        end
+      end
+
+      it 'splits the ID and title from the set_with_title field' do
+        parent = subject.index_parent_sets.first
+        expect(parent[:id]).to eq 'set1'
+        expect(parent[:title_display]).to eq 'SetABC'
+      end
+    end
+
+    describe 'private methods' do
+      describe '#set_solr_params' do
+        before { subject[:set] << 'set2' }
+
+        it 'returns an fq with the set IDs joined with an "OR"' do
+          expect(subject.send(:set_solr_params)[:params]).to eq(fq: 'id:set1 OR id:set2')
+        end
+      end
+    end
+  end
+end
