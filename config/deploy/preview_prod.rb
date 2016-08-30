e8a7ad47a57c4949b655b3e8a81cf2c492eb1b1e
Jessie Keck <jessie.keck@gmail.com>
2016-08-30 10:15:24 -0700

    Update uat environment to separate preview prod/stage environments.

---
+++ config/deploy/preview_prod.rb
@@ -0,0 +1,7 @@
+set :bundle_without, %w(sqlite development test).join(' ')
+set :deploy_to, "/opt/app/#{fetch(:user)}/#{fetch(:application)}"
+
+server 'searchworks-preview.stanford.edu', user: fetch(:user), roles: %w(web db app)
+
+Capistrano::OneTimeKey.generate_one_time_key!
+set :rails_env, 'production'
--- config/deploy/uat.rb
+++
@@ -1,8 +0,0 @@
-set :deploy_host, ask("Server", 'e.g. server.stanford.edu')
-set :bundle_without, %w{sqlite development test}.join(' ')
-set :deploy_to, "/opt/app/#{fetch(:user)}/#{fetch(:application)}"
-
-server fetch(:deploy_host), user: fetch(:user), roles: %w{web db app}
-
-Capistrano::OneTimeKey.generate_one_time_key!
-set :rails_env, "production"
