62c70df4d547646d0a6aa5e9834c2510e678aa3e
Chris Beer <chris@cbeer.info>
2016-10-12 11:03:40 -0700

    Replace squash with honeybadger

--- config/initializers/squash_exceptions.rb
+++
@@ -1,5 +0,0 @@
-Squash::Ruby.configure :api_host => Settings.SQUASH_API_HOST,
-                       :api_key => Settings.SQUASH_API_KEY,
-                       :environment => Settings.SQUASH_ENVIRONMENT || Rails.env,
-                       :disabled => Settings.SQUASH_DISABLE,
-                       :revision_file => File.join(Rails.root, 'REVISION')
