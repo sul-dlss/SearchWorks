43e23c20e3d2d00e131e309b299c5c0331b0cc29
Jessie Keck <jessie.keck@gmail.com>
2017-06-06 13:30:44 -0700

    Move index/marc language processing to a new Language MarcField subclass.

---
+++ app/models/marc_fields/language.rb
@@ -0,0 +1,42 @@
+##
+# A class to merge content from the indexed
+# language data and the MARC 546 field
+# https://www.loc.gov/marc/bibliographic/bd546.html
+class Language < MarcField
+  def values
+    [[indexed_languages.join(', '), super.join(', ')].reject(&:blank?).join('. ')]
+  end
+
+  def label
+    return I18n.t('searchworks.marc_fields.notation.label') if notation?
+    super
+  end
+
+  private
+
+  def tags
+    %w(546)
+  end
+
+  def notation?
+    document_formats.include?('Music score') && contains_only_subfield_b?
+  end
+
+  def contains_only_subfield_b?
+    relevant_fields.all? do |field|
+      field.subfields.all? do |subfield|
+        subfield.code == 'b'
+      end
+    end
+  end
+
+  def indexed_languages
+    languages = Array.wrap(document['language'])
+    vern_languages = Array.wrap(document['language_vern'])
+    [languages, vern_languages].flatten.compact
+  end
+
+  def document_formats
+    document[document.format_key] || []
+  end
+end
---
+++ spec/models/marc_fields/langauge_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+
+describe Language do
+  include MarcMetadataFixtures
+
+  let(:document) { SolrDocument.new(marcxml: marc, format_main_ssim: formats) }
+  subject(:language) { described_class.new(document) }
+
+  describe 'label' do
+    context 'when the format includes "Music score"' do
+      let(:formats) { ['Music score'] }
+
+      context 'when there are subfields other that $b' do
+        let(:marc) { language_fixture }
+
+        it 'is "Language"' do
+          expect(language.label).to eq 'Language'
+        end
+      end
+
+      context 'when there are only subfield $b present' do
+        let(:marc) { notation_fixture }
+
+        it 'is "Notation"' do
+          expect(language.label).to eq 'Notation'
+        end
+      end
+    end
+
+    context 'when the format does not include "Music score"' do
+      let(:formats) { ['Not a Music score'] }
+      let(:marc) { notation_fixture }
+
+      it 'is "Language" regardless of subfield' do
+        expect(language.label).to eq 'Language'
+      end
+    end
+  end
+
+  describe 'values' do
+    let(:document) do
+      SolrDocument.new(marcxml: marc, language: ['English'], language_vern: ['English in another language'])
+    end
+    let(:marc) { language_fixture }
+
+    it 'appends language index fields with marc fields' do
+      expect(language.values.length).to eq 1
+      expect(language.values.first).to eq('English, English in another language. Language $a Language $b')
+    end
+  end
+end
