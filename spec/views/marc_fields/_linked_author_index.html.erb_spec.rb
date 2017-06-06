bb279c50d6008e783b7b96da794db4905f90a0ad
Darren Hardy <drh@stanford.edu>
2017-06-06 14:15:56 -0700

    add new LinkedAuthor class for rendering linked "authors" and 711 $j suppression

---
+++ spec/views/marc_fields/_linked_author_index.html.erb_spec.rb
@@ -0,0 +1,76 @@
+require 'spec_helper'
+
+describe 'marc_fields/_linked_author_index.html.erb', type: :view do
+  include MarcMetadataFixtures
+  let(:document) { SolrDocument.new(marcxml: send("linked_author_#{target}_fixture".to_sym)) }
+
+  before do
+    allow(view).to receive_messages(linked_author: LinkedAuthor.new(document, target))
+    render
+  end
+
+  context 'Creator' do
+    let(:target) { :creator }
+
+    it 'does not render the label' do
+      expect(rendered).not_to match(/Author\/Creator/)
+    end
+
+    it 'renders link, search, and post_text correctly' do
+      expect(rendered).to have_css('li a', text: 'Dodaro, Gene L.')
+      expect(rendered).to have_css('li a @href', text: /q=%22Dodaro%2C\+Gene\+L\.\+%22/)
+      expect(rendered).to have_css('li a @href', text: /search_field=search_author/)
+      expect(rendered).not_to have_css('li a @href', text: /author\./)
+      expect(rendered).not_to have_css('li a @href', text: /\sAuthor/)
+    end
+
+    it 'included the extra text after the link' do
+      expect(rendered).to match(/author\. Author/)
+    end
+  end
+
+  context 'Corporate Author' do
+    let(:target) { :corporate_author }
+
+    it 'does not render the label' do
+      expect(rendered).not_to match(/Corporate Author/)
+    end
+
+    it 'renders link, search, and post_text correctly' do
+      expect(rendered).to have_css('li a', text: 'Ecuador. Procuraduría General del Estado, A Title')
+      expect(rendered).to have_css('li a @href', text: /q=%22Ecuador.\+Procuradur%C3%ADa\+General\+del\+Estado%2C\+%22/)
+      expect(rendered).to have_css('li a @href', text: /search_field=search_author/)
+      expect(rendered).not_to have_css('li a @href', text: /A Title/)
+      expect(rendered).not_to have_css('li a @href', text: /issuing body/)
+    end
+
+    it 'included the extra text after the link' do
+      expect(rendered).to match(/author, issuing body\. Art copyist/)
+    end
+  end
+
+  context 'Meeting (with venacular)' do
+    let(:target) { :meeting }
+
+    it 'does not render label' do
+      expect(rendered).not_to match(/Meeting/)
+    end
+
+    it 'renders link, search, and post_text correctly' do
+      expect(rendered).to have_css('li:nth-child(1) a', text: 'Technical Workshop on Organic Agriculture (1st : 2010 : Ogbomoso, Nigeria) A title')
+      expect(rendered).to have_css('li:nth-child(1) a @href', text: /q=%22Technical\+Workshop\+on\+Organic\+Agriculture\+%281st\+%3A\+2010\+%3A\+Ogbomoso%2C\+Nigeria%29\+%22/)
+      expect(rendered).to have_css('li:nth-child(1) a @href', text: /search_field=search_author/)
+      expect(rendered).not_to have_css('li:nth-child(1) a @href', text: /A title/)
+      expect(rendered).not_to have_css('li:nth-child(1) a @href', text: /creator/)
+    end
+
+    it 'renders the venacular correctly' do
+      expect(rendered).to have_css('li:nth-child(2) a', text: 'Vernacular Title Vernacular Uniform Title')
+      expect(rendered).to have_css('li:nth-child(2) a @href', text: /q=%22Vernacular\+Uniform\+Title\+%22/)
+    end
+
+    it 'included the extra text after the link' do
+      expect(rendered).to match(/creator\. Other/)
+    end
+  end
+end
