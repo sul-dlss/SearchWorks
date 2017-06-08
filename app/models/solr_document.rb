dbc3da08a4c8c199f12ddf966f3c083339c78318
Chris Beer <chris@cbeer.info>
2017-06-08 15:23:20 -0700

    Move Email and SMS overrides into app/models/concerns and use module inheritance instead of monkeypatching to make appropriate changes

--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -55,10 +55,10 @@ class SolrDocument
   # self.unique_key = 'id'
 
   # Email uses the semantic field mappings below to generate the body of an email.
-  SolrDocument.use_extension( Blacklight::Document::Email )
+  SolrDocument.use_extension( Searchworks::Document::Email )
 
   # SMS uses the semantic field mappings below to generate the body of an SMS email.
-  SolrDocument.use_extension( Blacklight::Document::Sms )
+  SolrDocument.use_extension( Searchworks::Document::Sms )
 
   # DublinCore uses the semantic field mappings below to assemble an OAI-compliant Dublin Core document
   # Semantic mappings of solr stored fields. Fields may be multi or
