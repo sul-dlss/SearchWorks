3906a7ac3bac78a00ede983a00553dd597d5cef5
Chris Beer <chris@cbeer.info>
2017-06-12 13:58:17 -0700

    run the blacklight_range_limit install generator

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -1,5 +1,6 @@
 class SearchBuilder < Blacklight::SearchBuilder
   include Blacklight::Solr::SearchBuilderBehavior
   include BlacklightAdvancedSearch::AdvancedSearchBuilder
+  include BlacklightRangeLimit::RangeLimitBuilder
   self.default_processor_chain += [:add_advanced_parse_q_to_solr, :add_advanced_search_to_solr]
 end
