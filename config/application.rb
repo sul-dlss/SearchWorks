2da09efe2f7144fc3b28f6d16ff3adad841d29c1
Chris Beer <chris@cbeer.info>
2017-06-13 07:18:43 -0700

    Update to Rails 5.0

--- config/application.rb
+++ config/application.rb
@@ -42,5 +42,7 @@ module SearchWorks
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.action_controller.permit_all_parameters = true
   end
 end
