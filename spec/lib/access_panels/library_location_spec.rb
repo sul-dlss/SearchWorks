3ee4a5e85bc51066e65e2385d38eadf9b6734012
Jessie Keck <jessie.keck@gmail.com>
2017-06-14 16:30:39 -0700

    Update how we require some classes to deal with eager loading issues.

--- spec/lib/access_panels/library_location_spec.rb
+++ spec/lib/access_panels/library_location_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
-require 'library_location'
+require 'access_panels/library_location'
 
-describe LibraryLocation do
+describe AccessPanels::LibraryLocation do
   let(:non_present_library_doc) {
     SolrDocument.new(
       id: '123',
@@ -19,18 +19,18 @@ describe LibraryLocation do
       )
     }
     it "should only return the libraries" do
-      expect(LibraryLocation.new(doc).libraries.length).to eq 2
+      expect(described_class.new(doc).libraries.length).to eq 2
     end
   end
   describe "present?" do
     it "should have a library location present" do
       doc = SolrDocument.new(id: '123', item_display: ["36105217238315 -|- EARTH-SCI -|- STACKS -|-  -|- STKS -|- G70.212 .A426 2011 -|- lc g   0070.212000 a0.426000 002011 -|- en~j~~~zzsz}xyxzzz~pz}vxtzzz~zzxzyy~~~~~~~~~~~~~~~ -|- G70.212 .A426 2011 -|- lc g   0070.212000 a0.426000 002011"])
-      expect(LibraryLocation.new(doc).present?).to eq true
+      expect(described_class.new(doc).present?).to eq true
     end
 
     it "should not have a library location present" do
       doc = SolrDocument.new(id: '123')
-      expect(LibraryLocation.new(doc).present?).to eq false
+      expect(described_class.new(doc).present?).to eq false
     end
   end
 end
