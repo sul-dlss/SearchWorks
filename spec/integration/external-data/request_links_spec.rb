681941b11f92d077a87de4fb0d3c68a2162ec5c0
Chris Beer <chris@cbeer.info>
2017-06-16 09:12:16 -0700

    Update request links spec to match current UX

--- spec/integration/external-data/request_links_spec.rb
+++ spec/integration/external-data/request_links_spec.rb
@@ -5,28 +5,28 @@ describe 'Request Links', type: :feature, :"data-integration" => true do
     it 'should be rendered for configured -30 locations' do
       visit solr_document_path '4085340'
 
-      within('ul.location') do
-        expect(page).to have_css('li', text: /University Archives\s*Request/)
-        expect(page).to have_css('li a', text: "Request")
+      within('.location') do
+        expect(page).to have_css('.location-name', text: "University Archives")
+        expect(page).to have_css('.request-button', text: "Request on-site access")
       end
     end
     describe 'Hopkins' do
       it 'should not render request links for items that exist at other libraries' do
         visit solr_document_path("10180544")
 
-        within(first("ul.location")) do
-          expect(page).to_not have_css('li a', text: "Request")
+        within(first(".location")) do
+          expect(page).not_to have_css('.request-button', text: "Request on-site access")
         end
       end
       it 'should not render request links for items that are available online' do
         visit solr_document_path("10402123")
 
-        expect(page).to_not have_css('li a', text: "Request")
+        expect(page).to_not have_css('.request-button', text: "Request on-site access")
       end
       it 'should render request links for items that are not available online or at other libraries' do
         visit solr_document_path("3010807")
 
-        expect(page).to have_css('li a', text: "Request")
+        expect(page).to have_css('.request-button', text: "Request")
       end
     end
   end
