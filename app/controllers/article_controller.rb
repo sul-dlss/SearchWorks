5cc9241385481619ba7008edd46c44f6e823d723
Darren Hardy <drh@stanford.edu>
2017-06-21 13:32:27 -0700

    adds a new Article search landing page at /article/home

---
+++ app/controllers/article_controller.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# ArticleController is the controller for Article Search
+class ArticleController < ApplicationController
+  def new; end
+end
---
+++ app/helpers/article_helper.rb
@@ -0,0 +1,2 @@
+module ArticleHelper
+end
