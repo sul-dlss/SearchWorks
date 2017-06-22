0d132b2a6cbbdbc85440bd33d532558669cd9cd5
Jessie Keck <jessie.keck@gmail.com>
2017-06-22 16:16:00 -0700

    Add a custom path helper method in the search form.

--- spec/helpers/article_helper_spec.rb
+++ spec/helpers/article_helper_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe ArticleHelper do
     context 'when in the ArticlesController' do
       before { expect(helper).to receive_messages(controller_name: 'article') }
 
-      it { expect(helper.article_search?).to be false }
+      it { expect(helper.article_search?).to be true }
     end
 
     context 'when not in the ArticlesController' do
       before { expect(helper).to receive_messages(controller_name: 'anything_else') }
 
-      it { expect(helper.article_search?).to be true }
+      it { expect(helper.article_search?).to be false }
     end
   end
 end
