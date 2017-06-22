ccf635c9c3c9ed5000766fc16253428374cb5638
Jessie Keck <jessie.keck@gmail.com>
2017-06-22 15:45:05 -0700

    Add a dropdown menu to switch between catalog and article search contexts.

--- spec/helpers/article_helper_spec.rb
+++ spec/helpers/article_helper_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 RSpec.describe ArticleHelper do
-  pending 'No methods yet'
+  describe '#article_search?' do
+    context 'when in the ArticlesController' do
+      before { expect(helper).to receive_messages(controller_name: 'article') }
+
+      it { expect(helper.article_search?).to be false }
+    end
+
+    context 'when not in the ArticlesController' do
+      before { expect(helper).to receive_messages(controller_name: 'anything_else') }
+
+      it { expect(helper.article_search?).to be true }
+    end
+  end
 end
