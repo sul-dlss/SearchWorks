bc45a899386c451bbc9308f748e6d9e4059419d8
Jessie Keck <jessie.keck@gmail.com>
2017-06-23 11:01:03 -0700

    Add a deploy environment for article search.

---
+++ config/deploy/preview_articles.rb
@@ -0,0 +1,6 @@
+set :bundle_without, %w[sqlite development test].join(' ')
+
+server 'sw-webapp-sandbox-f.stanford.edu', user: 'blacklight', roles: %w[web db app]
+
+Capistrano::OneTimeKey.generate_one_time_key!
+set :rails_env, 'production'
