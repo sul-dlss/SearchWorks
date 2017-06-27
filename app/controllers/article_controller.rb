a67eb6b1a5f091f9227be40550302c13d632f703
Jessie Keck <jessie.keck@gmail.com>
2017-06-27 12:27:16 -0700

    Update field names for updated EDS api gem.

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -26,8 +26,8 @@ class ArticleController < ApplicationController
     config.repository_class = Eds::Repository
 
     # solr field configuration for search results/index views
-    config.index.title_field = :title_display
-    config.index.show_link = 'title_display'
+    config.index.title_field = :eds_title
+    config.index.show_link = 'eds_title'
     config.index.record_display_type = 'format'
 
     config.add_index_field 'author_display', label: 'Author'
@@ -39,26 +39,29 @@ class ArticleController < ApplicationController
     config.add_index_field 'id'
 
     # solr field configuration for document/show views
-    config.show.html_title = 'title_display'
-    config.show.heading = 'title_display'
+    config.show.html_title = 'eds_title'
+    config.show.heading = 'eds_title'
     config.show.display_type = 'format'
-    config.show.pub_date = 'pub_date'
-    config.show.pub_info = 'pub_info'
-    config.show.abstract = 'abstract'
-    config.show.full_text_url = 'full_text_url'
-    config.show.plink = 'plink'
+    config.show.pub_date = 'eds_publication_date'
+    config.show.pub_info = 'eds_publisher_info'
+    config.show.abstract = 'eds_abstract'
+    config.show.plink = 'eds_plink'
     config.show.route = { controller: 'article' }
 
     # solr fields to be displayed in the show (single result) view
     #   The ordering of the field names is the order of the display
-    config.add_show_field 'title_display', label: 'Title'
-    config.add_show_field 'academic_journal', label: 'Journal'
-    config.add_show_field 'author_display', label: 'Author'
-    config.add_show_field 'format', label: 'Format'
-    config.add_show_field 'pub_date', label: 'Publication Date'
-    config.add_show_field 'pub_info', label: 'Published'
-    config.add_show_field 'abstract', label: 'Abstract'
-    config.add_show_field 'doi', label: 'DOI', helper_method: :doi_link
+    config.add_show_field 'eds_title', label: 'Title'
+    config.add_show_field 'eds_languages', label: 'Language'
+    config.add_show_field 'eds_physical_description', label: 'Physical Description'
+    config.add_show_field 'eds_source_title', label: 'Journal'
+    config.add_show_field 'eds_database_name', label: 'Database'
+    config.add_show_field 'eds_authors', label: 'Author'
+    config.add_show_field 'eds_publication_type', label: 'Format'
+    config.add_show_field 'eds_publication_date', label: 'Publication Date'
+    config.add_show_field 'eds_publisher_info', label: 'Published'
+    config.add_show_field 'eds_abstract', label: 'Abstract'
+    config.add_show_field 'eds_subjects', label: 'Subjects'
+    config.add_show_field 'eds_document_doi', label: 'DOI', helper_method: :doi_link
   end
 
   def index
