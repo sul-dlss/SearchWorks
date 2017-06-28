0b6a8e4efde6f354561ee7d2d3f208526d85d206
Darren Hardy <drh@stanford.edu>
2017-06-28 11:22:26 -0700

    Restructure the banner into a red search bar and a grey navbar

--- spec/features/responsive/search_toolbar_responsive_spec.rb
+++ spec/features/responsive/search_toolbar_responsive_spec.rb
@@ -7,34 +7,25 @@ describe "Responsive search bar", js: true, feature: true do
     click_button 'search'
   end
   describe " - desktop view (> 980px)" do
-    it "should display correct tools" do
+    it "displays the search form" do
       within "#search-navbar" do
         expect(page).to have_css(".search-form", visible: true)
-        expect(page).to have_css("#searchbar-navbar-collapse li", count: 3, visible: true)
       end
     end
   end
   describe " - tablet view (768px - 980px) - " do
-    it "should display correct tools" do
+    it "displays the search form" do
       within "#search-navbar" do
         page.driver.resize("800", "800")
         expect(page).to have_css(".search-form", visible: true)
-        expect(page).to have_css("#searchbar-navbar-collapse li", count: 3, visible: true)
       end
     end
   end
   describe " - mobile landscape view (480px - 767px) - " do
-    it "should display correct tools" do
+    it "displays the search form" do
       page.driver.resize("700", "700")
       within "#search-navbar" do
         expect(page).to have_css(".search-form", visible: true)
-        expect(page).to_not have_css("#searchbar-navbar-collapse li", count: 3, visible: true)
-        find("button.navbar-toggle").click
-        within "#searchbar-navbar-collapse" do
-          expect(page).to have_css("li a", text: "ADVANCED", visible: true)
-          expect(page).to have_css("li a", text: "BROWSE", visible: true)
-          expect(page).to have_css("li a", text: /SELECTIONS/, visible: true)
-        end
       end
     end
   end
