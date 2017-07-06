45f9fe0c49e144d3adf454a3a9cdc6ffceb3e77d
Jessie Keck <jessie.keck@gmail.com>
2017-07-06 13:48:34 -0700

    Add 1 spec and unskip 1 spec using new article stubbing feature.

--- spec/features/article_searching_spec.rb
+++ spec/features/article_searching_spec.rb
@@ -30,7 +30,7 @@ feature 'Article Searching' do
   end
 
   scenario 'when searching w/i the article context we stay in the articles controller' do
-    skip 'need to stub out Eds::SearchService'
+    stub_article_service(docs: [SolrDocument.new(id: 'abc123')])
     visit article_home_path
 
     within '.search-form' do
@@ -39,6 +39,32 @@ feature 'Article Searching' do
       click_button 'Search'
     end
 
+    expect(page).to have_css('h2', text: /\d+ results?/)
+
     expect(current_url).to match(%r{/article\?.*&q=Kittens})
+  end
+
+  scenario 'article records are navigable from search results' do
+    results = [
+      SolrDocument.new(id: 'abc123', eds_title: 'The title of the document'),
+      SolrDocument.new(id: '321cba', eds_title: 'Another title for the document')
+    ]
+    stub_article_service(docs: results)
+    stub_article_service(type: :single, docs: [results.first]) # just a single document for the record view
+
+    visit article_home_path
+
+    within '.search-form' do
+      fill_in 'q', with: 'Kittens'
+
+      click_button 'Search'
+    end
+
+    within(first('.document')) do
+      click_link 'The title of the document'
+    end
+
+    expect(page).to have_css('h1', text: 'The title of the document')
+    expect(current_url).to match(%r{/article/abc123})
   end
 end
