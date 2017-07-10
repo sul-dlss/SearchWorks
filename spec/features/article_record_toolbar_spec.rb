4c1bb828eb60bae572d9fd927cb9624af64c6ab2
Jessie Keck <jessie.keck@gmail.com>
2017-07-10 15:45:37 -0700

    Update article service stubbing to be a bit farther up the chain and update toolbar specs.

--- spec/features/article_record_toolbar_spec.rb
+++ spec/features/article_record_toolbar_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-RSpec.describe 'Article Record Toolbar' do
+RSpec.describe 'Article Record Toolbar', js: true do
   let(:previous_document) { SolrDocument.new(id: 1, eds_title: 'My Prev Title') }
   let(:document) { SolrDocument.new(id: 2, eds_title: 'My Title') }
   let(:next_document) { SolrDocument.new(id: 3, eds_title: 'My Next Title') }
@@ -17,13 +17,12 @@ RSpec.describe 'Article Record Toolbar' do
   it 'shows the Send button' do
     within '.record-toolbar' do
       expect(page).to have_css('.btn-sul-toolbar', text: 'Send to')
-      expect(page).to have_css('.sms', text: 'text')
-      expect(page).to have_css('.email', text: 'email')
-      expect(page).to have_css('a[role="menuitem"]', text: 'printer')
+      expect(page).to have_css('.sms', text: 'text', visible: false)
+      expect(page).to have_css('.email', text: 'email', visible: false)
+      expect(page).to have_css('a[role="menuitem"]', text: 'printer', visible: false)
     end
   end
   it 'shows both prev and next buttons' do
-    skip 'not working yet' if ENV['CI']
     within '.record-toolbar' do
       expect(page).to have_css('.previous', text: 'Previous')
       expect(page).to have_css('.next', text: 'Next')
@@ -33,7 +32,6 @@ RSpec.describe 'Article Record Toolbar' do
   context 'handles the first page (no prev)' do
     let(:previous_document) { nil }
     it 'shows only the Next button' do
-      skip 'not working yet' if ENV['CI']
       within '.record-toolbar' do
         expect(page).not_to have_css('.previous', text: 'Previous')
         expect(page).to have_css('.next', text: 'Next')
@@ -44,7 +42,6 @@ RSpec.describe 'Article Record Toolbar' do
   context 'handles the last page (no next)' do
     let(:next_document) { nil }
     it 'shows only the Previous button' do
-      skip 'not working yet' if ENV['CI']
       within '.record-toolbar' do
         expect(page).to have_css('.previous', text: 'Previous')
         expect(page).not_to have_css('.next', text: 'Next')
@@ -56,7 +53,6 @@ RSpec.describe 'Article Record Toolbar' do
     let(:previous_document) { nil }
     let(:next_document) { nil }
     it 'does not show any Previous or Next buttons' do
-      skip 'not working yet' if ENV['CI']
       within '.record-toolbar' do
         expect(page).not_to have_css('.previous', text: 'Previous')
         expect(page).not_to have_css('.next', text: 'Next')
