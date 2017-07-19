f87221b34a177be3246a1525f9347dfa4d3dcb60
Darren Hardy <drh@stanford.edu>
2017-07-19 15:43:45 -0700

    permit tildes in the EDS identifiers

--- config/routes.rb
+++ config/routes.rb
@@ -68,7 +68,7 @@ Rails.application.routes.draw do
 
   resources :course_reserves, only: :index, path: "reserves"
 
-  constraints(id: /[-\w]+/) do # EDS identifier rules (e.g., db__id)
+  constraints(id: /[-~\w]+/) do # EDS identifier rules (e.g., db__id)
     resources :article, only: %i[index show]
     post "article/:id/track" => 'article#track', as: :track_article
   end
