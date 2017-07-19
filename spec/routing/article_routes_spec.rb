4d018112483e1d49329f399774a039dd95ce585a
Darren Hardy <drh@stanford.edu>
2017-07-19 15:43:45 -0700

    permit tildes in the EDS identifiers

--- spec/routing/article_routes_spec.rb
+++ spec/routing/article_routes_spec.rb
@@ -8,11 +8,13 @@ RSpec.describe 'Article Routing', type: :routing do
     expect(get('/article/1')).to route_to(controller: 'article', action: 'show', id: '1')
     expect(get('/article/eds__style1')).to route_to(controller: 'article', action: 'show', id: 'eds__style1')
     expect(get('/article/eds__Style2-with-hyphens')).to route_to(controller: 'article', action: 'show', id: 'eds__Style2-with-hyphens')
+    expect(get('/article/eds__Style2-with~tildes')).to route_to(controller: 'article', action: 'show', id: 'eds__Style2-with~tildes')
   end
   it '#track' do
     expect(post('/article/1/track')).to route_to(controller: 'article', action: 'track', id: '1')
     expect(post('/article/eds__style1/track')).to route_to(controller: 'article', action: 'track', id: 'eds__style1')
     expect(post('/article/eds__Style2-with-hyphens/track')).to route_to(controller: 'article', action: 'track', id: 'eds__Style2-with-hyphens')
+    expect(post('/article/eds__Style2-with~tildes/track')).to route_to(controller: 'article', action: 'track', id: 'eds__Style2-with~tildes')
   end
   it 'other actions are not routable' do
     expect(post('/article')).not_to be_routable
