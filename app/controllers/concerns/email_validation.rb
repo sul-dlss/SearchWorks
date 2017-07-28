05445195017aa56c89f567d0292f659a2afeb758
Camille Villa <cvilla100@gmail.com>
2017-07-28 11:56:18 -0700

    Add email action to articles

--- app/controllers/concerns/email_validation.rb
+++ app/controllers/concerns/email_validation.rb
@@ -1,21 +1,6 @@
 module EmailValidation
   extend ActiveSupport::Concern
 
-  def send_emails_to_all_recipients
-    email_params = { message: params[:message], subject: params[:subject], email_from: params[:email_from] }
-    email_addresses.each do |email_address|
-      email_params[:to] = email_address
-      email = if params[:type] == 'full'
-                SearchWorksRecordMailer.full_email_record(@documents, email_params, url_options)
-              else
-                SearchWorksRecordMailer.email_record(@documents, email_params, url_options)
-              end
-      email.deliver_now
-    end
-
-    flash[:success] = I18n.t('blacklight.email.success')
-  end
-
   def validate_email_params
     case
     when !%w(full brief).include?(params[:type])
