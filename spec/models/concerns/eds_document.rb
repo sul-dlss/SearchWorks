8763f723d0f1beadfad50cdc944e1f378ff111e3
Camille Villa <cvilla100@gmail.com>
2017-08-16 12:02:13 -0700

    Add HTML fulltext links to article results

--- spec/models/concerns/eds_document.rb
+++ spec/models/concerns/eds_document.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe EdsDocument do
   let(:document) do
-    SolrDocument.new(id: '123', eds_html_fulltext: '<anid>09dfa;</anid><p>This Journal</p>, 10(1)')
+    SolrDocument.new(id: '123', eds_html_fulltext_available: true, eds_html_fulltext: '<anid>09dfa;</anid><p>This Journal</p>, 10(1)')
   end
   let(:empty_document) do
     SolrDocument.new(id: '456', eds_html_fulltext: '')
@@ -10,11 +10,11 @@ describe EdsDocument do
 
   describe '#html_fulltext_available' do
     it 'should return true when fulltext is present' do
-      expect(document.html_fulltext_available?).to be true
+      expect(document.html_fulltext?).to be true
     end
 
     it 'returns nil when fulltext is not available' do
-      expect(empty_document.html_fulltext_available?).to be_falsey
+      expect(empty_document.html_fulltext?).to be_falsey
     end
   end
 end
