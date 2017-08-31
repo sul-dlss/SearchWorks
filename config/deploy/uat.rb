d843bd8c78fbbd7e0e751ef75f0fbb16e77d4005
Jessie Keck <jessie.keck@gmail.com>
2017-08-31 07:52:17 -0700

    Add a new UAT deploy environment.

---
+++ config/deploy/uat.rb
@@ -0,0 +1,7 @@
+set :bundle_without, %w[sqlite development test].join(' ')
+
+# Aliased to searchworks-uat
+server 'sw-webapp-sandbox-g.stanford.edu', user: 'blacklight', roles: %w[web db app]
+
+Capistrano::OneTimeKey.generate_one_time_key!
+set :rails_env, 'production'
