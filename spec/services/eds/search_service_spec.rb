ff81da1e109964889338245a24e097d9a14cf888
Jack Reed <phillipjreed@gmail.com>
2017-09-01 13:10:17 -0400

    enhance eds search service to add fetch_many

--- spec/services/eds/search_service_spec.rb
+++ spec/services/eds/search_service_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 
 RSpec.describe Eds::SearchService do
+  let(:document_model) { double(unique_key: :id) }
   let(:blacklight_config) {
     instance_double(
       Blacklight::Configuration,
       repository_class: Eds::Repository,
-      search_builder_class: Blacklight::SearchBuilder
+      search_builder_class: Blacklight::SearchBuilder,
+      document_model: document_model
     )
   }
   subject(:instance) { described_class.new(blacklight_config) }
@@ -23,10 +25,34 @@ RSpec.describe Eds::SearchService do
     expect(results[1]).to eq StubArticleService::SAMPLE_RESULTS
   end
 
-  it '#fetch' do
+  describe '#fetch' do
+    context 'with a single id' do
+      it 'returns a single result' do
+        results = instance.fetch('a')
+        expect(results[1].id).to eq StubArticleService::SAMPLE_RESULTS.first.id
+        expect(results[1]).to be_an SolrDocument
+      end
+    end
+    context 'with multiple ids' do
+      it 'returns a single result' do
+        results = instance.fetch(%w[a b])
+        expect(results[1].count).to eq StubArticleService::SAMPLE_RESULTS.count
+        expect(results[1]).to be_an Array
+      end
+    end
+  end
+
+  it '#fetch_one' do
     results = instance.fetch(StubArticleService::SAMPLE_RESULTS.first.id)
     expect(results.length).to eq 2
     expect(results[0]).to be_an(Blacklight::Solr::Response)
     expect(results[1].id).to eq StubArticleService::SAMPLE_RESULTS.first.id
+  end
+
+  it '#fetch_many' do
+    results = instance.fetch_many(StubArticleService::SAMPLE_RESULTS.map(&:id), {})
+    expect(results.length).to eq 2
+    expect(results[0]).to be_an(Blacklight::Solr::Response)
+    expect(results[1].count).to eq StubArticleService::SAMPLE_RESULTS.count
   end
 end
