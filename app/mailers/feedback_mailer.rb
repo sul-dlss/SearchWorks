8ce1ecfaed3762ee9c31d7929f8acb193f9d86d8
Jack Reed <phillipjreed@gmail.com>
2017-09-06 14:13:58 -0400

    extract class for parsing of params needed for feedback mailer

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -1,28 +1,14 @@
 class FeedbackMailer < ActionMailer::Base
   def submit_feedback(params, ip)
-    if params[:name].present?
-      @name = params[:name]
-    else
-      @name = 'No name given'
-    end
+    @mailer_parser = FeedbackMailerParser.new(params, ip)
 
-    if params[:to].present?
-      @email = params[:to]
-    else
-      @email = 'No email given'
-    end
-
-    @message = params[:message]
-    @url = params[:url]
-    @ip = ip
-    @user_agent = params[:user_agent]
-    @viewport = params[:viewport]
-
-    mail(:to => Settings.EMAIL_TO,
-         :subject => "Feedback from SearchWorks",
-         :from => "feedback@searchworks.stanford.edu",
-         :reply_to => Settings.EMAIL_TO)
+    mail(to: Settings.EMAIL_TO,
+         subject: 'Feedback from SearchWorks',
+         from: 'feedback@searchworks.stanford.edu',
+         reply_to: Settings.EMAIL_TO,
+         template_name: @mailer_parser.type)
   end
+
   def submit_wrong_book_cover(params, ip)
     @url = params[:url]
     @ip = ip
