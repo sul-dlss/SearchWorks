1aa0707bc1b022ff42fbfeb0dbed00311d995d77
Jack Reed <phillipjreed@gmail.com>
2017-09-08 17:17:38 -0400

    add alternate catalog feature

---
+++ app/assets/javascripts/alternate_catalog.js
@@ -0,0 +1,82 @@
+(function (global) {
+  var AlternateCatalog = {
+    init: function(el) {
+      var $el = $(el);
+      var data = $el.data();
+      var $documents = $('#documents');
+      var $title = $el.find('.alternate-catalog-title');
+      var $body = $el.find('.alternate-catalog-body');
+      var $count = $el.find('.alternate-catalog-count');
+      var $close = $el.find('.alternate-catalog-close');
+      var $facets = $el.find('.alternate-catalog-facets');
+
+      // Setup close click handler
+      $close.click(function () {
+        $el.hide();
+      });
+
+      // Insert between the 3rd and 4th document
+      var afterThird = $documents.find('.document-position-2').after($el);
+      // If there is no third document, just append to the end of #documents
+      if (afterThird.length === 0) {
+        $documents.append($el);
+      }
+
+      // Update title
+      $title.text('Searching...')
+
+      // Try other catalog
+      $.getJSON({
+        url: data.alternateCatalog
+      }).done(function (response) {
+        var count = response.response.pages.total_count;
+        if (count > 0) {
+          // Update title
+          $title.text('Your search also found results in');
+          $count.text(parseInt(count).toLocaleString());
+          // Update body
+          var facetHtml = createFacets(response.response.facets, data.alternateCatalog);
+          $facets.html(facetHtml);
+          $body.show();
+        } else {
+          $title.text('No additional results were found in');
+          $body.find('a.btn').remove();
+          $body.find('dl').remove();
+          $body.show();
+        }
+      });
+    }
+  };
+
+  function createFacets(facets, url) {
+    var facetLinks = [];
+    // Iterating over everything here would be too slow
+    var sourceOrResouce = facets.filter( function(facet) {
+      return facet.label === 'Source type' || facet.label === 'Resource type'
+    })
+    sourceOrResouce.forEach(function (facet) {
+      var count = 0;
+      // Iterating over everything here would be even slower.. sort it and take
+      // the top5
+      var topFive = facet.items.sort(function(a, b) {
+        return b.hits - a.hits;
+      }).slice(0, 5);
+      topFive.forEach(function (item) {
+        linkedUrl = url + '&f[' + facet.name + '][]=' + item.value;
+        linkText = item.label + ' (' + parseInt(item.hits).toLocaleString() + ')';
+        facetLinks.push('<a href="' + linkedUrl + '">' + linkText + '</a>');
+      });
+    });
+    return facetLinks.join(' - ')
+  }
+
+  global.AlternateCatalog = AlternateCatalog;
+}(this));
+
+Blacklight.onLoad(function () {
+  'use strict';
+
+  $('[data-alternate-catalog]').each(function (i, element) {
+    AlternateCatalog.init(element);
+  });
+});
---
+++ spec/features/alternate_catalog_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+feature 'Alterate catalog results', js: true do
+  before do
+    stub_article_service(docs: StubArticleService::SAMPLE_RESULTS)
+  end
+
+  scenario 'on catalog search' do
+    visit search_catalog_path(q: '1*')
+    wait_for_ajax
+    within '.alternate-catalog' do
+      expect(page).to have_css 'h3', text: 'YOUR SEARCH ALSO FOUND RESULTS IN'
+      expect(page).to have_css 'a.btn', text: 'See 4 article+ results'
+      expect(page).to have_css(
+        'a[href="/articles?q=1%2A&f[eds_publication_type_facet][]=Academic journals"]',
+        text: 'Academic journals (1)'
+      )
+    end
+  end
+
+  scenario 'on article search' do
+    visit articles_path(q: '1*')
+    wait_for_ajax
+    within '.alternate-catalog' do
+      expect(page).to have_css 'h3', text: 'YOUR SEARCH ALSO FOUND RESULTS IN'
+      expect(page).to have_css 'a.btn', text: 'See 29 catalog results'
+      expect(page).to have_css 'a[href="/catalog?q=1%2A&f[format_main_ssim][]=Book"]', text: 'Book (9)'
+      expect(page).to have_css 'a[href="/catalog?q=1%2A&f[format_main_ssim][]=Image"]', text: 'Image (7)'
+      expect(page).to have_css 'a[href="/catalog?q=1%2A&f[format_main_ssim][]=Database"]', text: 'Database (4)'
+      expect(page).to have_css 'a[href="/catalog?q=1%2A&f[format_main_ssim][]=Newspaper"]', text: 'Newspaper (4)'
+      expect(page).to have_css 'a[href="/catalog?q=1%2A&f[format_main_ssim][]=Video"]', text: 'Video (4)'
+    end
+  end
+end
