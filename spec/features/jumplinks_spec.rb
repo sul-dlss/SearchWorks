547a18284c501a2c502dfd2ca074c447b7e27467
Camille Villa <cvilla100@gmail.com>
2017-09-14 12:20:38 -0700

    Add Yewno to found type links

---
+++ spec/features/jumplinks_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'Jump links', js: true do
+  before do
+    allow_any_instance_of(AbstractSearchService).to receive(:search).and_return(response)
+
+    visit quick_search_path
+    fill_in 'params-q', with: 'jane stanford'
+    click_button 'Search'
+  end
+
+  context 'searcher has results' do
+    let(:response) do
+      instance_double(
+        ArticleSearchService::Response,
+        results: [
+          instance_double(AbstractSearchService::Result)
+        ],
+        total: 666_666,
+        facets: [],
+        highlighted_facet_values: []
+      )
+    end
+
+    scenario 'points to each search module anchor' do
+      within '.result-types' do
+        expect(page).to have_css(
+          'a[href$="#catalog"]',
+          text: /Catalog/
+        )
+        expect(page).to have_css(
+          'a[href$="#article"]',
+          text: /Articles+/
+        )
+        expect(page).to have_css(
+          'a[href$="#yewno"]',
+          text: /Yewno/
+        )
+      end
+    end
+  end
+end
