8d8652fc5c28677ad9c86cc341860f87244e228f
Camille Villa <cvilla100@gmail.com>
2017-09-20 13:15:00 -0700

    Redirect about page to home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'quick_search/search#index'
   mount QuickSearch::Engine => '/all'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/about', to: redirect('/all')
 end
