3572f92b03de166b84bcdb92efb46b72ea0f1f01
Jessie Keck <jessie.keck@gmail.com>
2017-11-30 19:16:02 -0800

    Truncate result descriptions to 150 characters.

--- spec/views/quick_search/search/_result_details.html.erb_spec.rb
+++ spec/views/quick_search/search/_result_details.html.erb_spec.rb
@@ -3,11 +3,12 @@
 require 'rails_helper'
 
 describe 'quick_search/search/_result_details.html.erb' do
+  let(:description) { 'The Description' }
   let(:result) do
     ArticleSearchService::Result.new.tap do |r|
       r.title = 'Title'
       r.link = 'http://example.com'
-      r.description = 'The Description'
+      r.description = description
       r.fulltext_link_html = '<a href="#">Link</a>'
     end
   end
@@ -27,6 +28,16 @@ describe 'quick_search/search/_result_details.html.erb' do
 
   it 'renders the description' do
     expect(rendered).to have_css('p', text: 'The Description')
+  end
+
+  context 'long descriptions' do
+    let(:description) { 'The description ' * 50 }
+
+    it 'are truncated to 150 characters' do
+      text = Capybara.string(rendered).find('p', text: /^The description/).text
+      expect(text.length).to eq 150
+      expect(text).to end_with '...'
+    end
   end
 
   it 'renders the fulltext link html' do
