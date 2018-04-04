37975201204f2548b2cc98b50cf6f80861f4854a
Jessie Keck <jessie.keck@gmail.com>
2018-04-04 09:47:02 -0700

    Explicitly declare branch running in preview-gryphon deploy env.

--- config/deploy/preview_gryphon.rb
+++ config/deploy/preview_gryphon.rb
@@ -1,5 +1,7 @@
 set :bundle_without, %w[sqlite development test].join(' ')
 
+set :branch, 'stanford-work-locations-facet'
+
 # Other aliases are sw-gryphon-search, gryphon-search, and searchworks-gryphon-search
 server 'sw-webapp-sandbox-c.stanford.edu', user: 'blacklight', roles: %w[web db app]
 
