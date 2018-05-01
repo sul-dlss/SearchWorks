5a20b41977c978358e1a64c86e9362904a2252b7
Justin Coyne <jcoyne@justincoyne.com>
2018-05-01 08:26:32 -0500

    Remove dead code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :password, :password_confirmation if Rails::VERSION::MAJOR < 4
   # Connects this user object to Blacklights Bookmarks.
   include Blacklight::User
   # Include default devise modules. Others available are:
