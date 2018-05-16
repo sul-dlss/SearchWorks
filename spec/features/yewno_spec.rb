014ad35b35ba406ab0d3fa8a1f564b915ff87752
Chris Beer <cabeer@stanford.edu>
2018-05-16 15:10:20 -0700

    Update stubbing to include all called methods for the various services

--- spec/features/yewno_spec.rb
+++ spec/features/yewno_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 feature 'Yewno', js: true do
   before do
-    response = instance_double(AbstractSearchService::Response, results: [])
+    response = instance_double(AbstractSearchService::Response, results: [], total: 0)
     allow_any_instance_of(AbstractSearchService).to receive(:search)
       .and_return response
     visit quick_search_path
