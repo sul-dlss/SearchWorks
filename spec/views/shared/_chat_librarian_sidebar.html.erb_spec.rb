dd34f8b8099b08b5ed015cf857125178aba49cac
Jack Reed <phillipjreed@gmail.com>
2018-10-01 09:35:39 -0600

    get live hours lookup for zero results chat block

--- spec/views/shared/_chat_librarian_sidebar.html.erb_spec.rb
+++ spec/views/shared/_chat_librarian_sidebar.html.erb_spec.rb
@@ -7,5 +7,7 @@ describe 'shared/_chat_librarian_sidebar.html.erb' do
   it 'should have correct data attributes' do
     expect(rendered).to have_css('[data-jid="ic@chat.libraryh3lp.com"]')
     expect(rendered).to have_css('[data-library-h3lp]')
+    expect(rendered).to have_css('[data-hours-route=\'/hours/IC\']')
+    expect(rendered).to have_css '.location-hours-today'
   end
 end
