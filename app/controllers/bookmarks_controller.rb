686538f95d37f2259ce8163aa40c1e3f3a2be0c1
Jessie Keck <jessie.keck@gmail.com>
2018-10-26 17:31:54 -0700

    Move clear action from class_eval in initializer to a locla controller.

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,4 +1,8 @@
-Blacklight::BookmarksController.class_eval do
+# frozen_string_literal: true
+
+class BookmarksController < CatalogController
+  include Blacklight::Bookmarks
+
   def clear
     if current_or_guest_user.bookmarks.clear
       flash[:success] = I18n.t('blacklight.bookmarks.clear.success')
