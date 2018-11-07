b52e54fa3e1a99002d1394d0de35e98a3f40c9c2
Jack Reed <phillipjreed@gmail.com>
2018-11-07 07:28:47 -0700

    ensure that one url is sent in the encoded url (not an array)
    This fixes an issue where the IIIF manifest url could be encoded as
    an array.

--- app/models/concerns/iiif_concern.rb
+++ app/models/concerns/iiif_concern.rb
@@ -2,6 +2,6 @@
 # Module that provides SolrDocument IIIF specific methods
 module IiifConcern
   def iiif_manifest
-    fetch(:iiif_manifest_url_ssim, nil)
+    fetch(:iiif_manifest_url_ssim, []).first
   end
 end
