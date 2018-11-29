c66b5e97a4ac74618e3afd219edee7a893665da9
Jessie Keck <jessie.keck@gmail.com>
2018-11-28 20:15:44 -0800

    Add mobile and tablet chrome drive browser configurations.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,20 @@ Capybara.register_driver :headless_chrome do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
 end
 
+Capybara.register_driver :mobile_headless do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w[headless disable-gpu no-sandbox window-size=700,700] }
+  )
+  Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
+end
+
+Capybara.register_driver :tablet_headless do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w[headless disable-gpu no-sandbox window-size=800,700] }
+  )
+  Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
+end
+
 Capybara.default_max_wait_time = ENV["CI"] ? 30 : 10
 
 if ENV["COVERAGE"] or ENV["CI"]
@@ -40,6 +54,25 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
 
   config.include Capybara::DSL
+
+  config.before(:example, responsive: true) do |example|
+    page_width = example.metadata[:page_width].to_i
+    driver = if page_width < 767
+               :mobile_headless
+             elsif page_width < 980
+               :tablet_headless
+             else
+               raise ArgumentError, "Browsers over 980px wide don't need to use the responsive test mode"
+             end
+
+    Capybara.javascript_driver = driver
+    Capybara.current_driver = driver
+  end
+
+  config.after(:example, responsive: true) do
+    Capybara.javascript_driver = :headless_chrome
+    Capybara.current_driver = :headless_chrome
+  end
 
   # ## Mock Framework
   #
