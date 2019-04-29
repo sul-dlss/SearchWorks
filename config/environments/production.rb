3baf33802c7f43167fe5016e791090624541aafd
Jessie Keck <jessie.keck@gmail.com>
2019-04-29 16:38:03 -0700

    Set Uglifier to use the experimental Harmony mode (to get ES6 syntax)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,8 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # Harmony mode is experimental. We should not use it in production.
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
