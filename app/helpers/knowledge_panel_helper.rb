85eaf87b7cb71ebf8fb27074b2a562c3aa4fdab5
Jack Reed <phillipjreed@gmail.com>
2019-04-29 18:41:14 -0600

    Hacky start to think about integration of Who's on First data in SearchWorks
    Make sure your root project directory has a wof_lookup.json file.
    
    curl -o wof_lookup.json https://gist.githubusercontent.com/mejackreed/d217e5f7a6b35bf7c964cc8089fb0e4a/raw/0cb526a96c099fdc4be5aa15688830104a32c29b/wof_lookup.json
    
    lib/tasks/wof.rake - rake tasks to create the data needed

--- app/helpers/knowledge_panel_helper.rb
+++ app/helpers/knowledge_panel_helper.rb
@@ -6,4 +6,13 @@ module KnowledgePanelHelper
       URI.parse(u).host
     end
   end
+
+  def wof_from_query(p = params)
+    subject_terms = (p['search_field'] == 'subject_terms') && p['q'] && Wof[p['q'].gsub('"', '')]
+    genre_facet = p[:f] && p[:f][:genre_ssim] && Wof[p[:f][:genre_ssim].first]
+    geo_facet = p[:f] && p[:f][:geographic_facet] && Wof[p[:f][:geographic_facet].first]
+    topic_facet = p[:f] && p[:f][:topic_facet] && Wof[p[:f][:topic_facet].first]
+
+    [subject_terms, genre_facet, geo_facet, topic_facet].select { |w| w.is_a?(Hash) }.first
+  end
 end
