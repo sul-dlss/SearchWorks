5e8d15fd9853789d53222d30538d2a5f9a105785
Jessie Keck <jessie.keck@gmail.com>
2019-05-03 17:24:05 -0700

    Symlink wof lookup file from shared on deploy.

--- config/deploy/preview_articles.rb
+++ config/deploy/preview_articles.rb
@@ -2,5 +2,7 @@ set :bundle_without, %w[sqlite development test].join(' ')
 
 server 'sw-webapp-sandbox-f.stanford.edu', user: 'blacklight', roles: %w[web db app]
 
+set :linked_files, fetch(:linked_files, []) << 'wof_lookup.json'
+
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'production'
