f6ec198a46c0453624a7ce091da80dfac60c2697
Camille Villa <cvilla100@gmail.com>
2019-05-09 14:28:52 -0700

    Remove redundant selenium require

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,7 +9,6 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'rspec/rails'
 
 require 'capybara/rspec'
-require 'selenium-webdriver'
 
 Capybara.javascript_driver = :selenium_chrome_headless
 Capybara.default_max_wait_time = 15
