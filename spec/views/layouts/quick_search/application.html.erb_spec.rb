0f016a84b98395398ef1dfade6b60c6991d4a17c
Jack Reed <mejackreed@users.noreply.github.com>
2020-07-30 11:10:25 -0600

    Fixes #287 adds working skip links (#301)

---
+++ spec/views/layouts/quick_search/application.html.erb_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'layouts/quick_search/application.html.erb' do
+  before do
+    without_partial_double_verification do
+      allow(view).to receive(:opensearch_path).and_return ''
+      allow(view).to receive(:body_class).and_return ''
+    end
+    render
+  end
+  describe 'contains skip links' do
+    it do
+      expect(rendered).to have_link('Skip to search', href: '#params-q')
+      expect(rendered).to have_link('Skip to main content', href: '#main-content')
+    end
+  end
+end
