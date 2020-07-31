b1238194b853d948ee7abffe500c4c50423c673f
Jessie Keck <jessie.keck@gmail.com>
2020-07-31 13:53:00 -0700

    Move code that augments documents for JSON API response to a presenter class.

---
+++ app/models/json_results_document_presenter.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class JsonResultsDocumentPresenter
+  def initialize(source_document)
+    @source_document = source_document
+  end
+
+  def as_json(*)
+    source_document.as_json.merge(fulltext_link_html)
+  end
+
+  private
+
+  attr_reader :source_document
+
+  def fulltext_link_html
+    return {} if online_links.blank?
+
+    {
+      'fulltext_link_html' => online_links.map do |link|
+        html = link.stanford_only? ? "<span class=\"stanford-only\">#{link.html}</span>" : link.html
+
+        "<span class=\"online-label\">Online</span> #{html}"
+      end
+    }
+  end
+
+  def online_links
+    return [] unless source_document&.access_panels&.online?
+
+    source_document&.access_panels&.online&.links
+  end
+end
---
+++ spec/requests/json_api_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe 'JSON API Responses' do
+  it 'has augmmented documents using the JsonResultsDocumentPresenter' do
+    get '/catalog', params: { q: '57', format: 'json' }
+    documents = JSON.parse(response.body).dig('response', 'docs')
+
+    expect(documents.length).to be 1
+    expect(documents.first['fulltext_link_html']).to be_present
+    expect(documents.first['fulltext_link_html'].first).to include('sfx.example.com')
+  end
+end
