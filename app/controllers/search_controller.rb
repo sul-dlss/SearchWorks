b129e115986599d3086a1ad01d1a3760dbe71326
Chris Beer <cabeer@stanford.edu>
2020-08-12 08:13:02 -0700

    Extract 'doi trap' to some clever routing

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,43 +1,8 @@
 class SearchController < ApplicationController
-  # include QuickSearch::DoiTrap
-
-  def doi_trap
-    unless params_q_scrubbed.nil?
-      if is_a_doi?(doi_query)
-        redirect_to doi_loaded_link
-      end
-    end
-  end
-
-  def is_a_doi?(query)
-    if doi_regex.match(query)
-      true
-    else
-      false
-    end
-  end
-
-  def doi_regex
-    /^(?:(?:doi:?\s*|(?:http:\/\/)?(?:dx\.)?(?:doi\.org)?\/))?(10(?:\.\d+){1,2}\/\S+)$/i
-  end
-
-  def doi_loaded_link
-     Settings.quick_search.doi_loaded_link + CGI.escape(doi_regex.match(doi_query)[1])
-  end
-
-  def doi_query
-    query = params_q_scrubbed
-    query.strip!
-    query.squish!
-    query
-  end
-
   def params_q_scrubbed
     params[:q]&.scrub
   end
   helper_method :params_q_scrubbed
-
-  before_action :doi_trap
 
   def index
     @search_form_placeholder = I18n.t "defaults_search.search_form_placeholder"
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,32 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  class DoiQueryConstraint
+    def self.doi_regex
+      /^(?:(?:doi:?\s*|(?:http:\/\/)?(?:dx\.)?(?:doi\.org)?\/))?(10(?:\.\d+){1,2}\/\S+)$/i
+    end
+
+    def self.extract_doi(query)
+      query.scrub.strip.squish
+    end
+
+    def initialize; end
+
+    def matches?(request)
+      return false unless request.params[:q]
+
+      doi = DoiQueryConstraint.extract_doi(request.params[:q])
+      DoiQueryConstraint.doi_regex.match(doi)
+    end
+  end
+
+  constraints(DoiQueryConstraint.new) do
+    get '/', to: redirect { |path_params, req|
+      doi = DoiQueryConstraint.extract_doi(req.params[:q])
+      Settings.quick_search.doi_loaded_link + CGI.escape(DoiQueryConstraint.doi_regex.match(doi)[1])
+    }
+  end
+
   root to: 'search#index'
   get '/' => 'search#index', as: 'quick_search'
   get 'opensearch' => 'opensearch#opensearch', :defaults => { :format => 'xml' }
