e4e744ba910ab8d9bd77872074ef768fc1677f08
Chris Beer <cabeer@stanford.edu>
2020-08-12 09:57:44 -0700

    Backfill some routing tests

---
+++ spec/requests/doi_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'DOI queries', type: :request do
+  it 'bounces you to the DOI endpoint' do
+    get '/?q=doi:10.1109/5.771073'
+    expect(response).to redirect_to('http://doi.org/10.1109%2F5.771073')
+  end
+end
