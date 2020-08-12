c833c7373e60826dc7efd6d72672a16332b127ce
Chris Beer <cabeer@stanford.edu>
2020-08-12 09:27:12 -0700

    Consolidate xhr rendering into the render_module helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -36,6 +36,12 @@ module ApplicationHelper
   end
 
   def render_module(searcher, service_name, partial_name = 'module', per_page = 3)
-    render partial: partial_name , locals: { module_display_name: t("#{service_name}_search.display_name"), searcher: searcher, search: '', service_name: service_name, per_page: per_page }
+    if searcher
+      render partial: partial_name , locals: { module_display_name: t("#{service_name}_search.display_name"), searcher: searcher, search: '', service_name: service_name, per_page: per_page }
+    else
+      content_tag :div, id: service_name, class: 'module-contents', tabindex: -1 do
+        content_tag :p, '', class: 'search-error', data: { 'quicksearch-xhr-url' => xhr_search_path(service_name, q: params[:q]) }
+      end
+    end
   end
 end
