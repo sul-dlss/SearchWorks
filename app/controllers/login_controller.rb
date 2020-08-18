2db77b86d741d2621d976de0476a211c552b288f
Jessie Keck <jessie.keck@gmail.com>
2020-08-18 10:20:00 -0700

    Print out affiliation in layout

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -6,6 +6,10 @@ class LoginController < ApplicationController
     # Set eds_guest to nil so the EDS session gets reset if needed in the article controller
     session['eds_guest'] = nil if session['eds_guest']
     session['suAffiliation'] = request.env['suAffiliation'] || ENV['suAffiliation']
+    session['eduPersonAffiliation'] = request.env['eduPersonAffiliation'] || ENV['eduPersonAffiliation']
+    session['eduPersonScopedAffiliation'] = request.env['eduPersonScopedAffiliation'] || ENV['eduPersonScopedAffiliation']
+    session['unscoped-affiliation'] = request.env['unscoped-affiliation'] || ENV['unscoped-affiliation']
+    session['eduPersonEntitlement'] = request.env['eduPersonEntitlement'] || ENV['eduPersonEntitlement']
 
     if params[:referrer].present?
       redirect_to params[:referrer]
