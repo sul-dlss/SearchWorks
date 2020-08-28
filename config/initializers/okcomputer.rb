14e945c41745b6ad930ca32ed6a58122ff7719b3
Jack Reed <mejackreed@users.noreply.github.com>
2020-08-28 16:43:22 -0600

    Remove legacy library website searcher fixes #360 (#373)

--- config/initializers/okcomputer.rb
+++ config/initializers/okcomputer.rb
@@ -27,6 +27,6 @@ end
 OkComputer::Registry.register 'search_articles', SearcherCheck.new(QuickSearch::ArticleSearcher)
 OkComputer::Registry.register 'search_catalog', SearcherCheck.new(QuickSearch::CatalogSearcher)
 OkComputer::Registry.register 'search_lib_guides', SearcherCheck.new(QuickSearch::LibGuidesSearcher)
-OkComputer::Registry.register 'search_library_website', SearcherCheck.new(QuickSearch::LibraryWebsiteSearcher)
+OkComputer::Registry.register 'search_library_website', SearcherCheck.new(QuickSearch::LibraryWebsiteApiSearcher)
 
 OkComputer.make_optional(%w[search_articles search_catalog search_lib_guides search_library_website])
