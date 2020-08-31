f5864fca747997231c5e4bb12c51412866e3186b
Camille Villa <cvilla100@gmail.com>
2020-08-31 13:57:32 -0700

    Point routes at /all

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
   end
 
   constraints(DoiQueryConstraint.new) do
-    get '/', to: redirect { |path_params, req|
+    get '/all', to: redirect { |path_params, req|
       doi = DoiQueryConstraint.extract_doi(req.params[:q])
       Settings.quick_search.doi_loaded_link + CGI.escape(DoiQueryConstraint.doi_regex.match(doi)[1])
     }
@@ -29,10 +29,11 @@ Rails.application.routes.draw do
 
   constraints(lambda { |request| request.params[:q].blank? || request.params[:q].scrub.blank? }) do
     get '/' => 'pages#home'
+    get '/all' => 'pages#home'
   end
 
   root to: 'search#index'
-  get '/' => 'search#index', as: 'quick_search'
-  get 'opensearch' => 'opensearch#opensearch', :defaults => { :format => 'xml' }
-  get 'xhr_search/:endpoint' => 'search#xhr_search', as: 'xhr_search', defaults: { :format => 'html' }
+  get '/all' => 'search#index', as: 'quick_search'
+  get '/all/opensearch' => 'opensearch#opensearch', as: 'opensearch', :defaults => { :format => 'xml' }
+  get '/all/xhr_search/:endpoint' => 'search#xhr_search', as: 'xhr_search', defaults: { :format => 'html' }
 end
