5617a86edeafbc1c27ae2c3c45907d63f23d2bde
Chris Beer <cabeer@stanford.edu>
2021-02-18 14:45:42 -0800

    Add marcxml back to the document json response for potential API consumers

--- app/models/concerns/marc_json_extension.rb
+++ app/models/concerns/marc_json_extension.rb
@@ -8,4 +8,16 @@ module MarcJsonExtension
   def _marc_format_type
     :json
   end
+
+  def as_json
+    super.merge(marcxml: solrmarc_style_marcxml)
+  end
+
+  def solrmarc_style_marcxml
+    <<-EOXML
+      <?xml version="1.0" encoding="UTF-8"?><collection xmlns="http://www.loc.gov/MARC21/slim">
+        #{export_as_marcxml}
+      </collection>
+    EOXML
+  end
 end
