67c0a2ac63d17309286e17825dc726c13c437962
Chris Beer <cabeer@stanford.edu>
2021-04-16 10:04:07 -0700

    Configure blacklight_dynamic_sitemaps (#2649)

--- config/initializers/blacklight_initialize.rb
+++ config/initializers/blacklight_initialize.rb
@@ -2,3 +2,5 @@
 
 # EDS identifier rules (e.g., db__acid) where acid has all sorts of different punctuation
 Blacklight::Engine.config.routes.identifier_constraint = /[^\/]+/
+
+BlacklightDynamicSitemap::Engine.config.last_modified_field = 'last_updated'
