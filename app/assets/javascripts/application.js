ad5e989ff7f4ac86117228ec5dc555764e31b77f
Chris Beer <chris@cbeer.info>
2021-05-07 06:33:57 -0700

    Update to jquery3

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,12 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery3
 //= require honeybadger
 //= require 'blacklight_advanced_search'
 //= require retina_tag
 
-//= require jquery_ujs
+//= require rails-ujs
 //= require turbolinks
 //= require responsiveTruncator
 //= require trunk8
