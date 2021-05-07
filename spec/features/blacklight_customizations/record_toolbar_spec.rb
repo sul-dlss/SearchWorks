72a02354376d35338e4b384d16bc5cf01d399682
Chris Beer <chris@cbeer.info>
2021-05-07 07:04:25 -0700

    the bookmark markup changed?

--- spec/features/blacklight_customizations/record_toolbar_spec.rb
+++ spec/features/blacklight_customizations/record_toolbar_spec.rb
@@ -23,7 +23,7 @@ feature "Record Toolbar" do
       within "div.navbar-collapse" do
         expect(page).to have_css("li a", text: "Cite")
         expect(page).to have_css("li button", text: "Send to")
-        expect(page).to have_css("form label", text: "Select")
+        expect(page).to have_button("Select")
       end
     end
   end
