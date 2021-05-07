542d7e5ef356879c3a3757df4d28df6eccad8813
Chris Beer <chris@cbeer.info>
2021-05-07 10:04:53 -0700

    Fix jsonpresenter signature

--- app/controllers/concerns/backend_lookup.rb
+++ app/controllers/concerns/backend_lookup.rb
@@ -7,10 +7,7 @@ module BackendLookup
     (@response, @document_list) = search_service.search_results
     respond_to do |format|
       format.json do
-        @presenter = Blacklight::JsonPresenter.new(@response,
-                                                   @document_list,
-                                                   facets_from_request,
-                                                   blacklight_config)
+        @presenter = Blacklight::JsonPresenter.new(@response, blacklight_config)
       end
       format.html { render status: :bad_request, layout: false, file: Rails.root.join('public', '500.html') }
     end
