90191270590ea6e98ff3c335b719116f28ceb8c4
Chris Beer <chris@cbeer.info>
2021-08-12 09:18:13 -0700

    identifier_constraint configuration is obsolete and removed in Blacklight 7

--- config/initializers/blacklight_initialize.rb
+++ config/initializers/blacklight_initialize.rb
@@ -1,6 +1,1 @@
-# Blacklight.secret_key = Settings.BLACKLIGHT_SECRET_KEY
-
-# EDS identifier rules (e.g., db__acid) where acid has all sorts of different punctuation
-Blacklight::Engine.config.routes.identifier_constraint = /[^\/]+/
-
 BlacklightDynamicSitemap::Engine.config.last_modified_field = 'last_updated'
