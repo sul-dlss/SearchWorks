f130f8328de4d3f0926e4232a7b328d8b93f15e2
Chris Beer <chris@cbeer.info>
2021-08-12 09:19:35 -0700

    Wrap email @documents in an array to match current expectations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -201,6 +201,7 @@ class ArticlesController < ApplicationController
   def email
     # TODO: Handle arrays of IDs in future selection work
     @response, @documents = fetch(params[:id])
+    @documents = Array.wrap(@documents)
     if request.post? && validate_email_params_and_recaptcha
       send_emails_to_all_recipients
 
