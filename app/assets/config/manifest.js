0b7191006bd8cb5804ef51014f06bf3d1972df89
Chris Beer <chris@cbeer.info>
2021-08-17 08:13:19 -0700

    Update to Rails 7

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,5 @@
+//= link_tree ../images
+//= link_tree ../fonts
+//= link application.js
+//= link application.css
+//= link print.css
---
+++ config/initializers/zeitwerk.rb
@@ -0,0 +1,4 @@
+ActiveSupport::Inflector.inflections(:en) do |inflect|
+  inflect.acronym 'CJK'
+  inflect.acronym 'IP'
+end
