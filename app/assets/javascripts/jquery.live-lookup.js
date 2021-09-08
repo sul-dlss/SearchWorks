8d33dc9823da832cab8846761b067d257b1de56a
Chris Beer <chris@cbeer.info>
2021-09-08 11:39:59 -0700

    Open item-level request links in a new tab

--- app/assets/javascripts/jquery.live-lookup.js
+++ app/assets/javascripts/jquery.live-lookup.js
@@ -38,10 +38,9 @@
               }
               if ( dom_item.data('request-url') ) {
                 var link = $(
-                  "<a rel='nofollow' class='btn btn-default btn-xs request-button' title='Opens in new tab' href='" + dom_item.data('request-url') + "'>Request <span class='sr-only'>(opens in new tab)</span></a>"
+                  "<a rel='nofollow' class='btn btn-default btn-xs request-button' title='Opens in new tab' target='_blank' href='" + dom_item.data('request-url') + "'>Request <span class='sr-only'>(opens in new tab)</span></a>"
                 );
                 $('.request-link', dom_item).html(link);
-                link.requestsModal();
               }
             }
             if ( !live_data.due_date && dom_item.length > 0  && target.hasClass('unknown')) {
