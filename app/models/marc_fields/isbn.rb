df38be2c55f85c0a3566abf09006602350d4fb1c
Chris Beer <chris@cbeer.info>
2022-03-18 09:28:02 -0700

    Override #display_value instead of handling all the field parsing in Isbn

--- app/models/marc_fields/isbn.rb
+++ app/models/marc_fields/isbn.rb
@@ -2,15 +2,11 @@
 # A class to handle MARC 020 (display everything but subfield c)
 # https://www.loc.gov/marc/bibliographic/bd020.html
 class Isbn < MarcField
-  def values
-    return [] if marc.blank?
-
-    @values ||= extracted_fields.flat_map do |_field, subfields|
-      subfields.reject { |s| s.code == 'c' }.map(&:value).map(&:strip)
-    end.compact
-  end
-
   private
+
+  def display_value(field, subfields)
+    super(field, subfields.reject { |s| s.code == 'c' })
+  end
 
   def tags
     %w[020]
