18f78ef8175868cc2355cd8f5ec9553c4def999f
Chris Beer <chris@cbeer.info>
2022-07-04 09:12:21 -0700

    Define a custom component to inject resource type icons

---
+++ app/components/resource_facet_item_component.rb
@@ -0,0 +1,13 @@
+class ResourceFacetItemComponent < Blacklight::FacetItemComponent
+  def render_facet_value(...)
+    resource_icon + super
+  end
+
+  def render_selected_facet_value(...)
+    resource_icon + super
+  end
+
+  def resource_icon
+    helpers.render_resource_icon(@facet_item.value)
+  end
+end
