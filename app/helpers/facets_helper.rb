e6e0df9625b7e73ed3a3d67ee45c435a5caa58a2
Chris Beer <chris@cbeer.info>
2022-07-05 10:09:56 -0700

    Remove unused facet overrides

--- app/helpers/facets_helper.rb
+++ app/helpers/facets_helper.rb
@@ -1,18 +1,6 @@
 module FacetsHelper
   include Blacklight::FacetsHelperBehavior
 
-  def collapse_home_page_facet?(facet)
-    ['language', 'building_facet', 'format_main_ssim'].include? facet.field
-  end
-
-  def render_single_facet(facet_name, options = {})
-    facet = @response.aggregations.values.find do |facet|
-      facet.name == facet_name
-    end
-
-    render_facet_limit(facet, options) if facet
-  end
-
   def render_resource_icon(values)
     values = Array(values).flatten.compact
     values.delete("Database") if values.length > 1
--- spec/helpers/facets_helper_spec.rb
+++ spec/helpers/facets_helper_spec.rb
@@ -1,42 +1,6 @@
 require "spec_helper"
 
 describe FacetsHelper do
-  describe "#render_single_facet" do
-    before do
-      @response = OpenStruct.new(aggregations: {})
-    end
-
-    it "should return nil if there is no facet available" do
-      expect(helper.send(:render_single_facet, "not_a_facet")).to be_nil
-    end
-    it "should render the facet limit when the facet does exist " do
-      the_facet = OpenStruct.new(name: "this_facet")
-      @response.aggregations = { this_facet: the_facet }
-      expect(helper).to receive(:render_facet_limit).with(the_facet, {}).and_return("a-partial")
-      expect(helper.send(:render_single_facet, "this_facet")).to eq "a-partial"
-    end
-    it "should pass options onto #render_facet_limit" do
-      the_facet = OpenStruct.new(name: "this_facet")
-      @response.aggregations = { this_facet: the_facet }
-      options = { partial: "the-partial-to-render" }
-      expect(helper).to receive(:render_facet_limit).with(the_facet, options).and_return("a-partial")
-
-      expect(helper.send(:render_single_facet, "this_facet", options)).to eq "a-partial"
-    end
-  end
-
-  describe "#collapse_home_page_facet?" do
-    let(:collapse_facet) { OpenStruct.new(field: 'building_facet') }
-    let(:non_collapse_facet) { OpenStruct.new(field: 'other_facet') }
-
-    it "should identify building_facet to be collapsed on the home page" do
-      expect(collapse_home_page_facet?(collapse_facet)).to be_truthy
-    end
-    it "should identify other facets to not be collapsed on the home page" do
-      expect(collapse_home_page_facet?(non_collapse_facet)).to be_falsey
-    end
-  end
-
   describe "#render_resource_icon" do
     it "should not render anything if format_main_ssim field is not present" do
       expect(helper.render_resource_icon(nil)).to be_nil
