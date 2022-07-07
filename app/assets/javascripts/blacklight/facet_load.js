769ea145a48e7ec5847f8bcd089230c7099ff22c
Chris Beer <chris@cbeer.info>
2022-07-07 14:22:02 -0700

    Stash

--- app/assets/javascripts/blacklight/facet_load.js
+++
@@ -1,5 +0,0 @@
-/*global Blacklight */
-
-// We don't want facet_load at all, but need to keep the file so that Blacklight's default facet_load doesn't perform
-// Can delete this override file if facet_load is removed from BL
-(function() {})();
