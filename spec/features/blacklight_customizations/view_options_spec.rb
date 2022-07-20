43a5661ac68e7927ce71047844acf0255f4b93dc
Chris Beer <chris@cbeer.info>
2022-07-20 07:02:53 -0700

    Lower-case the view labels in the i18n file instead of doing it on-the-fly with css

--- spec/features/blacklight_customizations/view_options_spec.rb
+++ spec/features/blacklight_customizations/view_options_spec.rb
@@ -7,13 +7,13 @@ feature "View options" do
     click_button 'search'
 
     within('#view-type-dropdown') do
-      expect(page).to have_css("li a.view-type-list span.view-type-label", text: 'Normal')
+      expect(page).to have_css("li a.view-type-list span.view-type-label", text: 'normal')
       expect(page).to have_css("li a.view-type-list i.fa.fa-th-list")
 
-      expect(page).to have_css("li a.view-type-gallery span.view-type-label", text: 'Gallery')
+      expect(page).to have_css("li a.view-type-gallery span.view-type-label", text: 'gallery')
       expect(page).to have_css("li a.view-type-gallery i.fa.fa-th")
 
-      expect(page).to have_css("li a.view-type-brief span.view-type-label", text: 'Brief')
+      expect(page).to have_css("li a.view-type-brief span.view-type-label", text: 'brief')
       expect(page).to have_css("li a.view-type-brief i.fa.fa-align-justify")
     end
   end
