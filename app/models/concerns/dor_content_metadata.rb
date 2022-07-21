40f689c8a06c623273be33f8941bfc1a20621ae1
Cory Lown <corylown@stanford.edu>
2022-07-21 12:02:38 -0400

    Add published_content? method to SolrDocument

---
+++ app/models/concerns/dor_content_metadata.rb
@@ -0,0 +1,9 @@
+module DorContentMetadata
+  # Document has (or could have) published content if:
+  #   dor_resource_count_isi field is present and positive (resource has published content)
+  #   OR if dor_resource_count_isi field is not present (could have published content --
+  #   for records that have not been re-indexed)
+  def published_content?
+    self['dor_resource_count_isi']&.positive? || !key?('dor_resource_count_isi')
+  end
+end
