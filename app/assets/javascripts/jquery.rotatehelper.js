1f8dfeb9afe76131b78ba966798cb417e214f644
Marlo Longley <mlongley@stanford.edu>
2022-08-04 16:38:07 -0700

    Fix icon rotation on toggle in mobile menu
    Fixes #1461

--- app/assets/javascripts/jquery.rotatehelper.js
+++ app/assets/javascripts/jquery.rotatehelper.js
@@ -19,16 +19,27 @@
           $toRotate,
           $parent,
           cssRotate90cw = 'search-navbar-dropdown-btn-rotate-90',
+          cssRotate270cw = 'search-navbar-dropdown-btn-rotate-270',
           cssRotate;
 
       cssRotate   = getRotateCssClass();
       $parent     = getParent();
       $toRotate = getRotatableElement();
 
-      $parent.on('show.bs.collapse', function() { $toRotate.addClass(cssRotate); });
-      $parent.on('show.bs.dropdown', function() { $toRotate.addClass(cssRotate); });
-      $parent.on('hide.bs.collapse', function() { $toRotate.removeClass(cssRotate); });
-      $parent.on('hide.bs.dropdown', function() { $toRotate.removeClass(cssRotate); });
+      // in SearchWorks the main Menu in .navbar-header (mobile) uses data-toggle="collapse" 
+      $parent.on('show.bs.collapse', function() { $toRotate.addClass(cssRotate90cw); });
+      $parent.on('hide.bs.collapse', function() { $toRotate.removeClass(cssRotate90cw); });
+
+      // in SearchWorks nested menu items beneath the main menu (mobile) use data-toggle="dropdown" 
+      // Because they are nested, stop event propation so they only impact the intended element
+      $parent.on('show.bs.dropdown', function(e) {
+        e.stopImmediatePropagation(); 
+        $toRotate.removeClass(cssRotate270cw); 
+      });
+      $parent.on('hide.bs.dropdown', function(e) {  
+        e.stopImmediatePropagation(); 
+        $toRotate.addClass(cssRotate270cw); 
+      });
 
       function getParent() {
         var $parent = $button.parent(),
--- app/assets/javascripts/search_navbar_services_menu.js
+++ app/assets/javascripts/search_navbar_services_menu.js
@@ -1,6 +1,10 @@
 Blacklight.onLoad(function() {
 
-  $('#search-navbar .navbar-header .dropdown-toggle').rotateHelper();
+  // top level Menu in navbar-header
   $('.navbar-toggle').rotateHelper();
+
+  // inner dropdown toggles such as Help under the Menu
+  // initial state is caret triangle pointed right at 270deg
+  $('#search-subnavbar-collapse .navbar-nav .dropdown-toggle').addClass('search-navbar-dropdown-btn-rotate-270').rotateHelper();
 
 });
