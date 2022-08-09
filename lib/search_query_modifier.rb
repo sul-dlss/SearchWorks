b182abb2e880cf4bb37aa5e52d22111a19f715d5
Chris Beer <chris@cbeer.info>
2022-08-09 06:48:13 -0700

    Remove dead code

--- lib/search_query_modifier.rb
+++ lib/search_query_modifier.rb
@@ -5,20 +5,6 @@ class SearchQueryModifier
     @config = @search_state.blacklight_config
   end
 
-  def present?
-    has_filters_and_query? || fielded_search? || query_has_stopwords?
-  end
-
-  def params_without_stopwords
-    @params.merge(q: query_without_stopwords)
-  end
-
-  def query_has_stopwords?
-    has_query? && @params[:q].split.any? do |word|
-      SearchQueryModifier.stopwords.include?(word.downcase)
-    end
-  end
-
   def params_without_fielded_search_and_filters
     @params.merge(search_field: nil, f: nil, range: nil).except(:controller, :action)
   end
@@ -37,14 +23,6 @@ class SearchQueryModifier
     @params.merge(f: nil, range: nil).except(:controller, :action)
   end
 
-  def selected_filter_labels
-    if has_filters?
-      @config.facet_fields.values.map do |facet|
-        "#{facet.label} > #{@params[:f][facet.field].join(', ')}" if @params[:f].present? && @params[:f][facet.field].present?
-      end.compact.join('+ ')
-    end
-  end
-
   def has_filters?
     @params[:f].present? || @params[:range].present?
   end
@@ -53,21 +31,9 @@ class SearchQueryModifier
     @params[:q].present?
   end
 
-  def self.stopwords
-    %w{and of the}
-  end
-
   private
 
   def has_filters_and_query?
     has_filters? && has_query?
-  end
-
-  def query_without_stopwords
-    if has_query?
-      @params[:q].split.reject do |word|
-        SearchQueryModifier.stopwords.include?(word.downcase)
-      end.join(' ')
-    end
   end
 end
--- spec/lib/search_query_modifier_spec.rb
+++ spec/lib/search_query_modifier_spec.rb
@@ -4,42 +4,6 @@ describe SearchQueryModifier do
   let(:default_config) { {} }
   let(:stopwords_query) { SearchQueryModifier.new(Blacklight::SearchState.new({ q: "And we have stopwords oF THE month", other: 'something' }, default_config)) }
 
-  describe "#present?" do
-    it "should return true when there is a facet and query" do
-      expect(SearchQueryModifier.new(Blacklight::SearchState.new({ q: "hello", f: { something: '' } }, default_config))).to be_present
-    end
-    it "should return true when the search is fielded" do
-      config = OpenStruct.new(default_search_field: OpenStruct.new(field: 'search'))
-      expect(SearchQueryModifier.new(Blacklight::SearchState.new({ q: "hello", search_field: 'someting_else' }, config))).to be_present
-    end
-    it "should return true when there are stopwords" do
-      expect(stopwords_query).to be_present
-    end
-    it "should return false when there is only a facet OR query" do
-      expect(SearchQueryModifier.new(Blacklight::SearchState.new({ q: "hello" }, default_config))).not_to be_present
-      expect(SearchQueryModifier.new(Blacklight::SearchState.new({ f: { something: '' } }, default_config))).not_to be_present
-    end
-  end
-
-  describe "stopwords" do
-    let(:no_stopwords_query) { SearchQueryModifier.new(Blacklight::SearchState.new({ q: "This query does not have stopwords", other: 'something' }, default_config)) }
-
-    describe "#params_without_stopwords" do
-      it "should replace the 'q' parameter in the options with one that doesn't contain stopwords" do
-        expect(stopwords_query.params_without_stopwords).to eq({ 'q' => "we have stopwords month", 'other' => 'something' })
-      end
-    end
-
-    describe "#query_has_stopwords?" do
-      it "should return true when a query contains stopwords" do
-        expect(stopwords_query.query_has_stopwords?).to be_truthy
-      end
-      it "should return false when a query does not contain stopwords" do
-        expect(no_stopwords_query.query_has_stopwords?).to be_falsey
-      end
-    end
-  end
-
   describe "fielded search" do
     let(:config) { OpenStruct.new(default_search_field: OpenStruct.new(field: 'search')) }
     let(:fielded_search) { SearchQueryModifier.new(Blacklight::SearchState.new({ search_field: "search_title", q: 'something', f: 'else' }, config)) }
@@ -117,15 +81,6 @@ describe SearchQueryModifier do
       it "should return the parameters hash without a range param" do
         expect(ranged_search.params_without_fielded_search[:q]).to eq 'something'
         expect(ranged_search.params_without_filters[:range]).not_to be_present
-      end
-    end
-
-    describe "#selected_filter_labels" do
-      it "should return labels as a string" do
-        expect(filtered_search.selected_filter_labels).to eq "A field > Something+ Another field > Something Else"
-      end
-      it "should handle when there is no f parameter" do
-        expect(ranged_search.selected_filter_labels).to eq ''
       end
     end
   end
