5ccdee462aa44b0892ccafd019a89916ffdece7a
Chris Beer <chris@cbeer.info>
2022-08-08 17:22:34 -0700

    Use consistent argument ordering + naming

--- app/models/nearby_on_shelf.rb
+++ app/models/nearby_on_shelf.rb
@@ -39,19 +39,19 @@ class NearbyOnShelf
   end
 
   def get_preceding_spines_from_field
-    get_next_spines_from_field(reverse_shelfkey, 'reverse_shelfkey')
+    get_next_spines_from_field('reverse_shelfkey', reverse_shelfkey)
   end
 
   def get_following_spines_from_field
-    get_next_spines_from_field(shelfkey, 'shelfkey')
+    get_next_spines_from_field('shelfkey', shelfkey)
   end
 
   # given a shelfkey or reverse shelfkey (for a lopped call number), get the
   #  text for the next window of nearby items
-  def get_next_spines_from_field(starting_value, field_name)
+  def get_next_spines_from_field(field_name, starting_value)
     number_of_shelfkeys_to_request = per * page.abs + (per / 2).floor
 
-    desired_values = get_next_terms(starting_value, field_name, number_of_shelfkeys_to_request).keys
+    desired_values = get_next_terms(field_name, starting_value, number_of_shelfkeys_to_request).keys
 
     # perform client-side windowing to get the desired number of items
     # because the solr terms component will include everything between the current
@@ -61,39 +61,26 @@ class NearbyOnShelf
     get_spines_from_field_values(field_name, desired_values)
   end
 
-  # create an array of sorted html list items containing the appropriate display text
-  #  (analogous to what would be visible if you were looking at the spine of
-  #  a book on a shelf) from relevant solr docs, given a particular solr
-  #  field and value for which to retrieve spine info.
-  # Each html list item must match a desired value
-  def get_spines_from_field_values(field, desired_values)
+  def get_spines_from_field_values(field_name, desired_values)
     # Get the documents for a set of shelf keys
     response, docs = search_service.search_results do |builder|
-      builder.where(field => desired_values.compact)
+      builder.where(field_name => desired_values.compact)
     end
 
     spines = docs.flat_map do |doc|
-      get_spines_from_doc(doc, field, desired_values.compact)
+      get_spines_from_doc(doc, field_name, desired_values.compact)
     end
 
     spines.uniq { |spine| spine[:sort_key] }.sort_by { |spine| spine[:sort_key] }
   end
 
-  # create a hash with
-  #     key = sorting key for the spine,
-  #     value = the html list item containing appropriate display text
-  #  (analogous to what would be visible if you were looking at the spine of
-  #  a book on a shelf) from a solr doc.
-  #   spine is:  <li> title [(pub year)] [<br/> author] <br/> callnum </li>
-  # Each element of the hash must match a desired value in the
-  #   desired_values array for the indicated piece (shelfkey or reverse shelfkey)
-  def get_spines_from_doc(doc, field, desired_values)
-    return [] if doc[:item_display].nil?
-
+  # @param [SolrDocument] doc
+  # @param [String] field_name
+  # @param [Array<String>] desired_values the shelfkeys or reverse shelfkeys to filter on
+  def get_spines_from_doc(doc, field_name, desired_values)
     # This winnows down the holdings hashs on only ones where the desired values includes the shelfkey or reverse shelfkey using a very quick select statment
-    # The resulting array looke like [[:"36105123456789",{:barcode=>"36105123456789",:callnumber=>"PS3156 .A53"}]]
     item_array = doc.holdings.callnumbers.select do |callnumber|
-      desired_values.include?(callnumber.send(field))
+      desired_values.include?(callnumber.send(field_name))
     end
 
     item_array.map do |callnumber|
@@ -101,10 +88,10 @@ class NearbyOnShelf
     end
   end
 
-  def get_next_terms(field, curr_value, how_many)
+  def get_next_terms(field_name, curr_value, how_many)
     # TermsComponent Query to get the terms
     solr_params = {
-      'terms.fl' => field,
+      'terms.fl' => field_name,
       'terms.lower' => curr_value,
       'terms.lower.incl' => false,
       'terms.sort' => 'index',
@@ -114,7 +101,7 @@ class NearbyOnShelf
 
     solr_response = Blacklight.default_index.connection.alphaTerms({ params: solr_params })
 
-    solr_response.dig('terms', field) || {}
+    solr_response.dig('terms', field_name) || {}
   end
 
   def current_callnumber
