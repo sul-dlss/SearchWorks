68b764e25f631b42c880e47d5e324e8da2891954
Justin Coyne <jcoyne@justincoyne.com>
2022-11-21 07:54:33 -0600

    Remove unnecessary rubocop disable

--- app/controllers/quick_reports_controller.rb
+++ app/controllers/quick_reports_controller.rb
@@ -2,7 +2,7 @@ class QuickReportsController < ApplicationController
   def create
     if request.post?
       FeedbackMailer.submit_wrong_book_cover(params, request.remote_ip).deliver_now
-      flash[:success] = t("blacklight.feedback_form.success") # rubocop:disable Rails/ActionControllerFlashBeforeRender
+      flash[:success] = t("blacklight.feedback_form.success")
       respond_to do |format|
         format.json do
           render json: flash
