4fc9794c9f5d8d66a1695a185dece1cc7d50bacc
Cory Lown <corylown@stanford.edu>
2022-12-12 08:45:16 -0500

    Autocorrect rubocop violations

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,7 +17,7 @@ Capybara.javascript_driver = :headless_chrome
 
 Capybara.register_driver :headless_chrome do |app|
   Capybara::Selenium::Driver.load_selenium
-  browser_options = ::Selenium::WebDriver::Chrome::Options.new.tap do |opts|
+  browser_options = Selenium::WebDriver::Chrome::Options.new.tap do |opts|
     opts.args << '--headless'
     opts.args << '--disable-gpu'
     # Workaround https://bugs.chromium.org/p/chromedriver/issues/detail?id=2650&q=load&sort=-id&colspec=ID%20Status%20Pri%20Owner%20Summary
