8327872fa51d561d8ffa8f14f7a8c9093b7f606b
Chris Beer <chris@cbeer.info>
2023-01-30 15:31:37 -0800

    Allow item public notes to wrap at a punctuation mark

--- app/components/access_panels/library_location_component.rb
+++ app/components/access_panels/library_location_component.rb
@@ -13,5 +13,11 @@ module AccessPanels
     def location_request_link
       @location_request_link ||= LocationRequestLinkComponent.for(document: document, library: library.code, location: location.code, items: location.items)
     end
+
+    # encourage long lines to wrap at punctuation
+    # Note: the default line break character is the zero-width space
+    def inject_line_break_opportunities(text, line_break_character: '​')
+      text.gsub(/([:,;.]+)/, "\\1#{line_break_character}")
+    end
   end
 end
