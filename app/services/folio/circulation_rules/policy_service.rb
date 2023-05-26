ae49eb7126c04b700eac87db5675d97e4db2169e
Nick Budak <budak@stanford.edu>
2023-05-26 11:13:28 -0700

    Support querying for specific item policies using circ rules
    Ref #3149

--- app/services/folio/circulation_rules/policy_service.rb
+++ app/services/folio/circulation_rules/policy_service.rb
@@ -3,6 +3,11 @@
 module Folio
   module CirculationRules
     CIRC_RULES_FILE_PATH = Rails.root.join('config/circulation_rules.txt').freeze
+    REQUEST_POLICY_FILE_PATH = Rails.root.join('config/request_policies.json').freeze
+    LOAN_POLICY_FILE_PATH = Rails.root.join('config/loan_policies.json').freeze
+    OVERDUE_POLICY_FILE_PATH = Rails.root.join('config/overdue_policies.json').freeze
+    LOST_POLICY_FILE_PATH = Rails.root.join('config/lost_policies.json').freeze
+    NOTICE_POLICY_FILE_PATH = Rails.root.join('config/notice_policies.json').freeze
 
     class PolicyService
       # Load the circulation rules file and parse it into a set of ordered rules
@@ -14,9 +19,53 @@ module Folio
         end
       end
 
-      # Provide custom rules or use the default set
-      def initialize(rules: nil)
-        @index = Folio::CirculationRules::Index.new(rules || self.class.default_rules)
+      # Load policy definitions from static JSON files and index by UUID
+      def self.policies
+        {
+          request: JSON.parse(REQUEST_POLICY_FILE_PATH.read).index_by { |p| p['id'] },
+          loan: JSON.parse(LOAN_POLICY_FILE_PATH.read).index_by { |p| p['id'] },
+          overdue: JSON.parse(OVERDUE_POLICY_FILE_PATH.read).index_by { |p| p['id'] },
+          lost: JSON.parse(LOST_POLICY_FILE_PATH.read).index_by { |p| p['id'] },
+          notice: JSON.parse(NOTICE_POLICY_FILE_PATH.read).index_by { |p| p['id'] }
+        }
+      end
+
+      attr_reader :index, :policies
+
+      # Provide custom rules and policies or use the defaults
+      def initialize(rules: nil, policies: nil)
+        @index = Folio::CirculationRules::Index.new(rules || self.class.rules)
+        @policies = policies || self.class.policies
+      end
+
+      # Return the request policy for the given Holdings::Item
+      def item_request_policy(item)
+        rule = item_rule(item)
+        @policies[:request].fetch(rule.policy['request'], nil)
+      end
+
+      # Return the loan policy for the given Holdings::Item
+      def item_loan_policy(item)
+        rule = item_rule(item)
+        @policies[:loan].fetch(rule.policy['loan'], nil)
+      end
+
+      # Return the overdue fine policy for the given Holdings::Item
+      def item_overdue_policy(item)
+        rule = item_rule(item)
+        @policies[:overdue].fetch(rule.policy['overdue'], nil)
+      end
+
+      # Return the lost item policy for the given Holdings::Item
+      def item_lost_policy(item)
+        rule = item_rule(item)
+        @policies[:lost].fetch(rule.policy['lost-item'], nil)
+      end
+
+      # Return the patron notice policy for the given Holdings::Item
+      def item_notice_policy(item)
+        rule = item_rule(item)
+        @policies[:notice].fetch(rule.policy['notice'], nil)
       end
 
       # Find the circulation rule that applies to the given Holdings::Item
--- spec/services/folio/circulation_rules/policy_service_spec.rb
+++ spec/services/folio/circulation_rules/policy_service_spec.rb
@@ -91,4 +91,92 @@ RSpec.describe Folio::CirculationRules::PolicyService do
       end
     end
   end
+
+  describe 'policy querying' do
+    let(:rules) {
+      [
+        Folio::CirculationRules::Rule.new(
+          { 'loan-type' => 'can-circ' },
+          {
+            'request' => 'allow-all',
+            'loan' => '3day-norenew-15mingrace',
+            'lost-item' => '30fee',
+            'notice' => 'short',
+            'overdue' => '150-1050'
+          }
+        ),
+        Folio::CirculationRules::Rule.new(
+          {},
+          {
+            'request' => 'no-requests',
+            'loan' => '28day-norenew-1daygrace',
+            'lost-item' => 'norepl',
+            'notice' => 'default',
+            'overdue' => 'nofine'
+          }
+        )
+      ]
+    }
+
+    let(:policies) {
+      {
+        request: {
+          'allow-all' => 'Allow all requests',
+          'no-requests' => 'No requests allowed'
+        },
+        loan: {
+          '3day-norenew-15mingrace' => '3 day loan, no renewals, 15 minute grace period',
+          '28day-norenew-1daygrace' => '28 day loan, no renewals, 1 day grace period'
+        },
+        lost: {
+          'norepl' => 'No replacement',
+          '30fee' => '$30 lost fee policy'
+        },
+        notice: {
+          'short' => 'Short-term loan notice policy',
+          'default' => 'Default notice policy'
+        },
+        overdue: {
+          '150-1050' => '1.50/10.50 overdue fine',
+          'nofine' => 'No fines policy'
+        }
+      }
+    }
+
+    let(:item) { instance_double(Holdings::Item) }
+
+    subject(:service) { described_class.new(rules:, policies:) }
+
+    before do
+      allow(item).to receive(:material_type).and_return(Folio::Item::MaterialType.new(id: 'book', name: 'Books'))
+      allow(item).to receive(:loan_type).and_return(Folio::Item::LoanType.new(id: 'can-circ', name: 'Can circulate'))
+      allow(item).to receive(:effective_location).and_return(
+        Folio::Location.new(
+          campus: Folio::Location::Campus.new(id: 'sul', code: 'SUL', name: 'Stanford University Libraries'),
+          library: Folio::Location::Library.new(id: 'green', code: 'GREEN', name: 'Cecil R. Green Library'),
+          location: Folio::Location::Location.new(id: 'green-stacks', code: 'GRE-STACKS', name: 'Green Stacks')
+        )
+      )
+    end
+
+    it 'can fetch the request policy for an item' do
+      expect(service.item_request_policy(item)).to eq('Allow all requests')
+    end
+
+    it 'can fetch the loan policy for an item' do
+      expect(service.item_loan_policy(item)).to eq('3 day loan, no renewals, 15 minute grace period')
+    end
+
+    it 'can fetch the lost policy for an item' do
+      expect(service.item_lost_policy(item)).to eq('$30 lost fee policy')
+    end
+
+    it 'can fetch the notice policy for an item' do
+      expect(service.item_notice_policy(item)).to eq('Short-term loan notice policy')
+    end
+
+    it 'can fetch the overdue policy for an item' do
+      expect(service.item_overdue_policy(item)).to eq('1.50/10.50 overdue fine')
+    end
+  end
 end
