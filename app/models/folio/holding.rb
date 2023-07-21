0284fd681af29329c96c643afd39cf631b57d724
Justin Coyne <jcoyne@justincoyne.com>
2023-07-21 15:05:50 -0500

    Migrate bound-with logic to use folio data
    Ref #3129

--- app/models/folio/holding.rb
+++ app/models/folio/holding.rb
@@ -1,15 +1,21 @@
 module Folio
   class Holding
-    attr_reader :id, :effective_location
+    attr_reader :id, :effective_location, :holdings_type
 
-    def initialize(id:, effective_location:)
+    def initialize(id:, effective_location:, holdings_type:)
       @id = id
       @effective_location = effective_location
+      @holdings_type = holdings_type
+    end
+
+    def bound_with?
+      holdings_type == 'Bound-with'
     end
 
     def self.from_dynamic(json)
       new(id: json.fetch('id'),
-          effective_location: Folio::Location.from_dynamic(json.dig('location', 'effectiveLocation')))
+          effective_location: Folio::Location.from_dynamic(json.dig('location', 'effectiveLocation')),
+          holdings_type: json.dig('holdingsType', 'code'))
     end
   end
 end
