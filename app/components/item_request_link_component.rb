414203059deb56c4d0566058dd77b7098aaeb8be
Chris Beer <chris@cbeer.info>
2023-08-02 13:40:38 -0700

    Remove item-level on-site locations; this can't happen in practice

--- app/components/item_request_link_component.rb
+++ app/components/item_request_link_component.rb
@@ -1,34 +1,4 @@
 class ItemRequestLinkComponent < ViewComponent::Base
-  LEGACY_ONSITE_LOCATIONS = %w(PAGE-AR PAGE-AS PAGE-EA PAGE-ED PAGE-EN PAGE-ES PAGE-GR
-                               PAGE-HA PAGE-HP PAGE-HV PAGE-LW PAGE-MA PAGE-MD PAGE-MU PAGE-RM PAGE-SP).freeze
-
-  FOLIO_ONSITE_LOCATIONS = %w(
-    SAL3-PAGE-AR
-    ART-PAGE-AR
-    SAL3-PAGE-AS
-    SAL3-PAGE-EA
-    EAL-PAGE-EA
-    SAL-PAGE-EA
-    SAL3-PAGE-ED
-    EDU-PAGE-ED
-    SAL3-PAGE-EN
-    ENG-PAGE-EN
-    SAL3-PAGE-ES
-    EAR-PAGE-ES
-    SAL-PAGE-GR
-    SAL3-PAGE-GR
-    GRE-PAGE-GR
-    MAR-PAGE-MA
-    SAL3-PAGE-MA
-    SAL3-PAGE-MD
-    MEDIA-PAGE-MD
-    SAL3-PAGE-MU
-    MUS-PAGE-MU
-    SAL3-PAGE-RM
-    RUM-PAGE-RM
-    SAL3-PAGE-SP
-  ).freeze
-
   attr_reader :item, :classes
 
   delegate :document, :library, :home_location, :barcode, to: :item
@@ -70,17 +40,6 @@ class ItemRequestLinkComponent < ViewComponent::Base
   end
 
   def link_text
-    locale_key = if on_site_location?
-                   'request_on_site' # e.g. "Request on-site access"
-                 else
-                   'default'
-                 end
-
-    t(locale_key, default: :default, scope: 'searchworks.request_link')
-  end
-
-  def on_site_location?
-    (item.folio_item? && FOLIO_ONSITE_LOCATIONS.include?(item.effective_location.location.code)) ||
-      LEGACY_ONSITE_LOCATIONS.include?(home_location)
+    t('searchworks.request_link.default')
   end
 end
