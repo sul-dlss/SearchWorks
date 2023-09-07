360144a1716296037431bb93dbab9874f4b5e36b
Chris Beer <chris@cbeer.info>
2023-09-07 10:47:14 -0700

    Configure BlacklightDynamicSitemap to use the export handler

--- config/initializers/blacklight_initialize.rb
+++ config/initializers/blacklight_initialize.rb
@@ -1,1 +1,12 @@
 BlacklightDynamicSitemap::Engine.config.last_modified_field = 'last_updated'
+
+if Settings.dynamic_sitemap_solr_endpoint == 'export'
+  BlacklightDynamicSitemap::Engine.config.solr_endpoint = Settings.dynamic_sitemap_solr_endpoint
+  BlacklightDynamicSitemap::Engine.config.modify_show_params = lambda do |id, _default_params|
+    {
+      sort: 'id asc',
+      q: "{!prefix f=hashed_id_ssi v=#{id}}",
+      fl: 'id, last_updated'
+    }
+  end
+end
