fbbbd574f13904058c25f1464bb78534c23892a8
Justin Coyne <jcoyne@justincoyne.com>
2023-09-25 16:03:13 -0500

    More

--- app/components/metadata_field_layout_component.rb
+++ app/components/metadata_field_layout_component.rb
@@ -1,5 +1,5 @@
 class MetadataFieldLayoutComponent < Blacklight::MetadataFieldLayoutComponent
   def initialize(field:, label_class: '', value_class: '')
-    super(field:, label_class: '', value_class: '')
+    super(field:, label_class: 'col-md-3', value_class: 'col-md-9')
   end
 end
