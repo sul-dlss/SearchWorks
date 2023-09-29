17da45b8da692c5f0c67d85da750f643a8bd0ece
Justin Coyne <jcoyne@justincoyne.com>
2023-09-29 10:02:12 -0500

    Remove unused blackight7_compat_shim.js

--- app/assets/javascripts/blacklight7_compat_shim.js
+++
@@ -1,7 +0,0 @@
-// Blacklight 7 compatibility:
-// if (Blacklight.modal === undefined) {
-//   Blacklight.modal = {};
-// }
-
-// Blacklight.modal.modalSelector = "#ajax-modal";
-// Blacklight.ajaxModal.triggerLinkSelector  = "a[data-ajax-modal~=trigger],a[data-blacklight-modal~=trigger],  a.lightboxLink,a.more_facets_link,.ajax_modal_launch";
