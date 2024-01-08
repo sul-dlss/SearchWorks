af1700db8d5b766b83078292f33bfa773eeab754
Justin Coyne <jcoyne@justincoyne.com>
2024-01-08 13:34:57 -0600

    Rename the main branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,8 +2,12 @@ set :application, 'bento'
 
 set :repo_url, 'https://github.com/sul-dlss/sul-bento-app.git'
 
-# Default branch is :master
-ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call unless ENV['DEPLOY']
+# Default branch is :master so we need to update to main
+if ENV['DEPLOY']
+  set :branch, 'main'
+else
+  ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+end
 
 set :deploy_to, '/opt/app/bento/bento'
 
