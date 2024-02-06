4c265d694a44f253efc0df7978006531c3df0585
Justin Coyne <jcoyne@justincoyne.com>
2024-02-06 10:45:35 -0600

    Add a feature spec for live-lookup
    Fixes #3882

--- app/models/concerns/live_lookup_ids.rb
+++
@@ -1,8 +0,0 @@
-# Mixin that adds an abstraction layer for bib/instance ids needed for live lookup availability
-
-module LiveLookupIds
-  # for FOLIO use the Instance UUID
-  def live_lookup_id
-    self[:uuid_ssi]
-  end
-end
