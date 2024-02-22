f5303ab14f2948871a596a0fcb6d2362b9b7a873
dnoneill <noneill@stanford.edu>
2024-02-22 16:57:17 -0500

    override blacklight @current_bookmarks for browse view, add tests to check for page refresh

--- spec/features/callnumber_browse_spec.rb
+++ spec/features/callnumber_browse_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe 'Callnumber Browse', :js do
   end
 
   describe 'full browse view' do
-    it 'is successful' do
+    it 'is successful and checkboxes stay checked after refresh' do
       visit solr_document_path('1')
 
       within '.record-browse-nearby' do
@@ -31,6 +31,18 @@ RSpec.describe 'Callnumber Browse', :js do
       end
 
       expect(page).to have_css('h1', text: 'Browse related items')
+
+      bookmarks = all('.toggle-bookmark')
+      bookmarks.first.click
+      bookmarks.last.click
+
+      expect(page).to have_css('.toggle-bookmark.checked')
+
+      page.driver.browser.navigate.refresh
+
+      expect(page).to have_css('.toggle-bookmark.checked')
+
+      expect(page).to have_css('[data-behavior="recent-selections"]', text: 'Selections (2)')
     end
   end
 end
