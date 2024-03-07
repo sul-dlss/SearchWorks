bea6a5cbaf02d39075cea69eff2458a68e4e25b4
Justin Coyne <jcoyne@justincoyne.com>
2024-03-07 17:29:00 -0600

    Display the child bound with records

---
+++ app/controllers/bound_with_children_controller.rb
@@ -0,0 +1,22 @@
+class BoundWithChildrenController < ApplicationController
+  include Blacklight::Configurable
+  include Blacklight::Searchable
+  copy_blacklight_config_from(CatalogController)
+
+  class Builder
+    def initialize(item_id)
+      @item_id = item_id
+    end
+
+    def reverse_merge(extra)
+      extra.merge(q: "bound_with_parent_item_ids_ssim:#{@item_id}", facet: false)
+    end
+  end
+
+  def index
+    @item_id = params.require(:item_id)
+    builder = Builder.new(@item_id)
+    search = search_service.repository.search(builder)
+    @bound_with_children = search.docs
+  end
+end
