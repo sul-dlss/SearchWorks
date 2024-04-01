9fb3bea28a7a5a5591de032469ec6ed20a9906d3
Cory Lown <corylown@stanford.edu>
2024-04-01 07:48:59 -0400

    Run rubocop autocorrect

--- spec/controllers/hours_controller_spec.rb
+++ spec/controllers/hours_controller_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe HoursController do
   describe "GET library" do
     it "should return correct response" do
       get :show, params: { format: "json", id: "GREEN" }
-      expect(response.status).to eq 200
+      expect(response).to have_http_status :ok
     end
   end
 end
