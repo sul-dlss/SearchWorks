7d234ca27c93afae3b6eaeb0fbca440b3abec411
Cory Lown <corylown@stanford.edu>
2024-04-08 14:38:05 -0400

    Update IIIF links to https://iiif.io/

--- spec/components/record/item/mods/metadata_component_spec.rb
+++ spec/components/record/item/mods/metadata_component_spec.rb
@@ -43,7 +43,7 @@ RSpec.describe Record::Item::Mods::MetadataComponent, type: :component do
       end
 
       it 'includes IIIF Drag n Drop link' do
-        expect(page).to have_css 'a.iiif-dnd[href="https://library.stanford.edu/projects/international-image-interoperability-framework/viewers?manifest=example.com"]'
+        expect(page).to have_css 'a.iiif-dnd[href="https://iiif.io?manifest=example.com"]'
       end
     end
   end
