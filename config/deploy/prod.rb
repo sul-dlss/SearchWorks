3d85427ee0733dd9110ebddbb6310459eebbe171
Chris Beer <cabeer@stanford.edu>
2024-07-05 10:19:13 -0700

    Remove db role from other sw webapp boxes.

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -3,10 +3,10 @@
 set :bundle_without, %w[sqlite development test].join(' ')
 
 server 'sw-webapp-a.stanford.edu', user: 'blacklight', roles: %w[web db app courses]
-server 'sw-webapp-b.stanford.edu', user: 'blacklight', roles: %w[web db app]
-server 'sw-webapp-c.stanford.edu', user: 'blacklight', roles: %w[web db app]
-server 'sw-webapp-d.stanford.edu', user: 'blacklight', roles: %w[web db app]
-server 'sw-webapp-e.stanford.edu', user: 'blacklight', roles: %w[web db app]
+server 'sw-webapp-b.stanford.edu', user: 'blacklight', roles: %w[web app]
+server 'sw-webapp-c.stanford.edu', user: 'blacklight', roles: %w[web app]
+server 'sw-webapp-d.stanford.edu', user: 'blacklight', roles: %w[web app]
+server 'sw-webapp-e.stanford.edu', user: 'blacklight', roles: %w[web app]
 
 server 'sw-webapp-bot-a.stanford.edu', user: 'blacklight', roles: %w[web db app]
 server 'sw-webapp-bot-b.stanford.edu', user: 'blacklight', roles: %w[web db app]
