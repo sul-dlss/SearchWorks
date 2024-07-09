58f3736062b6f47199dfbbc47cb33e811456fe4b
Cory Lown <corylown@stanford.edu>
2024-07-09 10:38:26 -0400

    Remove courses role in favor of db role for updating course data

--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -2,9 +2,9 @@
 
 set :bundle_without, %w[sqlite development test].join(' ')
 
-server 'sw-webapp-stage-a.stanford.edu', user: 'blacklight', roles: %w[web db app courses]
-server 'sw-webapp-stage-b.stanford.edu', user: 'blacklight', roles: %w[web db app]
-server 'sw-webapp-stage-c.stanford.edu', user: 'blacklight', roles: %w[web db app]
+server 'sw-webapp-stage-a.stanford.edu', user: 'blacklight', roles: %w[web db app]
+server 'sw-webapp-stage-b.stanford.edu', user: 'blacklight', roles: %w[web app]
+server 'sw-webapp-stage-c.stanford.edu', user: 'blacklight', roles: %w[web app]
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'production'
