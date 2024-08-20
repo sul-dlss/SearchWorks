6e95cfafc1101d9be09ce7781bd1c69c986f4cf1
mlongley <mlongley@stanford.edu>
2024-08-20 14:16:21 -0400

    Update to Rails 7.2 config

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_01_183130) do
+ActiveRecord::Schema[7.2].define(version: 2024_07_01_183130) do
   create_table "bookmarks", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "user_type"
@@ -63,5 +63,4 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_01_183130) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
