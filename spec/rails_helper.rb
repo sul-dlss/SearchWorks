07e2b58268deb422803f46dcc365e49cfb3fcc1a
Steve Taylor <sctaylor@stanford.edu>
2024-08-26 08:12:32 -0700

    Run rubocop autocorrect

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -28,7 +28,7 @@ Capybara.default_max_wait_time = 15
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Rails.root.glob('spec/support/**/*.rb').each { |f| require f }
 
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
