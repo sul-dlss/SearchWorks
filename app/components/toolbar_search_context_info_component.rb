66691617e441880cc0ff31fcfa72061958b3669e
Chris Beer <cabeer@stanford.edu>
2025-03-14 09:44:21 -0700

    stash

--- app/components/toolbar_search_context_info_component.rb
+++ app/components/toolbar_search_context_info_component.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
-class ToolbarSearchContextInfoComponent < Blacklight::SearchContextComponent
+class ToolbarSearchContextInfoComponent < Blacklight::SearchContext::ServerItemPaginationComponent
+  def link_to_next_document(*args)
+    helpers.link_to_next_document(*args)
+  end
+
+  def link_to_previous_document(*args)
+    helpers.link_to_previous_document(*args)
+  end
 end
