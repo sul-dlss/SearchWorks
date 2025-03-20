4fb4a4f79d81a3815a99f75a02e2bca0c199e465
Chris Beer <cabeer@stanford.edu>
2025-03-20 09:58:00 -0700

    Appease rubocop

--- app/helpers/blacklight_helper.rb
+++ app/helpers/blacklight_helper.rb
@@ -4,17 +4,17 @@ module BlacklightHelper
   include Blacklight::BlacklightHelperBehavior
 
   # @return [Integer]
-  def sw_current_per_page
-    (@response.rows if @response && @response.rows > 0) || # rubocop:disable Style/NumericPredicate
-      params.fetch(:per_page, @blacklight_config.default_per_page).to_i
+  def sw_current_per_page(response)
+    (response.rows if response && response.rows > 0) || # rubocop:disable Style/NumericPredicate
+      params.fetch(:per_page, blacklight_config.default_per_page).to_i
   end
 
   ##
   # The available options for results per page, in the style of #options_for_select
   def sw_per_page_options_for_select
-    return [] if @blacklight_config.per_page.blank?
+    return [] if blacklight_config.per_page.blank?
 
-    @blacklight_config.per_page.map do |count|
+    blacklight_config.per_page.map do |count|
       [t(:'blacklight.search.per_page.label', count: count).html_safe, count]
     end
   end
