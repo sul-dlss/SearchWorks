d7283b54ab3d52467064569edb1e43e35cb25bbd
Chris Beer <cabeer@stanford.edu>
2025-03-20 08:09:33 -0700

    Remove unused Searchworks::Document::Email

--- app/models/concerns/searchworks/document/email.rb
+++
@@ -1,7 +0,0 @@
-# -*- encoding : utf-8 -*-
-# frozen_string_literal: true
-
-# Overridding Blacklight's module to provide our own brief email text
-module Searchworks::Document::Email
-  include Blacklight::Document::Email
-end
