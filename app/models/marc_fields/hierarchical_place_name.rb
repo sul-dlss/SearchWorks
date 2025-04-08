4683f9dd2001cb4a56a1f10579ce9e66b5986f74
Chris Beer <cabeer@stanford.edu>
2025-04-08 13:21:21 -0700

    Don't use the -- subfield delimiter for the 752 relator term; it should be separated by a comma from the geography.

--- app/models/marc_fields/hierarchical_place_name.rb
+++ app/models/marc_fields/hierarchical_place_name.rb
@@ -4,6 +4,25 @@
 # A class to handle MARC 752 field logic
 # https://www.loc.gov/marc/bibliographic/bd752.html
 class HierarchicalPlaceName < MarcField
+  def display_value(field, subfields)
+    return if subfields.blank?
+
+    subfields.inject(+'') do |acc, subfield|
+      acc << if acc.blank?
+               ''
+             elsif subfield.code == 'e'
+               if acc.ends_with?(',')
+                 ' '
+               else
+                 ', '
+               end
+             else
+               subfield_delimiter
+             end
+      acc << sanitize_marc_value(subfield_value(field, subfield))
+    end
+  end
+
   private
 
   def tags
--- spec/models/marc_fields/place_name_spec.rb
+++ spec/models/marc_fields/place_name_spec.rb
@@ -5,7 +5,8 @@ require 'rails_helper'
 RSpec.describe 'Place names' do
   include MarcMetadataFixtures
 
-  let(:document) { SolrDocument.new(marc_json_struct: place_name_fixture) }
+  let(:document) { SolrDocument.new(marc_json_struct: fixture) }
+  let(:fixture) { place_name_fixture }
 
   subject(:place_name) { document.marc_field(:hierarchical_place_name) }
 
@@ -20,5 +21,71 @@ RSpec.describe 'Place names' do
   it 'joins subfields with a --' do
     expect(place_name.values.length).to eq 1
     expect(place_name.values.first).to eq 'Florida -- Tampa'
+  end
+
+  context 'with a relator term ($e)' do
+    let(:fixture) do
+      <<-JSON
+        {
+          "leader": "          22        4500",
+          "fields": [
+            {
+              "752": {
+                "ind1": " ",
+                "ind2": " ",
+                "subfields": [
+                  {
+                    "a": "Scotland"
+                  },
+                  {
+                    "d": "Edinburgh,"
+                  },
+                  {
+                    "e": "publication place."
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      JSON
+    end
+
+    it 'appends the relator term to the previous field' do
+      expect(place_name.values.first).to eq 'Scotland -- Edinburgh, publication place.'
+    end
+  end
+
+  context 'with a relator term ($e) not preceded by the appropriate punctuation' do
+    let(:fixture) do
+      <<-JSON
+        {
+          "leader": "          22        4500",
+          "fields": [
+            {
+              "752": {
+                "ind1": " ",
+                "ind2": " ",
+                "subfields": [
+                  {
+                    "a": "England"
+                  },
+                  {
+                    "d": "London"
+                  },
+                  {
+                    "e": "publication place."
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      JSON
+    end
+
+    it 'adds a comma before the relator term' do
+      expect(place_name.values.first).to eq 'England -- London, publication place.'
+    end
   end
 end
