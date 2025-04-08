76bdfc5c4a874f8a66f8fb9a26e20e2de07f909c
Justin Coyne <jcoyne@justincoyne.com>
2025-04-08 16:25:34 -0500

    Use specific capybara methods for links

--- spec/features/top_nav_spec.rb
+++ spec/features/top_nav_spec.rb
@@ -5,12 +5,10 @@ require 'rails_helper'
 RSpec.feature "Top Navigation" do
   scenario "should have navigational links and top menu", :js do
     visit root_path
-    within "#topnav" do
-      within ".header-links" do
-        expect(page).to have_css("a", text: "Login")
-        expect(page).to have_css("a", text: "My Account")
-        expect(page).to have_css("a", text: "Feedback")
-      end
+    within "#topnav .header-links" do
+      expect(page).to have_link "Login"
+      expect(page).to have_link "My Account"
+      expect(page).to have_link "Feedback"
     end
   end
 end
