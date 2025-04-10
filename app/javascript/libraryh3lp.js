d3f586f03acd8e7c1c285b832be497967aff6e6b
Justin Coyne <jcoyne@justincoyne.com>
2025-04-10 08:42:57 -0500

    Refactor libraryh3lp to remove jquery dependency

---
+++ app/javascript/libraryh3lp.js
@@ -0,0 +1,65 @@
+import fetchJsonp from "fetch-jsonp";
+
+function libraryH3lp(element) {
+  const item = element;
+  const link = element.querySelector("a");
+  const icon = element.querySelector("span.image-icon");
+  const jid = element.dataset.jid;
+
+  checkStatus();
+
+  function checkStatus() {
+    const jidSplit = jid.split("@");
+    fetchJsonp(
+      `https://libraryh3lp.com/presence/jid/${jidSplit[0]}/${jidSplit[1]}/js`,
+      {
+        jsonpCallback: "cb",
+      })
+      .then(() => {
+        // The libraryh3lp response sets the jabber_resources at the window level. It doesn't use the callback.
+        window["jabber_resources"].forEach((value) => {
+          if (value.show === "available") {
+            setAsAvailable()
+          } else {
+            setAsUnavailable()
+          }
+        })
+      })
+  }
+
+  function setAsAvailable() {
+    icon.classList.remove("image-icon-message-away")
+    icon.classList.add("image-icon-message-available")
+    link.classList.remove("disabled")
+    item.addEventListener("click", handleAvailableClick)
+  }
+
+  function setAsUnavailable() {
+    icon.classList.remove("image-icon-message-available")
+    icon.classList.add("image-icon-message-away")
+    link.classList.add("disabled")
+    item.removeEventListener("click", handleAvailableClick)
+    item.addEventListener("click", handleUnavailableClick)
+  }
+
+  function handleAvailableClick(e) {
+    e.preventDefault()
+    window.open(
+      `//libraryh3lp.com/chat/${jid}?skin=16208&sounds=true`,
+      "chat",
+      "resizable=1,width=420,height=400"
+    )
+  }
+
+  function handleUnavailableClick(e) {
+    e.preventDefault();
+    return;
+  }
+}
+
+function initializeLibraryH3lp() {
+  const elements = document.querySelectorAll("[data-library-h3lp]");
+  elements.forEach(element => libraryH3lp(element))
+}
+
+Blacklight.onLoad(initializeLibraryH3lp);
