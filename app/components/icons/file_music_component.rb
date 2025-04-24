c682e4008d1194596da7ad473d8b466955ab65a6
dnoneill <noneill@stanford.edu>
2025-04-24 13:45:39 -0400

    migrate sul-icons to blacklight icons

---
+++ app/components/icons/file_music_component.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Icons
+  class FileMusicComponent < Icons::SulIconsComponent
+    self.svg = <<~SVG
+      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" height="1em" width="1em" viewBox="-10 0 522 512">
+      	<path d="M60 512h392v-370l-141 -142h-251v512zM435 495h-358v-478h227l131 132v346zM316 9h-17v145h145v-17h-128v-128zM290 357q-5 -22 -28 -30t-52 -2t-47.5 24.5t-14 41t29 32t53.5 3.5q28 -6 46 -26.5t13 -42.5zM273 360q3 14 -10 29t-36 20t-41 -2t-20.5 -20t10.5 -26.5 t37 -18.5q25 -5 41.5 0.5t18.5 17.5zM274 369l17 1l15 -156q59 5 55 52l17 2q1 -16 -2.5 -28t-10.5 -19t-15.5 -12.5t-17.5 -7.5t-16.5 -3.5t-12.5 -1.5h-5h-7z" />
+      </svg>
+    SVG
+  end
+end
---
+++ app/components/icons/mail_1_component.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Icons
+  class Mail1Component < Icons::SulIconsComponent
+    self.svg = <<~SVG
+      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" height="1em" width="1em" viewBox="-10 0 522 512">
+      	<path d="M495 367v-222q0 -22 -14.5 -36.5t-36.5 -14.5h-376q-22 0 -36.5 14.5t-14.5 36.5v222q0 21 14 36t36 15h377q21 0 36 -15t15 -36zM478 367q0 14 -10 24t-24 10h-377q-7 0 -13 -2.5t-10.5 -7t-7 -11t-2.5 -13.5v-222q0 -16 9 -25t25 -9h376q16 0 25 9t9 25v222zM480 121 l-10 -13l-206 163l-222 -163l-10 13l233 171z" />
+      </svg>
+    SVG
+  end
+end
