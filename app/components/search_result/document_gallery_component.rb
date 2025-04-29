868d453dacf418d737bbc5be6787f610824e6a65
Chris Beer <cabeer@stanford.edu>
2025-04-29 12:18:42 -0700

    Move helper into the component.

--- app/components/search_result/document_gallery_component.rb
+++ app/components/search_result/document_gallery_component.rb
@@ -5,5 +5,9 @@ module SearchResult
     def preview_container_dom_class
       "preview-container-#{document.id}"
     end
+
+    def preview_data_attrs(preview_type, id, target)
+      "data-behavior=\"#{preview_type}\" data-preview-url=\"#{preview_path(id)}\" data-preview-target=\"#{target}\"".html_safe
+    end
   end
 end
