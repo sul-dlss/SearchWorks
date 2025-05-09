e407b054cafbbb5618687deb600f21c1c95e3167
Chris Beer <cabeer@stanford.edu>
2025-05-09 08:48:46 -0700

    Use the default search builder to construct the bound-with query.

--- app/controllers/bound_with_children_controller.rb
+++ app/controllers/bound_with_children_controller.rb
@@ -5,35 +5,35 @@ class BoundWithChildrenController < ApplicationController
   include Blacklight::Searchable
   copy_blacklight_config_from(CatalogController)
 
-  class Builder
-    def initialize(item_id, limit)
-      @item_id = item_id
-      @limit = limit
-    end
-
-    def reverse_merge(extra)
-      extra.merge(q: "bound_with_parent_item_ids_ssim:#{@item_id}", facet: false, rows: @limit)
-    end
+  configure_blacklight do |config|
+    config.default_solr_params = { facet: false, rows: 100, qt: 'search' }
+    config.search_state_fields = %i[id item_id]
+    config.facet_fields.clear
   end
 
   def modal
     @id = params.require(:id)
     @item_id = params.require(:item_id)
-    builder = Builder.new(@item_id, 100)
-    search = search_service.repository.search(builder)
-    @bound_with_children = filtered_children(search.docs)
-    @bound_with_parent = @bound_with_children.first.bound_with_parent
+    @response = search_results(@item_id)
+    @bound_with_children = filtered_children(@response.docs)
+    @bound_with_parent = @bound_with_children.first&.bound_with_parent
   end
 
   def index
     @id = params.require(:id)
     @item_id = params.require(:item_id)
-    @limit = params[:limit] ? params[:limit].to_i : 3
-    builder = Builder.new(@item_id, @limit)
-    search = search_service.repository.search(builder)
-    @number_of_results = search.response['numFound']
-    @bound_with_children = filtered_children(search.docs)
-    @bound_with_parent = @bound_with_children.first.bound_with_parent
+    limit = params[:limit] ? params[:limit].to_i : 3
+    @response = search_results(@item_id, limit: limit)
+    @bound_with_children = filtered_children(@response.docs)
+    @bound_with_parent = @bound_with_children.first&.bound_with_parent
+  end
+
+  private
+
+  def search_results(item_id, limit: 100)
+    search_service.search_results do |builder|
+      builder.where(bound_with_parent_item_ids_ssim: item_id).rows(limit)
+    end
   end
 
   def filtered_children(bound_with_children)
