091e750927a6f161a1bf2e332cc1d1c10b66b999
Justin Coyne <jcoyne@justincoyne.com>
2025-05-21 10:29:23 -0500

    Remove unnecessary function
    The arrow is already present on the markup in the backend

--- app/javascript/controllers/preview_brief_controller.js
+++ app/javascript/controllers/preview_brief_controller.js
@@ -1,6 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 import PreviewContent from '../preview-content'
 
+// Shows previews on the brief results view
 export default class extends Controller {
   static values = {
     url: String
@@ -10,14 +11,9 @@ export default class extends Controller {
 
   showPreview() {
     this.previewTarget.classList.add('preview')
-    this.appendPointer()
     PreviewContent.append(this.urlValue, $(this.previewTarget));
     this.showButtonTarget.hidden = true
     this.hideButtonTarget.hidden = false
-  }
-
-  appendPointer() {
-    this.previewTarget.innerHTML = '<div class="preview-arrow"></div>'
   }
 
   closePreview() {
