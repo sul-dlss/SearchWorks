91b411be8c107c2c6a3fe6c3537b0b9ebaea6aa2
Justin Coyne <jcoyne@justincoyne.com>
2025-05-21 11:40:29 -0500

    Switch to using a more semantic css class for showing brief preview

--- app/javascript/controllers/preview_brief_controller.js
+++ app/javascript/controllers/preview_brief_controller.js
@@ -5,13 +5,13 @@ export default class extends Controller {
   static targets = ["preview", "showButton", "hideButton"]
 
   showPreview() {
-    this.previewTarget.classList.add('preview')
+    this.previewTarget.classList.add('show')
     this.showButtonTarget.hidden = true
     this.hideButtonTarget.hidden = false
   }
 
   closePreview() {
-    this.previewTarget.classList.remove('preview')
+    this.previewTarget.classList.remove('show')
     this.showButtonTarget.hidden = false
     this.hideButtonTarget.hidden = true
   }
