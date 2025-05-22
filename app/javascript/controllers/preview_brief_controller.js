d79476b5c495e514331b19705ebd813fe000aaeb
Justin Coyne <jcoyne@justincoyne.com>
2025-05-22 13:01:43 -0500

    Remove unused include

--- app/javascript/controllers/preview_brief_controller.js
+++ app/javascript/controllers/preview_brief_controller.js
@@ -1,5 +1,4 @@
 import { Controller } from "@hotwired/stimulus"
-import PreviewContent from '../preview-content'
 
 export default class extends Controller {
   static targets = ["preview", "showButton", "hideButton"]
