c56ca9da1c2c7c50a6678c5c0d8c365e614d7d71
Justin Coyne <jcoyne@justincoyne.com>
2025-05-23 11:35:16 -0500

    Remove mini-map

--- spec/views/catalog/record/_mods_metadata_sections.html.erb_spec.rb
+++ spec/views/catalog/record/_mods_metadata_sections.html.erb_spec.rb
@@ -19,34 +19,6 @@ RSpec.describe "catalog/record/_mods_metadata_sections" do
       expect(rendered).to have_css('h3', text: "Bibliographic information")
       expect(rendered).to have_css('h3', text: "Access conditions")
     end
-
-    it "has side nav content handles" do
-      expect(rendered).to have_css(".section#contributors")
-      expect(rendered).to have_css(".section#abstract-contents")
-      expect(rendered).to have_css(".section#subjects")
-      expect(rendered).to have_css(".section#bibliography-info")
-      expect(rendered).to have_css(".section#access-conditions")
-    end
-
-    it "renders side nav content" do
-      expect(rendered).to have_css("ul.side-nav-minimap")
-
-      expect(rendered).to have_css(".side-nav-minimap button i.fa.fa-arrow-up")
-      expect(rendered).to have_css(".side-nav-minimap button i.fa.fa-arrow-down")
-
-      expect(rendered).to have_css(".side-nav-minimap button.contributors")
-      expect(rendered).to have_css(".side-nav-minimap button.abstract-contents")
-      expect(rendered).to have_css(".side-nav-minimap button.subjects")
-      expect(rendered).to have_css(".side-nav-minimap button.bibliography-info")
-      expect(rendered).to have_css(".side-nav-minimap button.access-conditions")
-
-      expect(rendered).to have_css(".side-nav-minimap button span.nav-label", text: "Top")
-      expect(rendered).to have_css(".side-nav-minimap button span.nav-label", text: "Contents")
-      expect(rendered).to have_css(".side-nav-minimap button span.nav-label", text: "Subjects")
-      expect(rendered).to have_css(".side-nav-minimap button span.nav-label", text: "Info")
-      expect(rendered).to have_css(".side-nav-minimap button span.nav-label", text: "Access")
-      expect(rendered).to have_css(".side-nav-minimap button span.nav-label", text: "Bottom")
-    end
   end
 
   context "when no metadata sections are available" do
