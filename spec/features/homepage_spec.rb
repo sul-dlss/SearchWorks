c4aab480bf4143f71ed2d5981c054ce4253bb295
Huda Khan <hudakhan@stanford.edu>
2025-06-09 13:47:52 -0600

    adding tests, updating styling for bold but not too bold

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -7,15 +7,23 @@ RSpec.feature 'Homepage' do
     visit quick_search_path
   end
 
-  scenario 'has links to various search contexts' do
-    within '.bento-panels' do
-      expect(page).to have_link 'Catalog', href: 'https://searchworks.stanford.edu'
-      expect(page).to have_link 'Articles+', href: 'https://searchworks.stanford.edu/articles'
-      expect(page).to have_link 'Guides', href: 'https://guides.library.stanford.edu'
-      expect(page).to have_link 'Library website', href: 'https://library.stanford.edu'
-      expect(page).to have_link 'Exhibits', href: 'https://exhibits.stanford.edu'
-    end
+  scenario 'has top level headings for search tools and other sources' do
+    expect(page).to have_css 'h3', text: 'Stanford University Libraries\' search tools'
+    expect(page).to have_css 'h3', text: 'Other sources searched'
+  end
 
-    expect(page).to have_link 'More search tools', href: 'https://guides.library.stanford.edu/search-services'
+  scenario 'has text for search tools headings' do
+    expect(page).to have_css '.bento-heading', text: 'SearchWorks', count: 2
+    expect(page).to have_css '.fw-bold', text: 'Catalog'
+    expect(page).to have_css '.fw-bold', text: 'Articles'
+    expect(page).to have_css 'div.fw-bold', text: 'Archival Collections at Stanford'
+    expect(page).to have_css 'div.fw-bold', text: 'EarthWorks'
+    expect(page).to have_css 'div.fw-bold', text: 'Spotlight Exhibits'
+  end
+
+  scenario 'has text for other sources searched' do
+    expect(page).to have_css 'div.fw-bold', text: 'Library website'
+    expect(page).to have_css 'div.fw-bold', text: 'Guides'
+    expect(page).to have_css 'div.fw-bold', text: 'Subject specialists'
   end
 end
