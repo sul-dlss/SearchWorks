c8ceb7e1eabaf34d5b6437b7ca458288801aa054
dnoneill <noneill@stanford.edu>
2025-06-12 17:34:58 -0400

    update earthworks bento to match figma design

--- app/services/earthworks_search_service.rb
+++ app/services/earthworks_search_service.rb
@@ -16,13 +16,36 @@ class EarthworksSearchService < AbstractSearchService
     def results
       solr_docs = json['data']
       solr_docs.collect do |doc|
+        format = doc.dig('attributes', 'gbl_resourceClass_sm', 'attributes', 'value')
         SearchResult.new(
           title: doc.dig('attributes', 'title'),
+          format: format,
+          date: date(doc),
+          coverage: coverage(doc),
+          icon: "earthworks/#{format.gsub(' ', '_').downcase}.svg",
           link: format(Settings.earthworks.fetch_url, id: doc['id']),
           author: doc.dig('attributes', 'dc_creator_sm', 'attributes', 'value'),
           description: doc.dig('attributes', 'dc_description_s', 'attributes', 'value')
         )
       end
+    end
+
+    private
+
+    def date(doc)
+      date = doc.dig('attributes', 'gbl_indexYear_im', 'attributes', 'value')
+
+      return if date.blank?
+
+      "Temporal Coverage: #{date}"
+    end
+
+    def coverage(doc)
+      coverage = doc.dig('attributes', 'dct_spatial_sm', 'attributes', 'value')
+
+      return if coverage.blank?
+
+      "Place: #{coverage}"
     end
   end
 end
