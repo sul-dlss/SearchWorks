83fbf98187d3936c96b346f6a71e4d8b960d2aed
Justin Coyne <jcoyne@justincoyne.com>
2025-06-12 15:43:35 -0500

    Remove unused methods

--- spec/searchers/quick_search/article_searcher_spec.rb
+++ spec/searchers/quick_search/article_searcher_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe QuickSearch::ArticleSearcher do
-  subject(:searcher) { described_class.new(HTTP, query, 10) }
+  subject(:searcher) { described_class.new(HTTP, query) }
 
   let(:query) { 'my query' }
   let(:response) { JSON.dump({ response: { docs: [] } }) }
