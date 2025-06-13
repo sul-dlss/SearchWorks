c9d88b6d997880b0dff3704542950a8d777a3c72
Justin Coyne <jcoyne@justincoyne.com>
2025-06-13 13:57:36 -0500

    Move the 100+ display for libguides to the view
    Fixes #820

--- spec/views/search/_module_heading.html.erb_spec.rb
+++ spec/views/search/_module_heading.html.erb_spec.rb
@@ -6,9 +6,10 @@ RSpec.describe 'search/_module_heading' do
   let(:catalog) do
     instance_double(CatalogSearcher, total: total, see_all_link: 'https://searchworks.stanford.edu/articles?q=climate%20change')
   end
+  let(:service_name) { 'catalog' }
 
   before do
-    render 'search/module_heading', service_name: 'catalog', searcher: catalog
+    render 'search/module_heading', service_name:, searcher: catalog
   end
 
   context 'with no results' do
@@ -58,6 +59,17 @@ RSpec.describe 'search/_module_heading' do
       expect(rendered).to have_css('.result-set-heading', text: 'Catalog')
       expect(rendered).to have_css('.result-set-subheading', text: /Physical and digital collections/)
       expect(rendered).to have_link 'See all 4 catalog results'
+    end
+  end
+
+  context 'with 100 results for lib_guides' do
+    let(:total) { 100 }
+    let(:service_name) { 'lib_guides' }
+
+    it 'renders' do
+      expect(rendered).to have_css('.result-set-heading', text: 'Guides')
+      expect(rendered).to have_css('.result-set-subheading', text: /Course- and topic-based guides to collections, tools and services/)
+      expect(rendered).to have_link 'See 100+ lib guides results'
     end
   end
 end
