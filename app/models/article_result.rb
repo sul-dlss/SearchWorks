bca27bdd6993c2ea516502a4752d17aa3bb4b6f9
Justin Coyne <jcoyne@justincoyne.com>
2025-06-18 12:48:20 -0500

    Extract article types/icon mapping

--- app/models/article_result.rb
+++ app/models/article_result.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 class ArticleResult
+  FORMAT_TO_ICON = {
+    'Academic Journal' => 'book-open-4.svg',
+    'Books' => 'notebook.svg',
+    'News' => 'newspaper.svg',
+    'eBook' => 'notebook.svg',
+    'Video Recording' => 'camera-film-1.svg'
+  }.freeze
+
   include ActiveModel::API
 
   attr_accessor :title, :format, :journal, :author, :description, :link, :fulltext_link_html,
@@ -9,6 +17,6 @@ class ArticleResult
   alias fulltext_stanford_only? fulltext_stanford_only
 
   def icon
-    'notebook.svg'
+    FORMAT_TO_ICON.fetch(format, 'notebook.svg')
   end
 end
