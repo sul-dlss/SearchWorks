b713321040823ab8eaa6794f3097c78d66d3672d
Huda Khan <hudakhan@stanford.edu>
2025-06-24 22:59:22 -0600

    adding pending in the right place

--- spec/features/quick_report_spec.rb
+++ spec/features/quick_report_spec.rb
@@ -7,12 +7,13 @@ RSpec.feature 'Quick report form (js)', :js do
     visit solr_document_path('1')
   end
 
-  pending 'Needs implementation after new feedback modal in separate issue'
   scenario 'Quick report is available on show page' do
     visit solr_document_path('1')
     click_link 'Feedback'
+    pending 'Needs implementation after new feedback modal in separate issue'
     expect(page).to have_css('button.btn-quick-report')
     click_button 'Report wrong cover image'
+    pending 'Needs implementation after new feedback modal in separate issue'
     expect(page).to have_css('div.alert-success', text: 'Thank you! Your feedback has been sent.')
   end
 end
@@ -22,11 +23,12 @@ RSpec.feature 'Quick report form (no js)' do
     visit solr_document_path('1')
   end
 
-  pending 'Needs implementation after new feedback modal in separate issue'
   scenario 'Quick report is available on show page' do
     click_link 'Feedback'
+    pending 'Needs implementation after new feedback modal in separate issue'
     expect(page).to have_css('button.btn-quick-report')
     click_button 'Report wrong cover image'
+    pending 'Needs implementation after new feedback modal in separate issue'
     expect(page).to have_css('div.alert-success', text: 'Thank you! Your feedback has been sent.')
   end
 end
