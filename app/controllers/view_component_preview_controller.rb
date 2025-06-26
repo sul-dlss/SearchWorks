e934944b84468fd82a087628e7877c5008011947
Chris Beer <cabeer@stanford.edu>
2025-06-26 11:23:56 -0700

    Add a controller + layout for lookbook previews.

---
+++ app/controllers/view_component_preview_controller.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class ViewComponentPreviewController < ActionController::Base # rubocop:disable Rails/ApplicationController
+  include ViewComponent::PreviewActions
+  # Adds a few additional behaviors into the application controller
+  helper_method :controller_tracking_method, :blacklight_config, :blacklight_configuration_context, :search_session, :current_search_session, :search_state
+
+  def controller_tracking_method
+    'track_catalog_path'
+  end
+
+  def blacklight_configuration_context
+    Blacklight::Configuration::Context.new(self)
+  end
+
+  delegate :blacklight_config, to: :CatalogController
+
+  def search_session
+    {}
+  end
+
+  def current_search_session
+    nil
+  end
+
+  def search_state
+    Blacklight::SearchState.new(params, blacklight_config)
+  end
+end
