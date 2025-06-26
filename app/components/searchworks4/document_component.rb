dafd6cda3493049b0bd348d4e962ab37f56a45fa
Justin Coyne <jcoyne@justincoyne.com>
2025-06-26 17:01:00 -0500

    Update Blacklight

--- app/components/searchworks4/document_component.rb
+++ app/components/searchworks4/document_component.rb
@@ -3,8 +3,6 @@
 # Overriding the Blacklight component to bring the thumbnail up into the main section.
 module Searchworks4
   class DocumentComponent < Blacklight::DocumentComponent
-    attr_reader :document, :counter
-
     def resource_icon
       helpers.render_resource_icon(presenter.formats)
     end
