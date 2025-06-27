6d8bdd01894254cb2d9f1b73f5109163338c3ac6
Justin Coyne <jcoyne@justincoyne.com>
2025-06-27 15:24:25 -0500

    Facet toggle button style fixes
    Display as blue. Don't display the header and toggle button at the same time

--- app/components/facets/group_component.rb
+++ app/components/facets/group_component.rb
@@ -2,5 +2,8 @@
 
 module Facets
   class GroupComponent < Blacklight::Response::FacetGroupComponent
+    def collapse_toggle_button(panel_id, classes:)
+      render button_component.new(panel_id:, classes:)
+    end
   end
 end
