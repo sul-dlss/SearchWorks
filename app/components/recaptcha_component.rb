f6174a26a5a2ee5c6f058b6eadc98012f16af1d1
Steve Taylor <sctaylor@stanford.edu>
2025-06-27 12:11:01 -0700

    Update to use reCAPTCHA v3

---
+++ app/components/recaptcha_component.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class RecaptchaComponent < ViewComponent::Base
+  attr_reader :action, :inline_script, :site_key
+
+  def initialize(action:, inline_script: false, site_key: Recaptcha.configuration.site_key)
+    @action = action
+    @inline_script = inline_script
+    @site_key = site_key
+    super
+  end
+end
