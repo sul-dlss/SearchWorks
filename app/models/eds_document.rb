1951958bad5d555e0c46f00caae886a314b18801
Huda Khan <hudakhan@stanford.edu>
2025-07-02 10:27:35 -0600

    testing

--- app/models/eds_document.rb
+++ app/models/eds_document.rb
@@ -342,7 +342,14 @@ class EdsDocument # rubocop:disable Metrics/ClassLength
                                               'searchlink' => %w[fieldcode term]
                                             ))
     sanitize_config = config.nil? ? default_config : config
-    puts "--->EDS DOCUMENT MODEL"
+    # Passing option to handle errant brackets
+    sanitize_config[:parser_options] = { max_attributes: 2048 }
+    puts "updated sanitize config"
+
+    sanitize_config.keys.each do |key|
+      puts key
+    end
+    #puts sanitize_config.inspect
 
     html = CGI.unescapeHTML(data.to_s)
     # need to double-unescape data with an ephtml section
@@ -352,27 +359,10 @@ class EdsDocument # rubocop:disable Metrics/ClassLength
       html = html.gsub("\\n ", '')
     end
 
-    #puts html
-    puts html.length
-    begin
-      puts "Testing html against Nokogir"
-      e = Nokogiri.HTML5(html[7500, 15000], max_attributes: -1)
-      e.xpath('//*').each do |element|
-        puts element.name
-        puts element.attributes
-      end
-
-    rescue StandardError => e
-      puts "Error #{e}"
-    end
-    puts "HTML Fragment-->>"
-    puts html[7500, 15000]
-    sanitized_html = Sanitize.fragment(html[7500, 15000], sanitize_config)
+    sanitized_html = Sanitize.fragment(html, sanitize_config)
     # sanitize 5.0 fails to restore element case after doing lowercase
     sanitized_html = sanitized_html.gsub('<searchlink', '<searchLink')
     sanitized_html.gsub('</searchlink>', '</searchLink>')
-    puts "Resulting sanitized html"
-    puts sanitized_html
   end
 
   # add searchlinks when they don't exist
