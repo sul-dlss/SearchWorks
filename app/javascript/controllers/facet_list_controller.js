01c3fbf3d11ff6af66027b900c6f4b14bb314431
Marlo Longley <mlongley@stanford.edu>
2025-07-02 15:46:10 -0400

    Create separate logic and layout for top and other filters
    Closes #5319

--- app/javascript/controllers/facet_list_controller.js
+++ app/javascript/controllers/facet_list_controller.js
@@ -2,10 +2,9 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="facet-list"
 export default class extends Controller {
-  static targets = ['button', 'hideFacets']
+  static targets = ['button', 'title', 'hideFacets']
 
   connect() {
-    this.element.querySelectorAll('.facet-limit').forEach(facet => facet.classList.add('d-xxl-block'))
     this.#hide();
   }
 
@@ -18,13 +17,15 @@ export default class extends Controller {
   }
 
   #show() {
-    this.hideFacets().forEach(facet => facet.classList.remove('d-none'))
+    this.hideFacets().forEach(facet => facet.classList.remove('d-md-none'))
+    this.titleTarget?.classList.remove('d-md-none')
     this.buttonTarget.setAttribute('aria-expanded', 'true')
-    this.buttonTarget.innerHTML = '<i class="bi bi-sliders" aria-hidden="true"></i> Hide filters'
+    this.buttonTarget.innerHTML = '<i class="bi bi-sliders" aria-hidden="true"></i> Hide other filters'
   }
 
   #hide() {
-    this.hideFacets().forEach(facet => facet.classList.add('d-none'))
+    this.hideFacets().forEach(facet => facet.classList.add('d-md-none'))
+    this.titleTarget?.classList.add('d-md-none')
     this.buttonTarget.setAttribute('aria-expanded', 'false')
     this.buttonTarget.innerHTML = '<i class="bi bi-sliders" aria-hidden="true"></i> Show all filters'
   }
