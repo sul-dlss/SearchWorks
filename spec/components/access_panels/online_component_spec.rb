2c872cbcc1e93d88808a4e95012426a60b5b1b31
Chris Beer <cabeer@stanford.edu>
2025-07-03 13:20:09 -0700

    Make the sidebar headers h2s

--- spec/components/access_panels/online_component_spec.rb
+++ spec/components/access_panels/online_component_spec.rb
@@ -156,7 +156,7 @@ RSpec.describe AccessPanels::OnlineComponent, type: :component do
       document = SolrDocument.new(marc_links_struct: [{ href: '...', link_text: 'Link text', fulltext: true }])
       render_inline(described_class.new(document:))
       expect(page).to have_css(".panel-online")
-      expect(page).to have_css("h3", text: "Available online")
+      expect(page).to have_css("h2", text: "Available online")
       expect(page).to have_css("ul.links li a", text: "Link text")
     end
     it 'adds the stanford-only icon to Stanford only resources' do
@@ -170,7 +170,7 @@ RSpec.describe AccessPanels::OnlineComponent, type: :component do
       document = SolrDocument.new(marc_links_struct: [{ href: '...', link_text: 'Link text', fulltext: true }], druid: 'ng161qh7958')
       render_inline(described_class.new(document:))
       expect(page).to have_css '.panel-online'
-      expect(page).to have_css 'h3', text: 'Also available at'
+      expect(page).to have_css 'h2', text: 'Also available at'
     end
 
     context 'when the record has an SFX link' do
@@ -190,7 +190,7 @@ RSpec.describe AccessPanels::OnlineComponent, type: :component do
 
       it "renders a special card heading" do
         render_inline(described_class.new(document:))
-        expect(page).to have_css("h3", text: "Search this database")
+        expect(page).to have_css("h2", text: "Search this database")
       end
       it "renders a special card footer" do
         render_inline(described_class.new(document:))
