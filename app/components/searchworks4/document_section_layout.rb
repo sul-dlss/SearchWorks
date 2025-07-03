6b01c40eb40c03dac07ae52dc127f8c69f8feca6
Chris Beer <cabeer@stanford.edu>
2025-07-03 13:09:19 -0700

    Move other metadata down to a new Description field

--- app/components/searchworks4/document_section_layout.rb
+++ app/components/searchworks4/document_section_layout.rb
@@ -2,8 +2,9 @@
 
 module Searchworks4
   class DocumentSectionLayout < ViewComponent::Base
-    def initialize(title:, heading_level: :h2, dl_classes: 'dl-horizontal')
+    def initialize(title:, classes: ['mb-5'], heading_level: :h2, dl_classes: 'dl-horizontal')
       @title = title
+      @classes = classes
       @heading_level = heading_level
       @dl_classes = dl_classes
       super
