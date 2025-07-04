08c6da7d3a75cad49cebfa2207c1d8c7255335b1
Chris Beer <cabeer@stanford.edu>
2025-07-04 09:35:28 -0700

    Extract Record::DocumentLayoutComponent

--- app/components/record/marc_document_component.rb
+++ app/components/record/marc_document_component.rb
@@ -3,5 +3,13 @@
 module Record
   class MarcDocumentComponent < Blacklight::DocumentComponent
     delegate :render_if_present, to: :helpers
+
+    attr_reader :layout
+
+    def initialize(layout: Record::DocumentLayoutComponent, **)
+      super
+
+      @layout = layout
+    end
   end
 end
--- app/components/record/mods_document_component.rb
+++ app/components/record/mods_document_component.rb
@@ -3,5 +3,13 @@
 module Record
   class ModsDocumentComponent < Blacklight::DocumentComponent
     delegate :mods_record_field, :mods_name_field, :linked_mods_subjects, :linked_mods_genres, to: :helpers
+
+    attr_reader :layout
+
+    def initialize(layout: Record::DocumentLayoutComponent, **)
+      super
+
+      @layout = layout
+    end
   end
 end
