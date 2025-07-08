b934bceaac369a61cd535740be1208ddbcc54eeb
Justin Coyne <jcoyne@justincoyne.com>
2025-07-08 18:05:48 -0500

    Add slide up/fade out animation when removing bookmarks from the bookmark page

--- spec/features/bookmarking_items_spec.rb
+++ spec/features/bookmarking_items_spec.rb
@@ -4,15 +4,17 @@ require 'rails_helper'
 
 RSpec.feature 'Bookmarking Items', :js do
   let(:oclc_citation) { instance_double(Citations::OclcCitation) }
+  let(:user) { User.create!(email: 'example@stanford.edu', password: 'totallysecurepassword') }
 
   before do
     allow(Citations::OclcCitation).to receive(:new).and_return(oclc_citation)
     allow(oclc_citation).to receive_messages(
       citations_by_oclc_number: { '12345' => { 'mla' => '<p class="citation_style_MLA">MLA Citation</p>' } }
     )
+    login_as(user)
   end
 
-  context 'with bookmarks', :js do
+  describe 'adding and removing bookmarks on the search page' do
     it 'renders the page' do
       visit search_catalog_path f: { format: ["Book"] }, view: "default"
 
@@ -51,8 +53,56 @@ RSpec.feature 'Bookmarking Items', :js do
       within "#documents" do
         expect(page).to have_css("h3.index_title a", count: 2)
       end
+    end
+  end
 
-      click_link 'Cite'
+  describe 'removing bookmarks from the bookmarks page' do
+    before do
+      %w[1 2 3].each do |id|
+        document = SolrDocument.new(id:)
+        Bookmark.create!(document:, user:)
+      end
+
+      visit bookmarks_path
+    end
+
+    it 'renders the page' do
+      expect(page).to have_css('.active .bookmark-counter', text: '3')
+      expect(page).to have_css('.bookmark-counter', text: '0')
+      within "#documents" do
+        expect(page).to have_css("article", count: 3)
+      end
+      # Remove the second document from saved records
+      within(first('.document:nth-child(2)')) do
+        click_button('Remove from saved records')
+      end
+
+      expect(page).to have_content('Removed from bookmarks')
+
+      expect(page).to have_css('.active .bookmark-counter', text: '2')
+      expect(page).to have_css('.bookmark-counter', text: '0')
+      within "#documents" do
+        expect(page).to have_css("article", count: 2)
+      end
+    end
+  end
+
+  describe 'the cite modal' do
+    before do
+      %w[1 2 3].each do |id|
+        document = SolrDocument.new(id:)
+        Bookmark.create!(document:, user:)
+      end
+
+      visit bookmarks_path
+    end
+
+    it 'renders the page' do
+      within('.bookmark-toolbar') do
+        expect(page).to have_link 'Email'
+        expect(page).to have_button 'Print'
+        click_link 'Cite'
+      end
 
       within('.modal-dialog') do
         expect(page).to have_css('div#all')
