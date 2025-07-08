39727141edaff9a18d38ea402ed7e6d5005543c4
Huda Khan <hudakhan@stanford.edu>
2025-07-08 17:17:09 -0600

    simplifying mods author display

---
+++ app/components/searchworks4/mods_authors_component.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Searchworks4
+  class ModsAuthorsComponent < ViewComponent::Base
+    def initialize(document:)
+      @document = document
+      super
+    end
+
+    # The comma should not be part of the link for the author name
+    def label_display(label)
+      return '' if label.blank?
+
+      ", #{format_label(label)}"
+    end
+
+    def format_label(label)
+      # Remove suffix for : if present at the end of the string
+      # Downcase to meet expected display
+      formatted_label = helpers.sanitize_mods_name_label(label).downcase
+      # Remove any beginning and ending parentheses from post text
+      formatted_label.gsub(/[()]/, '')
+    end
+  end
+end
