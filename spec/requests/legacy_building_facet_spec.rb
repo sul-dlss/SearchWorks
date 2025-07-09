64a7825171db38991a4e06cc92fd5a6ef51e809b
Chris Beer <cabeer@stanford.edu>
2025-07-09 16:54:33 -0700

    Map library labels consistently, and use the code not the uuid for the url parameter.

--- spec/requests/legacy_building_facet_spec.rb
+++ spec/requests/legacy_building_facet_spec.rb
@@ -6,6 +6,6 @@ RSpec.describe 'Legacy building_facet routing' do
   it 'redirects building_facet to the library_code_facet_ssim equivalent' do
     get '/?f[building_facet][]=Business&f[language][]=English&search_field=search&q=transformative'
 
-    expect(response).to redirect_to '/catalog?f%5Blanguage%5D%5B%5D=English&f%5Blibrary_code_facet_ssim%5D%5B%5D=f5c58187-3db6-4bda-b1bf-e5f0717e2149&q=transformative&search_field=search'
+    expect(response).to redirect_to '/catalog?f%5Blanguage%5D%5B%5D=English&f%5Blibrary%5D%5B%5D=BUSINESS&q=transformative&search_field=search'
   end
 end
