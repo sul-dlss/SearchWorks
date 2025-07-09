b43e0597b6a0b896120fa628f41866650c76b406
Justin Coyne <jcoyne@justincoyne.com>
2025-07-09 17:31:05 -0500

    cleanup legacy searchworks 3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,10 +5,6 @@ module ApplicationHelper
     render partial: 'catalog/search_bar_advanced_widget'
   end
 
-  def render_search_bar_selections_widget
-    render partial: 'catalog/search_bar_selections_widget'
-  end
-
   def render_search_targets_widget
     render partial: 'catalog/search_targets_widget'
   end
