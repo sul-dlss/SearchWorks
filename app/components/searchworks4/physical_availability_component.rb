248771f0f19b31de04ca50046e6ff53574da0530
Chris Beer <cabeer@stanford.edu>
2025-07-09 13:52:25 -0700

    Close the vertical gap in the availability component

--- app/components/searchworks4/physical_availability_component.rb
+++ app/components/searchworks4/physical_availability_component.rb
@@ -6,7 +6,7 @@ module Searchworks4
 
     delegate :link_to_document, to: :helpers
 
-    def initialize(document:, classes: %w[availability-component border rounded p-2 fs-15], header_classes: %w[gap-4 row-gap-3 align-items-center flex-wrap])
+    def initialize(document:, classes: %w[availability-component border rounded p-2 fs-15], header_classes: %w[gap-4 row-gap-2 align-items-center flex-wrap])
       @document = document
       @classes = classes
       @header_classes = header_classes
