c23950e1f5149de2706b2606df4c32c5ff4d6587
Justin Coyne <jcoyne@justincoyne.com>
2025-07-10 15:14:06 -0500

    Pure CSS solution to table striping with hidden rows

--- app/javascript/controllers/course_reserves_controller.js
+++ app/javascript/controllers/course_reserves_controller.js
@@ -59,16 +59,13 @@ export default class extends Controller {
       if (numberDrawn < this.pageRows && this.rowMatches(tr, this.searchFilter)) {
         if (numberSkipped < numberToSkip) {
           numberSkipped++
-          console.log("skipped")
-          tr.style.display = "none"
+          tr.classList.add("d-none")
         } else {
-          const trClass = numberDrawn % 2 ? 'even' : 'odd';
-          tr.style.display = ""
-          tr.classList = trClass;
+          tr.classList.remove("d-none")
           numberDrawn++
         }
       } else {
-        tr.style.display = "none"
+        tr.classList.add("d-none")
       }
     })
   }
