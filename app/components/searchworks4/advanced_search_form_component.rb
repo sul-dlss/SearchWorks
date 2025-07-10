86dde690f02b3d4b53981ad5123584a606c5832b
Chris Beer <cabeer@stanford.edu>
2025-07-10 06:33:44 -0700

    Limit the fields shown in advanced search

--- app/components/searchworks4/advanced_search_form_component.rb
+++ app/components/searchworks4/advanced_search_form_component.rb
@@ -6,8 +6,12 @@ module Searchworks4
     # don't need any of the upstream functionality
     def before_render; end
 
+    def search_fields
+      blacklight_config.search_fields.select { |_, v| helpers.should_render_field?(v) }.map { |_key, f| { field: f.key, label: helpers.label_for_search_field(f.key) } }
+    end
+
     def search_filter_controls
-      fields = blacklight_config.facet_fields.select { |_k, v| v.include_in_advanced_search || v.include_in_advanced_search.nil? }
+      fields = blacklight_config.facet_fields.select { |_k, v| v.include_in_advanced_search || (v.include_in_advanced_search.nil? && helpers.should_render_field?(v)) }
 
       fields.sort_by { |_k, config| helpers.facet_field_label(config.key) }.select { |field| helpers.should_render_field?(field) }.map do |_key, config|
         display_facet = @response.aggregations[config.field]
