d251f2425dfc64f3e38b2aba4e3873ed059a09fa
Huda Khan <hudakhan@stanford.edu>
2025-07-11 13:03:18 -0600

    updating tests

--- app/components/access_panels/library_location_popover_component.rb
+++ app/components/access_panels/library_location_popover_component.rb
@@ -1,34 +1,31 @@
-
 # frozen_string_literal: true
 
 module AccessPanels
-    class LibraryLocationPopoverComponent < ViewComponent::Base
-      
-        # location.mhld
-      def initialize(mhld, left:false)
-        @mhld = mhld
-      end
-
-      def render?
-        @mhld.present? && @mhld.any? { |mhld_item| mhld_item.library_has.present? }
-      end
-
-      def popover_content
-        sanitize "<div class='fw-bold mb-2'>Library has:</div><div> #{item_locations}</div>"
-      end
-
-
-      def item_locations
-        @mhld.map do |mhld_item|
-            library_has(mhld_item)
-        end.join(', ')   
-      end
-
-      def library_has(mhld_item)
-        return "" if mhld_item.library_has.blank?
-
-        mhld_item.library_has
-      end
+  class LibraryLocationPopoverComponent < ViewComponent::Base
+    # location.mhld
+    def initialize(mhld)
+      @mhld = mhld
+      super
     end
-end
 
+    def render?
+      @mhld.present? && @mhld.any? { |mhld_item| mhld_item.library_has.present? }
+    end
+
+    def popover_content
+      sanitize "<div class='fw-bold mb-2'>Library has:</div><div> #{item_locations}</div>"
+    end
+
+    def item_locations
+      @mhld.map do |mhld_item|
+        library_has(mhld_item)
+      end.join(', ')
+    end
+
+    def library_has(mhld_item)
+      return "" if mhld_item.library_has.blank?
+
+      mhld_item.library_has
+    end
+  end
+end
--- app/javascript/controllers/library_has_controller.js
+++ app/javascript/controllers/library_has_controller.js
@@ -5,7 +5,6 @@ export default class extends Controller {
   static targets = ["container"]
 
   connect() {
-    console.log("Connect to Library Has")
     this.updateDisplay()
   }
 
