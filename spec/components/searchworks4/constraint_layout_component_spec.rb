c08a5ef20aeae10b0a3ae67dce46ca13f2b6974d
Huda Khan <hudakhan@stanford.edu>
2025-07-11 17:15:35 -0600

    changing remove button to match exhibits style

--- spec/components/searchworks4/constraint_layout_component_spec.rb
+++ spec/components/searchworks4/constraint_layout_component_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Searchworks4::ConstraintLayoutComponent, type: :component do
   let(:component) { described_class.new(label: "my label", value: "my value", remove_path: "http://remove") }
 
   it "includes remove link that uses the font based icons" do
-    expect(page).to have_css("a.remove .bi.bi-x-lg")
+    expect(page).to have_css("a.remove .bi.bi-x.fs-4")
     expect(page).to have_no_css("svg")
   end
 end
