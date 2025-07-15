838bd656a538d5ffcd5d51b357647cd8696d85cd
Chris Beer <cabeer@stanford.edu>
2025-07-14 19:20:56 -0700

    Separate HomepageSearchComponent from MastheadSearchComponent

--- app/components/masthead_search_component.rb
+++ app/components/masthead_search_component.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
-class HomepageSearchComponent < Blacklight::SearchBarComponent
-  renders_one :footer
-
+class MastheadSearchComponent < Blacklight::SearchBarComponent
   def search_button
     render SearchButtonComponent.new(id: "#{@prefix}search", text: 'Search')
   end
