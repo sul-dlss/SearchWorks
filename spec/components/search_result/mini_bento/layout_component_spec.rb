602ea7753ca7d01824edbbe5b9ac77dd92d61c9d
Steve Taylor <sctaylor@stanford.edu>
2025-07-16 11:26:51 -0700

    Update other component specs for mini bento

--- spec/components/search_result/mini_bento/layout_component_spec.rb
+++ spec/components/search_result/mini_bento/layout_component_spec.rb
@@ -4,7 +4,7 @@ require "rails_helper"
 
 RSpec.describe SearchResult::MiniBento::LayoutComponent, type: :component do
   before do
-    render_inline(described_class.new(close: true, url: '/foo?bar=baz', i18n_key: :catalog))
+    render_inline(described_class.new(url: '/foo?bar=baz', i18n_key: :catalog))
   end
 
   it 'draws the page' do
