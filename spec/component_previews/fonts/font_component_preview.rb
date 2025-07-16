3b13d031c7aafb4e87cac9c03938eca1cd0c4b32
Chris Beer <cabeer@stanford.edu>
2025-07-16 09:58:09 -0700

    Add a lookbook for font stack customizations

---
+++ spec/component_previews/fonts/font_component_preview.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class DummyComponent < ViewComponent::Base
+  def initialize(string:)
+    @string = string
+    super
+  end
+
+  def call
+    @string
+  end
+end
+
+# This is a preview for the FontComponent.
+# It renders a simple message using the DummyComponent.
+# The message can be customized by passing a different name.
+module Fonts
+  class FontComponentPreview < Lookbook::Preview
+    layout 'lookbook'
+
+    # @!group Variations
+
+    # @label Tibetan (13885669)
+    def tibetan
+      render DummyComponent.new(string: "བོད་ཀྱི་གསོ་བ་རིག་པའི་ལོ་རྒྱུས་ཀྱི་བང་མཛོད་གཡུ་ཐོག་བླ་མ་དྲན་གླུ།")
+    end
+
+    # @label Cyrillic (in00000002927)
+    def cyrillic
+      render DummyComponent.new(string: "Strategii͡a planirovanii͡a izbiratelʹnoĭ kampanii")
+    end
+
+    # @!endgroup
+  end
+end
