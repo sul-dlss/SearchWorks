37a400ad8bb36062f20ce468c8de098c325bfc82
Justin Coyne <jcoyne@justincoyne.com>
2025-07-16 15:30:51 -0500

    Simplify initializer of MastheadSearchComponent (#5670)

--- app/components/masthead_search_component.rb
+++ app/components/masthead_search_component.rb
@@ -1,22 +1,11 @@
 # frozen_string_literal: true
 
 class MastheadSearchComponent < Blacklight::SearchBarComponent
-  # rubocop:disable Metrics/ParameterLists, Naming/MethodParameterName
-  def initialize(
-    url:,
-    params:,
-    advanced_search_url: nil,
-    classes: %w[search-query-form col-md-12 col-lg-8], prefix: nil,
-    method: 'GET', q: nil, query_param: :q,
-    search_field: nil, autocomplete_path: nil,
-    autofocus: nil, i18n: { scope: 'blacklight.search.form' },
-    form_options: {}
-  )
+  def initialize(**)
     super
     # Omit view to avoid being stuck in gallery mode from book funds/full page browse nearby.
     @params = @params.except(:view)
   end
-  # rubocop:enable Metrics/ParameterLists, Naming/MethodParameterName
 
   def search_button
     render SearchButtonComponent.new(id: "#{@prefix}search", text: 'Search')
