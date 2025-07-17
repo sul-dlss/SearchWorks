f55f0b672f5333a5f6e936c3aa84ccb76be715fc
Justin Coyne <jcoyne@justincoyne.com>
2025-07-17 11:48:56 -0500

    Stop using font-awesome icons

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,7 +8,6 @@ Rails.application.config.assets.version = "1.0"
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # # Add Yarn node_modules folder to the asset load path.
-Rails.application.config.assets.paths << Rails.root.join('node_modules', 'font-awesome', 'fonts')
 Rails.application.config.assets.paths << Rails.root.join('node_modules', 'leaflet', 'dist') # Just for images used in leaflet css
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
