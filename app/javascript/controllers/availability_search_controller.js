a11715bb7b8d64f81e28055d171aa9e9c58f10ea
Chris Beer <cabeer@stanford.edu>
2025-07-18 12:18:52 -0700

    Add fallback for RegExp.escape

--- app/javascript/controllers/availability_search_controller.js
+++ app/javascript/controllers/availability_search_controller.js
@@ -5,6 +5,15 @@ export default class extends Controller {
   connect() {
   }
 
+  escape(searchTerm) {
+    if (RegExp.escape) {
+      return RegExp.escape(searchTerm);
+    } else {
+      // Fallback for older browsers
+      return searchTerm.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // Escape special characters
+    }
+  }
+
   search(event) {
     const searchTerm = event.target.value.trim()
 
@@ -22,7 +31,7 @@ export default class extends Controller {
 
       if (element.textContent.toLowerCase().includes(searchTerm.toLowerCase())) {
         element.innerHTML = element.textContent.replace(
-          new RegExp(RegExp.escape(searchTerm), 'gi'),
+          new RegExp(this.escape(searchTerm), 'gi'),
           (match) => `<mark class="p-0 bg-body fw-bold">${match}</mark>`
         );
       }
