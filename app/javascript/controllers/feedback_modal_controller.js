083aa2d9949d8e7bb2ec096301d2bcc93818e392
Chris Beer <cabeer@stanford.edu>
2025-07-24 12:29:09 -0700

    We don't need to explicitly show the toast any more.

--- app/javascript/controllers/feedback_modal_controller.js
+++ app/javascript/controllers/feedback_modal_controller.js
@@ -2,13 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="feedback-modal"
 export default class extends Controller {
-  
-  async showToast(event) {
-    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(document.getElementById('toast'))
-    toastBootstrap.show()
-    this.closeModal()
-  }
-
   closeModal() {
     document.getElementById('blacklight-modal').close()
   }
