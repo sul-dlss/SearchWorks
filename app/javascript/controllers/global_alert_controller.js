d709eac72a91a67e0626e691a6e24bcb0715aab7
Chris Beer <cabeer@stanford.edu>
2025-07-24 10:57:34 -0700

    Fix up global alert cookies

--- app/javascript/controllers/global_alert_controller.js
+++ app/javascript/controllers/global_alert_controller.js
@@ -13,7 +13,7 @@ export default class extends Controller {
     if (this.dismissValue == "permanent") {
       const date = new Date();
       date.setFullYear(date.getFullYear() + 1);
-      lifetime = `expires=${date.toUTCString()}`;
+      lifetime = `expires=${date.toUTCString()};`;
     }
 
     document.cookie = `${cookieId}=dismissed; ${lifetime} path=/`;
