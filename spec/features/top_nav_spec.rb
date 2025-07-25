a6cda4439eab58743b8380fc3a45abf7634b7d62
dnoneill <noneill@stanford.edu>
2025-07-25 15:19:01 -0400

    possible fix for flappy top_nav test

--- spec/features/top_nav_spec.rb
+++ spec/features/top_nav_spec.rb
@@ -5,6 +5,8 @@ require 'rails_helper'
 RSpec.feature "Top Navigation" do
   it "has navigational links and top menu" do
     visit root_path
+
+    expect(page).to have_css('header')
     within "header > nav" do
       expect(page).to have_link "Login"
       expect(page).to have_link "Feedback"
