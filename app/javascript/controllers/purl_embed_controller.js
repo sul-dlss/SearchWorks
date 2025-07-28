1c677a2afef5c1e27108e57bc28b0ddb216b6f05
Chris Beer <cabeer@stanford.edu>
2025-07-28 07:23:41 -0700

    Convert purl-embed to a stimulus controller, and add a litte logic to stop duplicate loading

---
+++ app/javascript/controllers/purl_embed_controller.js
@@ -0,0 +1,29 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="purl-embed"
+export default class extends Controller {
+  static values = {
+    url: String
+  }
+
+  connect() {
+    this.loadEmbed()
+  }
+
+  loadEmbed() {
+    if (this.element.attributes.complete || this.element.attributes.busy) return;
+
+    this.element.setAttribute("busy", "");
+
+    fetch(this.urlValue)
+      .then(response => response.json())
+      .then(data => {
+        this.element.removeAttribute("busy");
+        if (data !== null) {
+          this.element.innerHTML = data.html
+          this.element.setAttribute("complete", "");
+        }
+      })
+      .catch(error => console.error('Error loading embed:', error))
+  }
+}
--- app/javascript/searchworks4.js
+++ app/javascript/searchworks4.js
@@ -7,7 +7,6 @@ import './article'
 import "./popover"
 import "./feedback_form"
 import "./responsive-aside"
-import "./purl-embed"
 import "./range-limit"
 
 // Prevent the back-button from trying to add a second instance of recaptcha
