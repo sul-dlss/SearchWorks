e5ec66f44e20ae0922c3df61ff2b3824874617c2
Chris Beer <cabeer@stanford.edu>
2025-08-01 09:58:01 -0700

    Consolidate feedback-modal into modal.

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,4 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
+import Blacklight from "blacklight-frontend"
 
 // Connects to data-controller="modal"
 export default class extends Controller {
@@ -6,5 +7,9 @@ export default class extends Controller {
   fixupBackdrop() {
     document.body.style.removeProperty('overflow');
     document.body.style.removeProperty('padding-right');
+  }
+
+  close() {
+    Blacklight.Modal.hide()
   }
 }
