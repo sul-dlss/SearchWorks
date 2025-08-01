e2de028b0b6e076b5dfd2aeca986a4f92679400d
Justin Coyne <jcoyne@justincoyne.com>
2025-08-01 08:16:23 -0500

    Fix missing template error

--- spec/requests/stackmap_spec.rb
+++ spec/requests/stackmap_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe 'Stackmap' do
 
     expect(response).to have_http_status(:ok)
   end
+
+  context 'when json is requested' do
+    it 'is not available' do
+      get '/view/4/GRE-STACKS/stackmap.json'
+
+      expect(response).to have_http_status(:not_acceptable)
+    end
+  end
 end
