32c64adde1e04c8222f45d1c17a6688b8be4746b
Chris Beer <cabeer@stanford.edu>
2025-08-01 08:19:10 -0700

    Render Citations::CitationComponent in the right initial state so it doesn't need javascript.

--- app/javascript/controllers/citation_style_picker_controller.js
+++ app/javascript/controllers/citation_style_picker_controller.js
@@ -4,10 +4,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["select", "tab"]
 
-  connect() {
-    this.switch()
-  }
-
   switch() {
     this.tabTargets.forEach(tab => tab.hidden = true )
     this.element.querySelector(`#${this.selectTarget.value}`).hidden = false
