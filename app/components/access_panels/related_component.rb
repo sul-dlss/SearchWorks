220183722b9d07677bc3f2317a66534f3de269b3
Chris Beer <cabeer@stanford.edu>
2025-08-06 15:35:09 -0700

    Push additional_finding_aids methods into the component that needs them.

--- app/components/access_panels/related_component.rb
+++ app/components/access_panels/related_component.rb
@@ -3,10 +3,18 @@
 module AccessPanels
   # Displays related items, additional finding aids, and provides a place for plugGoogleBookContent() to add book cover previews
   class RelatedComponent < AccessPanels::Base
-    delegate :additional_finding_aids?, :additional_finding_aids, to: :document
-
     def oclc
       document.oclc_number
+    end
+
+    def additional_finding_aids?
+      finding_aids.length > 1
+    end
+
+    def additional_finding_aids
+      return nil unless additional_finding_aids?
+
+      finding_aids.drop(1)
     end
 
     private
@@ -18,6 +26,10 @@ module AccessPanels
     def finding_aid_class(link)
       # OAC finding aids use both 'www.oac.cdlib.org' and 'oac.cdlib.org'
       'oac' if link.href&.include?('oac.cdlib.org')
+    end
+
+    def finding_aids
+      document.marc_links&.finding_aid || []
     end
   end
 end
