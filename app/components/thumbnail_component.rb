d8cf217ff182868e8eeb4ba576774f0d68e2201e
Chris Beer <cabeer@stanford.edu>
2025-08-06 07:35:02 -0700

    Rename ThumbnailWithIiifComponent to just ThumbnailComponent, because the IIIF analytics probably never worked right and are irrelevant now that we dropped the IIIF icon.

---
+++ app/components/thumbnail_component.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+class ThumbnailComponent < Blacklight::Document::ThumbnailComponent
+end
--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -122,7 +122,7 @@ class CatalogController < ApplicationController
     config.index.facet_pagination_component = Searchworks4::FacetPaginationComponent
 
     config.index.title_field = Blacklight::Configuration::IndexField.new(field: 'title_display', steps: [TitleRenderingStep])
-    config.index.thumbnail_component = ThumbnailWithIiifComponent
+    config.index.thumbnail_component = ThumbnailComponent
     config.index.thumbnail_method = :render_cover_image
     config.index.mini_bento_component = SearchResult::MiniBento::CatalogComponent
 
