a681a60447b9e09d3b33528f45a6700b2b3b4670
Chris Beer <cabeer@stanford.edu>
2025-08-26 08:46:31 -0700

    Update text from 'Also available at' to 'Also online at'
    Fixes #6068

--- spec/components/access_panels/online_component_spec.rb
+++ spec/components/access_panels/online_component_spec.rb
@@ -147,7 +147,7 @@ RSpec.describe AccessPanels::OnlineComponent, type: :component do
       document = SolrDocument.new(marc_links_struct: [{ href: '...', link_text: 'Link text', fulltext: true }], druid: 'ng161qh7958')
       render_inline(described_class.new(document:))
       expect(page).to have_css '.panel-online'
-      expect(page).to have_css 'h2', text: 'Also available at'
+      expect(page).to have_css 'h2', text: 'Also online at'
     end
 
     describe "database" do
