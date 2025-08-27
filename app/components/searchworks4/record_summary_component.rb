36f697e92415a160d5e27e9a743eb99455da346c
Chris Beer <cabeer@stanford.edu>
2025-08-27 11:51:32 -0700

    Constraint the height of the modal thumbnail.

--- app/components/searchworks4/record_summary_component.rb
+++ app/components/searchworks4/record_summary_component.rb
@@ -28,7 +28,7 @@ module Searchworks4
     def default_thumbnail
       return nil if presenter.document.eds? || presenter.document.managed_purls.many? || presenter.document.druid
 
-      Searchworks4::ThumbnailComponent.new(presenter: presenter, counter: nil, classes: %w[document-thumbnail float-end mt-1])
+      Searchworks4::ThumbnailComponent.new(presenter: presenter, counter: nil, classes: %w[document-thumbnail mt-1])
     end
   end
 end
