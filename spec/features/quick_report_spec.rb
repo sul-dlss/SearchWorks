408d3236baa0a7a9f2e7dff7f637a3b670d50b43
Steve Taylor <sctaylor@stanford.edu>
2025-09-10 14:55:43 -0700

    Update feedback form button language (#6116)

--- spec/features/quick_report_spec.rb
+++ spec/features/quick_report_spec.rb
@@ -7,7 +7,7 @@ RSpec.feature 'Quick report form', :js do
     visit solr_document_path('in00000053236')
     click_link 'Feedback'
     expect(page).to have_css('button.btn-quick-report')
-    click_button 'Report wrong cover image'
+    click_button 'Report incorrect bookcover'
     expect(page).to have_css('.toast', text: 'Your feedback has been sent.')
   end
 end
