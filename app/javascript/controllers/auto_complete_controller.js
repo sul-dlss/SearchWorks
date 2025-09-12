3f8c43e44c228dc640d6e4fb10e5aedc9f644c01
Huda Khan <hudakhan@stanford.edu>
2025-09-12 17:42:04 -0600

    if author field already selected, code now looks for that for  autocomplete

--- app/javascript/controllers/auto_complete_controller.js
+++ app/javascript/controllers/auto_complete_controller.js
@@ -1,7 +1,15 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["autoComplete", "popup"]
+  static targets = ["autoComplete", "popup", "select"]
+
+  connect() {
+    // Check if author has already been selected as search field.
+    // If author has been selected, ensure autocomplete source is set correctly
+    if (this.selectTarget.value == 'search_author') {
+      this.autoCompleteTarget.src = this.autoCompleteTarget.dataset['autoCompleteSrc']
+    }
+  }
  
   onChange(event) {
     const selectedValue = event.target.value;
