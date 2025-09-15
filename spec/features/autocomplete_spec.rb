baa410d988de7a22438c55b9f2e7e99a176aa2bf
Huda Khan <hudakhan@stanford.edu>
2025-09-15 13:32:44 -0600

    rubocop

--- spec/features/autocomplete_spec.rb
+++ spec/features/autocomplete_spec.rb
@@ -7,10 +7,11 @@ RSpec.feature "Autocomplete" do
     before do
       visit root_path
     end
+
     scenario "has autocomplete src set when user select author/contributor", :js do
       expect(page).to have_css('.input-group[data-controller="auto-complete"]')
       expect(page).to have_css('auto-complete[data-auto-complete-src="/catalog/suggest"]')
-      expect(page).to_not have_css('auto-complete[src="/catalog/suggest"]')
+      expect(page).to have_no_css('auto-complete[src="/catalog/suggest"]')
 
       select('Author/Contributor', from: 'search_field')
       expect(page).to have_css('auto-complete[src="/catalog/suggest"]')
@@ -25,7 +26,7 @@ RSpec.feature "Autocomplete" do
     scenario "has autocomplete src set when user select author/contributor", :js do
       expect(page).to have_css('.input-group[data-controller="auto-complete"]')
       expect(page).to have_css('auto-complete[data-auto-complete-src="/catalog/suggest"]')
-      expect(page).to_not have_css('auto-complete[src="/catalog/suggest"]')
+      expect(page).to have_no_css('auto-complete[src="/catalog/suggest"]')
 
       select('Author/Contributor', from: 'search_field')
       expect(page).to have_css('auto-complete[src="/catalog/suggest"]')
@@ -34,7 +35,7 @@ RSpec.feature "Autocomplete" do
 
   context "when on the search results page with author/contributor pre-selected", :js do
     before do
-      visit search_catalog_path('search_field': 'search_author')
+      visit search_catalog_path(search_field: 'search_author')
     end
 
     scenario "has autocomplete src set with pre-selected author/contributor", :js do
