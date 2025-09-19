dd03cde2083b607bf8e4cc73b4109e4f01fa3431
dnoneill <noneill@stanford.edu>
2025-09-19 17:04:52 -0400

    add back to avaliability in modal

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,18 @@ module ApplicationHelper
   def from_advanced_search?
     params[:search_field] == 'advanced' || params[:clause].present?
   end
+
+  def modal_back_link
+    return unless params[:backlink_href]
+
+    link_to(
+      params[:backlink_href],
+      data: { blacklight_modal: 'trigger' }
+    ) do
+      safe_join([
+        tag.i(class: 'bi bi-forward-fill rotate-180 me-1', aria: { hidden: true }),
+        "Back to #{params[:backlink_label]}"
+      ])
+    end
+  end
 end
