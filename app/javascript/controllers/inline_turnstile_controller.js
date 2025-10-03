1652816c490aba767f2dd796ef5133ec71acf69d
Chris Beer <cabeer@stanford.edu>
2025-10-03 14:52:36 -0700

    Implicitly render normal turnstile challenges too

--- app/javascript/controllers/inline_turnstile_controller.js
+++ app/javascript/controllers/inline_turnstile_controller.js
@@ -4,6 +4,10 @@ export default class extends Controller {
   static targets = ["challenge", "frame"]
   static values = { challengePath: String, siteKey: String, status: Boolean }
 
+  connect() {
+    turnstile.implicitRender();
+  }
+
   frameTargetConnected(element) {
     if (this.statusValue) {
       this.convertFrame(element.querySelector('turbo-frame'))
