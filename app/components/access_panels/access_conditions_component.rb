ab5faee72efa57f5276266083ee9d3e51d35c6d0
Nick Budak <budak@stanford.edu>
2026-03-04 10:05:46 -0800

    Render access conditions panel for Cocina-backed items
    Fixes #6514

--- app/components/access_panels/access_conditions_component.rb
+++ app/components/access_panels/access_conditions_component.rb
@@ -2,8 +2,24 @@
 
 module AccessPanels
   class AccessConditionsComponent < AccessPanels::Base
+    delegate :cocina_display, to: :document
+
+    def field_map
+      return [] unless document.cocina?
+
+      @field_map ||= [
+        cocina_display.use_and_reproduction_display_data,
+        cocina_display.copyright_display_data,
+        cocina_display.license_display_data
+      ].compact_blank
+    end
+
+    def mods_access_conditions
+      document.mods&.accessCondition || []
+    end
+
     def render?
-      document.mods&.accessCondition.present?
+      field_map.present? || mods_access_conditions.present?
     end
   end
 end
--- spec/components/access_panels/access_conditions_component_spec.rb
+++ spec/components/access_panels/access_conditions_component_spec.rb
@@ -8,24 +8,58 @@ RSpec.describe AccessPanels::AccessConditionsComponent, type: :component do
   subject(:page) { render_inline(component) }
 
   let(:component) { described_class.new(document:) }
-  let(:document) { SolrDocument.new(modsxml:) }
 
-  context "when access conditions are on the record" do
-    let(:modsxml) { mods_001 }
+  context "with MODS records" do
+    let(:document) { SolrDocument.new(modsxml:) }
 
-    it "displays access" do
-      expect(page).to have_css('h2', text: "Access conditions")
+    context "when access conditions are on the record" do
+      let(:modsxml) { mods_001 }
 
-      expect(page).to have_css("dt", text: "Use and reproduction")
-      expect(page).to have_css("dd", text: "Copyright © Stanford University.")
+      it "displays access" do
+        expect(page).to have_css('h2', text: "Access conditions")
+
+        expect(page).to have_css("dt", text: "Use and reproduction")
+        expect(page).to have_css("dd", text: "Copyright © Stanford University.")
+      end
+    end
+
+    context "when access conditions are not provided" do
+      let(:modsxml) { mods_only_title }
+
+      it "displays correct sections" do
+        expect(page).to have_no_css('h2', text: "Access conditions")
+      end
     end
   end
 
-  context "when access conditions are not provided" do
-    let(:modsxml) { mods_only_title }
+  context "with Cocina records" do
+    context "with a use and reproduction statement" do
+      let(:document) { SolrDocument.from_fixture("bc798xr9549.yml") }
 
-    it "displays correct sections" do
-      expect(page).to have_no_css('h2', text: "Access conditions")
+      it "displays the use and reproduction statement" do
+        expect(page).to have_css("dt", text: "Use and reproduction")
+        # rubocop:disable Layout/LineLength
+        expect(page).to have_css("dd", text: "Property rights reside with the repository. To obtain permission to publish or reproduce, please contact the East Asia Library at eastasialibrary@stanford.edu.")
+        # rubocop:enable Layout/LineLength
+      end
+    end
+
+    context "with a copyright statement" do
+      let(:document) { SolrDocument.from_fixture("bx988zq7071.yml") }
+
+      it "displays the copyright statement" do
+        expect(page).to have_css("dt", text: "Copyright")
+        expect(page).to have_css("dd", text: "Materials may be subject to copyright.")
+      end
+    end
+
+    context "with a license statement" do
+      let(:document) { SolrDocument.from_fixture("bb060nk0241.yml") }
+
+      it "displays the license statement" do
+        expect(page).to have_css("dt", text: "License")
+        expect(page).to have_css("dd", text: "This work is licensed under an Open Data Commons Attribution License v1.0.")
+      end
     end
   end
 end
